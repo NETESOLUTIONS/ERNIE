93392f575405274d3c51c60ad1e7004bdd52359b
VJ Davey <akdavey@email.wm.edu>
2017-10-11 15:41:54 -0400

    Derwent Weekly Automation Scripts for ERNIE
    Scripts used to automate weekly processes on ERNIE. Goal is to retain advances made in parallelization efforts from smokeload.

---
+++ Patents/Automation/Updates_Download/download_derwent.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+# This script downloads Derwent update files from FTP site, under ug directory and places those files locally into the update_files directory
+
+# Usage: sh gw_download_derwent.sh work_dir/ username pswd
+#        where work_dir specifies working directory, username specifies FTP username, and pswd specifies password to connect to data FTP site.
+
+# Author: VJ Davey, based on work by Lingtian "Lindsay" Wan
+# Create Date: 10/11/2017
+
+# Change to working directory.
+c_dir=$1; username=$2; pswd=$3
+cd $c_dir
+
+# Stamp current time.
+date ; date >> log_derwent_download.txt
+
+# Download a complete filelist from FTP site.
+echo ***Getting a list of files from the FTP server...
+ftp -in ftpserver.wila-derwent.com << SCRIPTEND
+user $username $pswd
+lcd $c_dir
+binary
+cd ug ; mls * $c_dir/full_ftp_filelist_ug.txt
+quit
+SCRIPTEND
+
+# List all the tar and meta files
+cat full_ftp_filelist_ug.txt | grep tar > tar_ftp_filelist_ug.txt ; cat full_ftp_filelist_ug.txt | grep meta > meta_ftp_filelist_ug.txt
+# Compare current files with file list to determine which files should be downloaded
+grep -Fxvf begin_filelist_ug.txt tar_ftp_filelist_ug.txt > derwent_download_list_ug.txt
+# Compare current meta files with filelist and determine files to be downloaded.
+grep -Fxvf begin_filelist_ug_meta.txt meta_ftp_filelist_ug.txt > derwent_download_list_ug_meta.txt
+
+# Write new file names to log.
+printf 'New update/delete files:\n' >> log_derwent_download.txt ; cat derwent_download_list_ug.txt >> log_derwent_download.txt
+printf 'New update/delete meta files:\n' >> log_derwent_download.txt ; cat derwent_download_list_ug_meta.txt >> log_derwent_download.txt
+
+
+# Write command to a download batch script for downloading ug files.
+echo ***Preparing to download newly-added files...
+printf 'ftp -in ftpserver.wila-derwent.com <<SCRIPTEND\n' > group_download_ug.sh
+printf 'user '$username' '$pswd'\n' >> group_download_ug.sh
+printf 'lcd '$c_dir'update_files/\n' >> group_download_ug.sh
+printf 'cd ug\n' >> group_download_ug.sh
+printf 'binary\n' >> group_download_ug.sh
+cat derwent_download_list_ug.txt | awk '{print "get " $1}' >> group_download_ug.sh
+printf 'quit\nSCRIPTEND\n\n' >> group_download_ug.sh
+
+# Write command to a download batch script for downloading ug meta files.
+echo ***Preparing to download newly-added files...
+printf 'ftp -in ftpserver.wila-derwent.com <<SCRIPTEND\n' > group_download_ug_meta.sh
+printf 'user '$username' '$pswd'\n' >> group_download_ug_meta.sh
+printf 'lcd '$c_dir'update_files/\n' >> group_download_ug_meta.sh
+printf 'cd ug\n' >> group_download_ug_meta.sh
+printf 'binary\n' >> group_download_ug_meta.sh
+cat derwent_download_list_ug_meta.txt | awk '{print "get " $1}' >> group_download_ug_meta.sh
+printf 'quit\nSCRIPTEND\n\n' >> group_download_ug_meta.sh
+
+# Download newly-added files from the FTP server.
+echo ***Downloading newly-added files...
+sh group_download_ug_meta.sh
+sh group_download_ug.sh
+echo ***Download finished.
+
+# Check if new files are all downloaded and write to log.
+echo ***Checking downloading results...
+ls update_files/ -ltr | grep cxml_ug2 | grep tar | awk '{print $9}' > updated_filelist_ug.txt
+grep -Fxvf begin_filelist_ug.txt updated_filelist_ug.txt > downloaded_filelist_ug.txt
+ls update_files/ -ltr | grep cxml_ug2 | grep meta | awk '{print $9}' > updated_filelist_ug_meta.txt
+grep -Fxvf begin_filelist_ug_meta.txt updated_filelist_ug_meta.txt > downloaded_filelist_ug_meta.txt
+cat downloaded_filelist_ug.txt >> begin_filelist_ug.txt
+cat downloaded_filelist_ug_meta.txt >> begin_filelist_ug_meta.txt
+printf 'Downloaded files:\n' >> log_derwent_download.txt
+cat downloaded_filelist_ug.txt >> log_derwent_download.txt
+cat downloaded_filelist_ug_meta.txt >> log_derwent_download.txt
+printf 'Files not downloaded:\n' >> log_derwent_download.txt
+grep -Fxvf downloaded_filelist_ug.txt derwent_download_list_ug.txt >> log_derwent_download.txt
+grep -Fxvf downloaded_filelist_ug_meta.txt derwent_download_list_ug_meta.txt >> log_derwent_download.txt
+
+# Compare checksum
+echo ***Comparing Checksum...
+cat downloaded_filelist_ug.txt | awk '{split($1,filename,"."); print "echo $(cat ./update_files/" filename[1] "_meta.xml | grep checksum | sed '\''s/\\(checksum=\\|\"\\|\\t\\| \\)//g'\'') ./update_files/" $1 " | md5sum -c -"}' > derwent_checksum.sh
+printf 'Checksum results:\n' >> log_derwent_download.txt ; sh derwent_checksum.sh >> log_derwent_download.txt
+
+# Remove UG and UGLSP files older than 5 weeks to save space.
+echo ***Removing old UG files...
+ls update_files/ | grep cxml_ug | sort -n > current_stored_ug_files.txt
+cnt=$(($(cat current_stored_ug_files.txt | wc -l) - 10))
+if [ $cnt -gt 0 ]
+  then
+    for file in $(cat current_stored_ug_files.txt | head -$cnt)
+    do
+      rm update_files/$file
+    done
+fi
+
+# Write ending time to log.
+date ; date >> log_derwent_download.txt ; printf '\n\n' >> log_derwent_download.txt
---
+++ Patents/Automation/Updates_Processing/derwent_process_parallel_auto.sh
@@ -0,0 +1,60 @@
+########################################################################################################
+# Program Name: derwent_process_parallel_auto.sh
+# Usage:        derwent_process_parallel_auto.sh update_dir work_dir csv_dir
+#           update_dir is the directory holding the xml.gz files, work_dir is a space where the script can do its job, csv_dir is the desired output directory for csv files
+# Author:       VJ Davey,  Lingtian "Lindsay" Wan
+# Date:         10/11/2017, VJ Davey, created script as an offshoot of derwent update auto
+#########################################################################################################
+
+date
+# Change to working directory.
+update_dir=$1 ; work_dir=$2 ;csv_dir=$3 ; cur_dir=$(pwd)
+process_start=`date +%s`
+# Determine files for the update, copy the good ones to the local directory for processing
+echo ***Getting update files...
+ls $update_dir | grep tar > complete_filelist_ug.txt
+grep -Fxvf finished_filelist.txt complete_filelist_ug.txt > remain_filelist.txt
+cat remain_filelist.txt | awk '{print "cp '$update_dir'/" $1 " '$cur_dir'"}' > cp_files.sh
+sh cp_files.sh
+
+# For each update file, parse, load, and update.
+for file in $(ls *.tar | sort -n)
+do
+  # Clear the work dir
+  cd $work_dir
+  rm -rf $work_dir/*
+  # Copy in the python codes
+  cp $cur_dir/*.py $work_dir
+  # For each file in update source dir, copy that file to the work dir
+  cp $cur_dir/$file $work_dir
+  cd $work_dir
+  # Unzip and prepare files for parsing.
+  echo ***Unzipping and renaming file: $file
+  tar -xvf $file *.xml* # extract *.xml.gz files
+  find . -name '*.xml.gz' -print0 | xargs -0 mv -t . # move them to current directory
+  subdir=$(echo "$file" | sed 's/.tar//g')
+  subdir=$(echo "$subdir" | sed 's/.*cxml/cxml/g')
+  echo 'substring for tar file is '$subdir
+  for f in *.xml.gz; do mv $f $subdir$f; done # rename *.xml.gz according to source .tar file
+  gunzip *.xml.gz # gunzip
+  # Prepare codes for parsing and loading xml files.
+  echo ***Preparing parsing and loading script for files from: $file
+  ls *.xml | grep -w xml | awk -v store_dir=$csv_dir/$subdir ' {split($1,filename,"."); if(NR%3!=0) print "echo Parsing " $1 "\n" "python derwent_xml_update_parser_parallel.py -filename " $1 " -csv_dir " store_dir "/ & \n";\
+else print "echo Parsing " $1 "\n" "python derwent_xml_update_parser_parallel.py -filename " $1 " -csv_dir " store_dir "/ & \n\nwait"; }' > parse_derwent_update.sh
+  # Parse xml files.
+  echo ***Parsing files from: $file
+  sh parse_derwent_update.sh
+  # Load xml files.
+  echo ***Parsing files from: $file
+  for file in $(find $csv_dir/$subdir -name *_load.sh); do
+    sh $file
+    wait
+  done
+done
+# Close out the script and log the times
+date
+process_finish=`date +%s`
+echo "TOTAL UPDATE DURATION:"
+echo $((process_finish-process_start)) | awk '{print  int($1/60)":"int($1%60) " : TOTAL PROCESS DURATION"}'
+# Print the log table to the screen
+psql -d ernie -c 'select * from update_log_derwent;'
