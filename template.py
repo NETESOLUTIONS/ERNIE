b83785fcbe8e73a1a5f9da163858fa0b44dffdb9
Dmitriy "DK" Korobskiy <dk@nete.com>
2017-11-30 16:44:54 -0500

    π

--- template.py
+++ template.py
@@ -1,6 +1,6 @@
-## This script will be used to generate xml data that will allow postgres data to be indexed in solr ##
-
 import sys
 
-for i in range(0,len(sys.argv)):
-    print sys.argv[i]
+i = 0
+for arg in sys.argv[0:]:
+    print "Argument #%d = %s" % (i, arg)
+    i += 1
