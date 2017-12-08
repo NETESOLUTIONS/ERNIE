b12b4ef02f3433b315f62360eaedcb9ce6fe586c
VJ Davey <akdavey@email.wm.edu>
2017-12-08 14:07:04 -0500

    Exporter smokeload files

---
+++ ExPORTER/Smokeload/column_extractor.py
@@ -0,0 +1,23 @@
+# column_extractor.py
+# this program is used to extract specific columns from the Reporter csv data
+# Date: 09/26/2017
+# Author: VJ Davey
+
+import pandas as pd
+import sys
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
+csv_file_path=str(sys.argv[1]); print 'Working with CSV file located at %s' %(csv_file_path)
+keep_headers = ['APPLICATION_ID','ACTIVITY','ADMINISTERING_IC','APPLICATION_TYPE','ARRA_FUNDED','AWARD_NOTICE_DATE','BUDGET_START',
+'BUDGET_END','CFDA_CODE','CORE_PROJECT_NUM','ED_INST_TYPE','FOA_NUMBER','FULL_PROJECT_NUM','SUBPROJECT_ID','FUNDING_ICs','FY','IC_NAME',
+'NIH_SPENDING_CATS','ORG_CITY','ORG_COUNTRY','ORG_DEPT','ORG_DISTRICT','ORG_DUNS','ORG_FIPS','ORG_NAME','ORG_STATE','ORG_ZIPCODE','PHR',
+'PI_IDS','PI_NAMEs','PROGRAM_OFFICER_NAME','PROJECT_START','PROJECT_END','PROJECT_TERMS','PROJECT_TITLE','SERIAL_NUMBER','STUDY_SECTION',
+'STUDY_SECTION_NAME','SUFFIX','SUPPORT_YEAR','TOTAL_COST','TOTAL_COST_SUB_PROJECT']
+df = pd.read_csv(csv_file_path, dtype=object, encoding='latin-1'); df = df[keep_headers]; 
+for header in keep_headers:
+    df[header] = df[header].str.replace(',','')
+    df[header] = df[header].str.encode('utf8')
+#df['PROJECT_TERMS'] = df['PROJECT_TERMS'].apply(lambda x: x.str[:18000])
+df.to_csv(csv_file_path[:-4]+'_EXTRACTED.csv', index=False)
+
---
+++ ExPORTER/Smokeload/reporter_smokeload.sh
@@ -0,0 +1,26 @@
+# reporter_smokeload.sh
+# This is a program to load all the RePORTER csv data into the ernie database
+# Date 09/26/2017
+
+link_files=$1
+proj_files=$2
+cur_dir=$3
+work_dir=$4
+echo 'STARTED'; date
+#create tables
+psql ernie -f create_reporter_tables.sql
+#unzip link_files and push into database
+cd $work_dir; for file in $(ls $link_files/*.zip); do unzip $file; done
+for file in $(ls $work_dir*.csv);do
+  echo 'Working on file' $file
+  psql ernie -c "copy reporter_publink from '$file' delimiter ',' CSV HEADER;" ; wait ;
+done; rm $work_dir*.csv
+#unzip proj_files, convert, and push into database
+cd $work_dir; for file in $(ls $proj_files/*.zip); do unzip $file; done
+for file in $(ls $work_dir*.csv);do
+  echo 'Working on file' $file
+  python $cur_dir/column_extractor.py $file; file_extract=$(echo $file | sed s/.csv/_EXTRACTED.csv/g)
+  psql ernie -c "copy reporter_projects from '$file_extract' delimiter ',' CSV HEADER;" ; wait ;
+done; rm $work_dir*.csv
+echo 'FINISHED'; date
+
