332bb60291b44874506338b139543e17ef06b74b
George Chacko <chackoge@gmail.com>
2018-01-18 22:47:38 +0000

    revised to ensure \copy commands are on a single line

--- Analysis/ipilimumab/ipilimumab_ct_table.sql
+++ Analysis/ipilimumab/ipilimumab_ct_table.sql
@@ -6,16 +6,14 @@ drop table if exists ipilimumab_ct;
 create table ipilimumab_ct as select nct_id, start_date, completion_date, SUBSTRING(completion_date 
 FROM '.{4}$') as year_of_completion from ct_clinical_studies 
 where nct_id in (select distinct nct_id from ct_interventions 
-where lower(intervention_name) like '%ipilimumab%' or lower(intervention_name) like '%yervoy%') 
-order by year_of_completion;
+where lower(intervention_name) like '%ipilimumab%' or lower(intervention_name) like '%yervoy%') order by year_of_completion;
 
 -- get pmids for ipilimumab_ct_reference_pmid file
-\copy (select pmid from ct_references where nct_id in (select nct_id from ipilimumab_ct) 
-order by pmid) to '/home/chackoge/ERNIE/Analysis/ipilimumab/ipilimumab_ct_reference_pmid';
+\copy (select pmid from ct_references where nct_id in (select nct_id from ipilimumab_ct) order by pmid) TO '/home/chackoge/ERNIE/Analysis/ipilimumab/ipilimumab_ct_reference_pmid';
 
 -- get pmids for ipilimumab_ct_publication_pmid file
-\copy (select pmid from ct_publications where nct_id in (select nct_id from ipilimumab_ct) 
-order by pmid) TO '/home/chackoge/ERNIE/Analysis/ipilimumab/ipilimumab_ct_publication_pmid';
+\copy (select pmid from ct_publications where nct_id in (select nct_id from ipilimumab_ct) order by pmid) TO '/home/chackoge/ERNIE/Analysis/ipilimumab/ipilimumab_ct_publication_pmid';
+
 
 
 
