6a8c4e999950f2afa1afc6f9cc65e9c0ab727ee2
George Chacko <chackoge@gmail.com>
2018-01-18 17:05:25 -0500

    revised to account for better inclusiveness with filter at the year of publication

--- Analysis/ipilimumab/ipilimumab_ct_table.sql
+++ Analysis/ipilimumab/ipilimumab_ct_table.sql
@@ -1,5 +1,6 @@
--- AUthor George Chacko 11/6/2017
--- build subtable setting cutoff as 2012 for ct data (best approximation)
+-- Author George Chacko 11/6/2017
+-- updated 1/18/2018 
+-- build subtable setting cutoff as 2011 for ct data (best approximation)
 
 drop table if exists ipilimumab_ct;
 create table ipilimumab_ct as select nct_id, start_date, completion_date, SUBSTRING(completion_date FROM '.{4}$') 
@@ -8,23 +9,12 @@ where nct_id in (select distinct nct_id from ct_interventions
 where lower(intervention_name) like '%ipilimumab%' or lower(intervention_name) like '%yervoy%') 
 order by year_of_completion;
 
--- get pmids for ipilimumab_ct_postapp_reference_pmid file
-\copy (select pmid from ct_references where nct_id in (select nct_id from ipilimumab_ct where year_of_completion::int > 2012 or year_of_completion is null) order by pmid) to '/home/chackoge/ERNIE/Analysis/ipilimumab/ipilimumab_ct_postapp_reference_pmid';
+-- get pmids for ipilimumab_ct_reference_pmid file
+\copy (select pmid from ct_references where nct_id in (select nct_id from ipilimumab_ct) order by pmid) to '/home/chackoge/ERNIE/Analysis/ipilimumab/ipilimumab_reference_pmid';
 
--- get pmids for ipilimumab_ct_postapp_publication_pmid file
-\copy (select pmid from ct_publications where nct_id in (select nct_id from ipilimumab_ct where year_of_completion::int > 2012 or year_of_completion is null) order by pmid) TO '/tmp/ipilimumab_ct_postapp_publication_pmid';
+-- get pmids for ipilimumab_ct_publication_pmid file
+\copy (select pmid from ct_publications where nct_id in (select nct_id from ipilimumab_ct) order by pmid) TO '/home/chackoge/ERNIE/Analysis/ipilimumab/ipilimumab_publication_pmid';
 
---get pmids for ipilimumab_ct_preapp_reference_pmid file
-\copy (select pmid from ct_references where nct_id in (select nct_id from ipilimumab_ct where year_of_completion::int <= 2012)  order by pmid) TO '~/ERNIE/Analysis/ipilimumab/pilimumab_ct_preapp_reference_pmid';
-
---get pmids for ipilimumab_ct_preapp_publication_pmid file
-\copy (select pmid from ct_publications where nct_id in (select nct_id from ipilimumab_ct where year_of_completion::int <= 2012)  order by pmid) TO '~/ERNIE/Analysis/ipilimumab/ipilimumab_ct_preapp_publication_pmid';
-
--- generate ipilimumab_ct_postapp_nct file
-\copy (select nct_id from ipilimumab_ct where year_of_completion::int > 2012 or year_of_completion is null) TO '~/ERNIE/Analysis/ipilimumab/ipilimumab_postapp_nct';
-
--- generate ipilimumab_ct_preapp_nct file
-\copy (select nct_id from ipilimumab_ct where year_of_completion::int <= 2012) TO '~/ERNIE/Analysis/ipilimumab/ipilimumab_preapp_nct';
 
 
 
