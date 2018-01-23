3effc2210e35e653731f8bd8589fc021bf5ac3fc
Dmitriy "DK" Korobskiy <dk@nete.com>
2018-01-23 16:14:08 -0500

    Fixing failing CT job * Minor

--- Trials/Automation/download_source_data.sh
+++ Trials/Automation/download_source_data.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -xe
+
 # download: directly from clinicaltrials.gov website
 wget --no-verbose 'https://clinicaltrials.gov/ct2/results/download?term=&down_fmt=xml&down_typ=results&down_stds=all' \
      --output-document=nct_files/CT_all.zip
