ac15283eb0b6ea94b490cc48e6f5a16842849ef7
George Chacko <chackoge@gmail.com>
2018-01-23 14:52:14 -0600

    update

---
+++ Analysis/ivacaftor/ipilimumab_ct_table.sql
@@ -0,0 +1,22 @@
+-- Author George Chacko 11/6/2017
+-- updated 1/18/2018 
+-- build subtable setting cutoff as 2011 for ct data (best approximation)
+
+drop table if exists ivacaftor_ct;
+create table ivacaftor_ct as select nct_id, start_date, completion_date, SUBSTRING(completion_date 
+FROM '.{4}$') as year_of_completion from ct_clinical_studies 
+where nct_id in (select distinct nct_id from ct_interventions 
+where lower(intervention_name) like '%ivacaftor%' or lower(intervention_name) like '%kalydeco%') order by year_of_completion;
+
+-- get pmids for ivacaftor_ct_reference_pmid file
+\copy (select pmid from ct_references where nct_id in (select nct_id from ivacaftor_ct) order by pmid) TO '/home/chackoge/ERNIE/Analysis/ivacaftor/ivacaftor_ct_reference_pmid';
+
+-- get pmids for ivacaftor_ct_publication_pmid file
+\copy (select pmid from ct_publications where nct_id in (select nct_id from ivacaftor_ct) order by pmid) TO '/home/chackoge/ERNIE/Analysis/ivacaftor/ivacaftor_ct_publication_pmid';
+
+
+
+
+
+
+
