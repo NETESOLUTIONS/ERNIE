6840f1359ca83c1e6b0e007ba73be872a0e28f85
George Chacko <chackoge@gmail.com>
2018-01-31 18:26:01 -0600

    simplified CT search for consistency with the others

--- Analysis/naltrexone/naltrexone_ct_table.sql
+++ Analysis/naltrexone/naltrexone_ct_table.sql
@@ -8,23 +8,17 @@ where nct_id in (select distinct nct_id from ct_interventions
 where lower(intervention_name) like '%naltrexone%' or lower(intervention_name) like '%revia%' or lower(intervention_name) 
 like '%vivitrol%') order by year_of_completion;
 
--- Get pmids for naltrexone_ct_postapp_reference_pmid file
-\copy (select pmid from ct_references where nct_id in (select nct_id from naltrexone_ct where year_of_completion::int > 1985 or year_of_completion is null) order by pmid) to '/home/chackoge/ERNIE/Analysis/naltrexone/naltrexone_ct_postapp_reference_pmid';
+-- Get pmids for naltrexone_ct_reference_pmid file
+\copy (select pmid from ct_references where nct_id in (select nct_id from naltrexone_ct) order by pmid) TO '/home/chackoge/ERNIE/Analysis/ivacaftor/ivacaftor_ct_reference_pmid';
 
--- get pmids for naltrexone_ct_postapp_publication_pmid file
-\copy (select pmid from ct_publications where nct_id in (select nct_id from naltrexone_ct where year_of_completion::int > 1985 or year_of_completion is null) order by pmid) TO '/tmp/naltrexone_ct_postapp_publication_pmid';
+-- get pmids for naltrexone_ct_publication_pmid file
+\copy (select pmid from ct_publications where nct_id in (select nct_id from naltrexone_ct) order by pmid) TO '/home/chackoge/ERNIE/Analysis/ivacaftor/ivacaftor_ct_publication_pmid';
 
---get pmids for naltrexone_ct_preapp_reference_pmid file
-\copy (select pmid from ct_references where nct_id in (select nct_id from naltrexone_ct where year_of_completion::int <= 1985)  order by pmid) TO '~/ERNIE/Analysis/naltrexone/pilimumab_ct_preapp_reference_pmid';
 
---get pmids for naltrexone_ct_preapp_publication_pmid file
-\copy (select pmid from ct_publications where nct_id in (select nct_id from naltrexone_ct where year_of_completion::int <= 1985)  order by pmid) TO '~/ERNIE/Analysis/naltrexone/naltrexone_ct_preapp_publication_pmid';
 
--- generate naltrexone_ct_postapp_nct file
-\copy (select nct_id from naltrexone_ct where year_of_completion::int > 1985 or year_of_completion is null) TO '~/ERNIE/Analysis/naltrexone/naltrexone_postapp_nct';
 
--- generate naltrexone_ct_preapp_nct file
-\copy (select nct_id from naltrexone_ct where year_of_completion::int <= 1985) TO '~/ERNIE/Analysis/naltrexone/naltrexone_preapp_nct';
+
+
 
 
 
