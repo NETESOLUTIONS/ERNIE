577f84c9ef6d229eafad250b1e40308fd9ba2b91
Dmitriy "DK" Korobskiy <dk@nete.com>
2018-02-06 13:23:44 -0500

    Updates in ad-hoc queries

--- Postgres/Users.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM pg_authid
-ORDER BY rolname;
-
-ALTER USER current_user WITH PASSWORD :'password';
---
+++ Postgres/users.sql
@@ -0,0 +1,47 @@
+-- Users = roles
+SELECT *
+FROM pg_authid
+ORDER BY rolname;
+
+-- Groups = roles without the login privilege
+SELECT *
+FROM pg_group;
+
+CREATE USER jenkins SUPERUSER;
+ALTER USER jenkins SUPERUSER;
+
+-- region Create non-superuser and grant all privileges
+CREATE USER jenkins;
+
+GRANT ALL ON SCHEMA public TO jenkins;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO jenkins;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO jenkins;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO jenkins;
+
+ALTER DEFAULT PRIVILEGES --
+-- All active users who can create public objects would grant theses privileges in the future
+FOR USER pardi_admin, avon, chackoge, dk --
+IN SCHEMA public --
+GRANT ALL ON TABLES TO jenkins;
+
+ALTER DEFAULT PRIVILEGES --
+-- All active users who can create public objects would grant theses privileges in the future
+FOR USER pardi_admin, avon, chackoge, dk --
+IN SCHEMA public --
+GRANT ALL ON SEQUENCES TO jenkins;
+
+ALTER DEFAULT PRIVILEGES --
+-- All active users who can create public objects would grant theses privileges in the future
+FOR USER pardi_admin, avon, chackoge, dk --
+IN SCHEMA public --
+GRANT ALL ON FUNCTIONS TO jenkins;
+
+ALTER DEFAULT PRIVILEGES --
+-- All active users who can create public objects would grant theses privileges in the future
+FOR USER pardi_admin, avon, chackoge, dk --
+IN SCHEMA public --
+GRANT ALL ON TYPES TO jenkins;
+-- endregion
+
+-- Set password
+ALTER USER current_user WITH PASSWORD :password;
