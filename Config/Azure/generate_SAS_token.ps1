c9c54f64cc088ac5a18c45ddf3a61921eb23bb34
Dmitriy "DK" Korobskiy <dk@nete.com>
2018-02-09 10:52:10 -0500

    Minor

--- Config/Azure/generate_SAS_token.ps1
+++ Config/Azure/generate_SAS_token.ps1
@@ -3,6 +3,7 @@ $storageAccountName = "erniediag759"
 Login-AzureRmAccount
 $storageAccount = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroup -Name $storageAccountName
 $ctx = $storageAccount.Context
-$accountSAS = New-AzureStorageAccountSASToken -Service Blob,File,Table,Queue -ResourceType Service,Container,Object -Permission "racwdlup" -Context $ctx
-echo "Generated SAS token:"
-echo $accountSAS
+$expiration = [System.DateTime]::Now.AddYears(100)
+$accountSAS = New-AzureStorageAccountSASToken -Service Blob,File,Table,Queue -ResourceType Service,Container,Object -Permission "racwdlup" -ExpiryTime $expiration -Context $ctx
+
+echo "Generated SAS token=``$accountSAS``, expiring at $expiration"
