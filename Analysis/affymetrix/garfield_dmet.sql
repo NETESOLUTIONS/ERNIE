4c192afdd61e09faeb673cf4625dc41520ed8d6a
George Chacko <chackoge@gmail.com>
2018-02-12 22:51:57 +0000

    edited file

--- Analysis/affymetrix/garfield_dmet.sql
+++ Analysis/affymetrix/garfield_dmet.sql
@@ -1,5 +1,6 @@
 -- script to connect Affymetrix DMET and Amplichip Data with garfield's h_graph
 
-DROP TABLE IF EXISTS garfield_hgraph
-CREATE TABLE garfield_hgraph(pmid int, wos_id varchar(30);
-COPY garfield_hgraph FROM '~/garfield_hgraph.csv' CSV HEADER DELIMITER ",':
+DROP TABLE IF EXISTS garfield_hgraph;
+CREATE TABLE garfield_hgraph(pmid int, wos_id varchar(30));
+\COPY garfield_hgraph FROM '~/garfield_hgraph.csv' CSV HEADER DELIMITER ',';
+
