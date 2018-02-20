7e233e5b043d9279c8fde5e4e0582e26b2cea2ab
George Chacko <chackoge@mail.nih.gov>
2018-02-20 11:28:20 -0500

    update

--- Analysis/affymetrix/garfield_import.sql
+++ Analysis/affymetrix/garfield_import.sql
@@ -1,4 +1,12 @@
--- revised simplified version of garfield trace data to DMET
+-- Script to generate csv files for import into neo4j
+-- This is the affymetrix case study tracing the affy seedset of <= 1991 to the DMET Plus
+-- Panel of 2017
+-- Author: George Chacko 2/20/2018
+
+-- End point is the garfield_hgraph series, which contains 23 wos_ids from Garfield's microarray historiograph
+-- Starting point is all papers identified in a keyword search in PubMed for DMET PLus
+-- Publications are connected/related by citation. The target is cited by the source.
+
 DROP TABLE IF EXISTS garfield_hgraph_end;
 CREATE TABLE garfield_hgraph_end AS
 SELECT source_id, publication_year 
@@ -9,7 +17,7 @@ publication_year <= 1992;
 DROP TABLE IF EXISTS garfield_gen1;
 CREATE TABLE garfield_gen1 AS
 SELECT source_id AS source, cited_source_uid AS target,
-'pub'::varchar(10) AS pub, 'endref'::varchar(10) AS ref
+'source'::varchar(10) AS stype, 'endref'::varchar(10) AS ttype
 FROM wos_references WHERE cited_source_uid IN
 (select source_id from garfield_hgraph_end);
 CREATE INDEX garfield_gen1_idx ON garfield_gen1(pub);
@@ -17,7 +25,7 @@ CREATE INDEX garfield_gen1_idx ON garfield_gen1(pub);
 DROP TABLE IF EXISTS garfield_gen2;
 CREATE TABLE garfield_gen2 AS
 SELECT source_id AS source, cited_source_uid AS target,
-'pub'::varchar(10) AS pub, 'ref'::varchar(10) AS ref
+'source'::varchar(10) AS stype, 'target'::varchar(10) AS ttype
 FROM wos_references WHERE cited_source_uid IN
 (select source from garfield_gen1);
 CREATE INDEX garfield_gen2_idx ON garfield_gen2(pub);
@@ -25,7 +33,7 @@ CREATE INDEX garfield_gen2_idx ON garfield_gen2(pub);
 DROP TABLE IF EXISTS garfield_dmet_begina;
 CREATE TABLE garfield_dmet_begina AS
 SELECT source_id AS source, cited_source_uid AS target,
-'startpub'::varchar(10) AS pub, 'ref'::varchar(10) AS ref
+'startref'::varchar(10) AS source, 'target'::varchar(10) AS ttype
 FROM wos_references WHERE source_id IN 
 (select wos_id from garfield_dmet3);
 CREATE INDEX garfield_dmet_begina_idx on garfield_dmet_begina(target);
@@ -37,34 +45,34 @@ wos_publications b ON a.target=b.source_id;
 
 DROP TABLE IF EXISTS garfield_node_assembly;
 CREATE TABLE  garfield_node_assembly(node_id varchar(16),
-node_name varchar(19),pub varchar(10),ref varchar(10));
+node_name varchar(19),stype varchar(10),ttype varchar(10));
 
 --build node_table
 --gen1
-INSERT INTO garfield_node_assembly(node_id,node_name,pub,ref) 
-SELECT 'n'||substring(source,5),source,pub,'none' 
-AS ref FROM garfield_gen1;
+INSERT INTO garfield_node_assembly(node_id,node_name,stype) 
+SELECT 'n'||substring(source,5),source,,stype
+FROM garfield_gen1;
 
-INSERT INTO garfield_node_assembly(node_id,node_name,pub,ref) 
-SELECT 'n'||substring(target,5),target,'none' 
-AS ref, ref FROM garfield_gen1;
+INSERT INTO garfield_node_assembly(node_id,node_name,ttype) 
+SELECT 'n'||substring(target,5),target,ttype
+FROM garfield_gen1;
 
 --gen2
-INSERT INTO garfield_node_assembly(node_id,node_name,pub,ref) 
-SELECT 'n'||substring(source,5),source,pub,'none' 
-AS ref FROM garfield_gen2;
+INSERT INTO garfield_node_assembly(node_id,node_name,stype) 
+SELECT 'n'||substring(source,5),source,stype
+FROM garfield_gen2;
 
-INSERT INTO garfield_node_assembly(node_id,node_name,pub,ref) 
-SELECT 'n'||substring(target,5),target,'none' 
-AS ref, ref FROM garfield_gen2;
+INSERT INTO garfield_node_assembly(node_id,node_name,pub,ttype) 
+SELECT 'n'||substring(target,5),target,ttype
+FROM garfield_gen2;
 
 --garfield_dmet_begin
-INSERT INTO garfield_node_assembly(node_id,node_name,pub,ref) 
-SELECT 'n'||substring(source,5),source,pub,'none' 
-AS ref FROM garfield_dmet_begin;
+INSERT INTO garfield_node_assembly(node_id,node_name,stype) 
+SELECT 'n'||substring(source,5),source,stype
+FROM garfield_dmet_begin;
 
-INSERT INTO garfield_node_assembly(node_id,node_name,pub,ref) 
-SELECT 'n'||substring(target,5),target,'none' 
+INSERT INTO garfield_node_assembly(node_id,node_name,ttype) 
+SELECT 'n'||substring(target,5),target,ttype
 AS ref, ref FROM garfield_dmet_begin;
 CREATE INDEX garfield_node_assembly_idx ON garfield_node_assembly(node_id);
 
