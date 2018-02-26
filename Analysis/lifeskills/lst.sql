9bf3e0b8d182af480c9b410a493f9fc427702afa
George Chacko <chackoge@gmail.com>
2018-02-26 14:26:51 -0600

    sql loading script

---
+++ Analysis/lifeskills/lst.sql
@@ -0,0 +1,13 @@
+-- script to assemble a nodelist and edgelist for a graph spanning the President's report on 
+-- combating addiction and the LST dataset.
+-- Author George Chacko 2/26/2018
+
+-- Assemble Trump report citations (Didi Cross and George Chacko)
+DROP TABLE IF EXISTS pcreport_start;
+CREATE TABLE pcreport_start (wos_id varchar(19));
+INSERT INTO pcreport_start (wos_id) '~/ERNIE/Analysis/lifeskills/trump_wosids.csv' CSV HEADER DELIMITER ',';
+
+
+
+
+
