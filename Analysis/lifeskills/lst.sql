a514d55e08b6085ba7cec4aedfd0ed23c974cca7
George Chacko <chackoge@gmail.com>
2018-02-27 11:22:15 -0600

    update

--- Analysis/lifeskills/lst.sql
+++ Analysis/lifeskills/lst.sql
@@ -7,6 +7,10 @@ DROP TABLE IF EXISTS pcreport_start;
 CREATE TABLE pcreport_start (wos_id varchar(19));
 INSERT INTO pcreport_start (wos_id) '~/ERNIE/Analysis/lifeskills/trump_wosids.csv' CSV HEADER DELIMITER ',';
 
+DROP TABLE IF EXIST sg_start;
+CREATE TABLE sg_start (wos_id varchar(19));
+INSERT INTO sg_start (wos_id) '~/ERNIE/Analysis/lifeskills/sg_80thp_wosid.csv' CSV HEADER DELIMITER ',';
+
 
 
 
