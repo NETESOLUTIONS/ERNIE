1ff4c823469d1686952fc19821f16a1e81eea1a2
Akshat05 <akshat.maltare@gmail.com>
2018-04-10 12:34:54 -0400

    New wos XML parser code

---
+++ Pubs/Automation/Updates_Processing/New_wos_xml_parser/reference.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+'''
+Function:   this is a class file for references to save its value in the form of an object
+
+
+USAGE:  	python reference.py -filename file_name -csv_dir csv_file_directory
+Author: 	Akshat Maltare
+Date:		03/24/2018
+Changes:
+'''
+
+class reference:
+    def __init__(self):
+        self.source_id=''
+        self.cited_source_id=None
+        self.cited_title=''
+        self.cited_work=''
+        self.cited_author=''
+        self.cited_year=''
+        self.cited_page=''
+        self.created_date=''
+        self.last_modified_date=''
+        self.cited_source_uid=None
+        self.id=None
