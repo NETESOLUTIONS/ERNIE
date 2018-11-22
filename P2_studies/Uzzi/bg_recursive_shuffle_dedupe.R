1ab5323afd6a56fb67446af301a6990811a399c4
George Chacko <chackoge@gmail.com>
2018-11-22 08:28:29 +0000

    minor edits

--- P2_studies/Uzzi/bg_recursive_shuffle_dedupe.R
+++ P2_studies/Uzzi/bg_recursive_shuffle_dedupe.R
@@ -3,15 +3,15 @@
 library(data.table); library(dplyr)
 rm(list=ls())
 # import source file (one year's worth of data- filename is hard coded in this script)
-sorted <- fread("data1980.csv")
+sorted <- fread('data1980.csv')
 
 # construct table for backjoining later
 
 refindex <- sorted %>% select(cited_source_uid,reference_issn,reference_year)
 refindex <- data.table(refindex)
-refindex <- unique(refindex,by=c("cited_source_uid","reference_issn","reference_year"))
+refindex <- unique(refindex,by=c('cited_source_uid','reference_issn','reference_year'))
 
-# loop to create 10 background models
+# loop to create background models
 for (i in 1:100) {
 S1 <- sorted %>% 
 select(source_id,source_year,o_cited_source_uid=cited_source_uid,o_refyear=reference_year,o_ref_issn=reference_issn) 
@@ -26,13 +26,15 @@ print(dim(s_delta))
 S2 <- S1[!S1$source_id %in% s_delta$source_id,]
 print(dim(S2))
 
+# only used during testing. Consistently returns zero so comment out
 # check to see if dupes are removed (should return zero rows)
 # S3 <- S2 %>% group_by(source_id,s_cited_source_uid) %>% filter(n()>1)
 # print(dim(S3))
 
 #back join, rename, and export
-S4 <- S2 %>% inner_join(refindex,by=c("s_cited_source_uid"="cited_source_uid"))
-colnames(S4) <- c("source_id","source_year","o_cited_source_uid","o_refyear","o_ref_issn","s_cited_source_uid","s_reference_issn","s_reference_year")
+S4 <- S2 %>% inner_join(refindex,by=c('s_cited_source_uid'='cited_source_uid'))
+colnames(S4) <- c('source_id','source_year','o_cited_source_uid','o_refyear','o_ref_issn','s_cited_source_uid','s_reference_issn','s_reference_year')
 print(dim(S4))
+# single quotes seem to work better in CentOS although this could be because of passaging through TextEdit.
 fwrite(S4,file=paste('bg_n_shuffled',i,'.csv',sep=''),row.names=FALSE)
 }
