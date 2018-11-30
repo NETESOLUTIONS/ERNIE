f2bb59636d7443c1d9d65629fe5faed88d233ae2
George Chacko <chackoge@gmail.com>
2018-11-30 15:37:03 +0000

    tweaked script for more parms'

---
+++ P2_studies/Uzzi/batch.R
@@ -0,0 +1,30 @@
+source('mcstats.R')
+
+mcmc_10_5 <- ucomparison('zscores1980_10_shuffle_5.csv',5,10,2880,4) 
+mcmc_1_5 <- ucomparison('zscores1980_1_shuffle_5.csv',5,1,360,3)
+perm_0_5 <- ucomparison('zscores1980_permute_5.csv',5,0,1,1) 
+swr_0_5 <- ucomparison('zscores1980_swr_5.csv',5,0,1,2) 
+
+mcmc_1_10 <- ucomparison('zscores1980_1_shuffle_10.csv',10,1,360,3)
+perm_0_10 <- ucomparison('zscores1980_permute_10.csv',10,0,1,1) 
+swr_0_10 <- ucomparison('zscores1980_swr_10.csv',10,0,1,2) 
+
+perm_0_100 <- ucomparison('zscores1980_permute_100.csv',100,0,1,1) 
+swr_0_100 <- ucomparison('zscores1980_swr_100.csv',100,0,1,2) 
+
+x <- inner_join(mcmc_10_5,mcmc_1_5,by='parm')
+colnames(x) <- c('parm','mcmc_10_5','mcmc_1_5')
+y <- inner_join(swr_0_5,perm_0_5,by='parm')
+colnames(y) <- c('parm','swr_0_5','perm_0_5')
+z <- inner_join(x,y,by='parm')
+
+x <- inner_join(mcmc_1_10,swr_0_10,by='parm')
+colnames(x) <- c('parm','mcmc_1_10','swr_0_10')
+x <-  inner_join(x,perm_0_10,by='parm')
+colnames(x) <- c('parm','mcmc_1_10','swr_0_10','perm_0_10')
+z <- inner_join(z,x,by='parm')
+
+x <- inner_join(swr_0_100,perm_0_100,by='parm')
+colnames(x) <- c('parm','swr_0_100','perm_0_100')
+summary_data <- inner_join(z,x,by='parm')
+
--- P2_studies/Uzzi/mcstats.R
+++ P2_studies/Uzzi/mcstats.R
@@ -8,33 +8,39 @@ library(data.table)
 df <- fread(df)
 
 rows <- c("method","reps","shuffles","runtime (min)",
-"output_rows","unique_pubs","unique_rp","unique_jp",
-"clean_jp","pcnt notInf", "min z_score","max_zscore","mean_zscore",
-"10th p'cntile zscore","median_zscore","Q1","Q3")	
+"output_rows","unique_pubs","% input_pubs","unique_rp",
+"unique_jp","clean_jp","pcnt notInf", "min z_score",
+"max_zscore","mean_zscore","10th p'cntile zscore","median_zscore",
+"Q1","Q3")	
+
 rundata <- numeric()
 
 rundata[1] <- method
 rundata[2] <- reps
 rundata[3] <- shuffles
 rundata[4] <- runtime
+
 rundata[5] <- dim(df)[1]
 rundata[6] <- length(unique(df$source_id))
-rundata[7] <- length(unique(df$wos_id_pairs))
-rundata[8] <- length(unique(df$journal_pairs))
+rundata[7] <- round(100*(rundata[6]/318682),2)
+rundata[8] <- length(unique(df$wos_id_pairs))
+
+
+rundata[9] <- length(unique(df$journal_pairs))
 
 df2 <- unique(df[,list(journal_pairs,frequency,z_score)])
 df3 <- df2[!(z_score == Inf | z_score == -Inf)]
-rundata[9] <- length(unique(df3$journal_pairs))
-print(rundata[8])
+rundata[10] <- length(unique(df3$journal_pairs))
 print(rundata[9])
-rundata[10] <- round(100*(rundata[9]/rundata[8]),1)
-rundata[11] <- min(df3$z_score)
-rundata[12] <- max(df3$z_score)
-rundata[13] <- mean(df3$z_score)
-rundata[14] <-quantile(df3$z_score,0.1)
-rundata[15] <-median(df3$z_score)
-rundata[16] <-quantile(df3$z_score,0.25)
-rundata[17] <-quantile(df3$z_score,0.75)
+print(rundata[10])
+rundata[11] <- round(100*(rundata[10]/rundata[9]),1)
+rundata[12] <- min(df3$z_score)
+rundata[13] <- max(df3$z_score)
+rundata[14] <- mean(df3$z_score)
+rundata[15] <-quantile(df3$z_score,0.1)
+rundata[16] <-median(df3$z_score)
+rundata[17] <-quantile(df3$z_score,0.25)
+rundata[18] <-quantile(df3$z_score,0.75)
 
 
 x <- data.frame(cbind(parm=rows,data=rundata),stringsAsFactors=FALSE)
