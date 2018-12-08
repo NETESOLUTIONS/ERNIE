4ba26304d4053de13b179a9625520665ab3d6c9b
George Chacko <chackoge@gmail.com>
2018-12-08 08:54:13 -0500

    added script to calculate frequencies

---
+++ P2_studies/Uzzi/frequencies.R
@@ -0,0 +1,18 @@
+# script to calculate probs for references
+system.time({
+
+	rm(list = ls())
+	library(data.table)
+	# read in data
+	t <- fread("dataset1980_dec2018.csv")
+	# subset to columns of interest
+	t1 <- t[, .(cited_source_uid, reference_year)]
+	setkey(t1, cited_source_uid)
+	# calculate frequencies of individual references
+	t1[, `:=`(f, .N), by = c("cited_source_uid", "reference_year")]
+	# calculate frequency of all references in a year
+	t1[, `:=`(F, sum(f)), by = "reference_year"]
+	# calculate probs for references
+	t1[, `:=`(p, f/F)]
+
+})
