7bd79be50402b600fb7e2fcf9d4792c1be36bc94
George Chacko <chackoge@gmail.com>
2018-12-08 12:03:29 -0500

    modified to suppress duplicate counts

--- P2_studies/Uzzi/frequencies.R
+++ P2_studies/Uzzi/frequencies.R
@@ -9,7 +9,9 @@ system.time({
 	t1 <- t[, .(cited_source_uid, reference_year)]
 	setkey(t1, cited_source_uid)
 	# calculate frequencies of individual references
-	t1[, `:=`(f, .N), by = c('cited_source_uid', 'reference_year')]
+	t1[, `:=`(f, .N), by = c('cited_source_uid')]
+	# suppress duplicate rows to avoid inflating counts by reference year
+	t1 <- unique(t1)
 	# calculate frequency of all references in a year
 	t1[, `:=`(F, sum(f)), by = 'reference_year']
 	# calculate probs for references
