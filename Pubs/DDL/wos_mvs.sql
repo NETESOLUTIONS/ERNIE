26dbc61331e22d579886e5b19a25f169100f7dd9
Dmitriy "DK" Korobskiy <dk@nete.com>
2018-12-20 18:33:11 -0500

    :construction: Make minor changes
    #ER-148 Use most frequently used WoS ISSNs in the Uzzi's workflow

--- Pubs/DDL/wos_mvs.sql
+++ Pubs/DDL/wos_mvs.sql
@@ -4,10 +4,10 @@
 -- DataGrip: start execution from here
 SET TIMEZONE = 'US/Eastern';
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS wos_issn_stats AS
+CREATE MATERIALIZED VIEW wos_issn_stats TABLESPACE wos_tbs AS
 SELECT CAST(document_id AS CHAR(9)) AS ssn, count(1) AS publication_count
 FROM wos_document_identifiers wdi
 WHERE document_id_type = 'issn'
 GROUP BY document_id;
 
-CREATE UNIQUE INDEX IF NOT EXISTS wis_ssn_uk ON wos_issn_stats(ssn) TABLESPACE index_tbs;
+CREATE UNIQUE INDEX wis_ssn_uk ON wos_issn_stats(ssn) TABLESPACE index_tbs;
