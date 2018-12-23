656f5dfee1d237b1357b35b415b0d6fa66efb354
Dmitriy "DK" Korobskiy <dk@nete.com>
2018-12-23 17:53:16 -0500

    Refactor SQL for Uzzi dataset generation

--- P2_studies/Uzzi/uz_ds.sql
+++ P2_studies/Uzzi/uz_ds.sql
@@ -17,102 +17,71 @@ SET TIMEZONE = 'US/Eastern';
 
 SELECT NOW();
 
-DROP TABLE IF EXISTS stg_uz_ds1;
+DROP TABLE IF EXISTS stg_uz_sources;
 
-CREATE TABLE stg_uz_ds1 AS
-SELECT source_id, publication_year AS source_year
-FROM wos_publications
-WHERE publication_year::INT = :year AND document_type = 'Article';
-
-CREATE INDEX stg_uz_ds1_idx ON stg_uz_ds1(source_id);
-
--- join to get cited references. Expect to lose some data since not all 
--- pubs will have references that meet the WHERE conditions in this query
-
-DROP TABLE IF EXISTS stg_uz_ds2;
-
-CREATE TABLE stg_uz_ds2 AS
-SELECT a.*, b.cited_source_uid
-FROM stg_uz_ds1 a
-JOIN wos_references b ON a.source_id = b.source_id
-WHERE substring(b.cited_source_uid, 1, 4) = 'WOS:' AND length(b.cited_source_uid) = 19;
-
-CREATE INDEX stg_uz_ds2_idx ON stg_uz_ds2(source_id, cited_source_uid);
-
--- inner join on wos_publications to get pub year of references
-DROP TABLE IF EXISTS stg_uz_ds3;
-
-CREATE TABLE stg_uz_ds3 AS
-SELECT a.*, b.publication_year AS reference_year
-FROM stg_uz_ds2 a
-JOIN wos_publications b ON a.cited_source_uid = b.source_id;
-
-CREATE INDEX stg_uz_ds3_idx ON stg_uz_ds3(source_id, cited_source_uid, reference_year);
-
--- add issns for refs
---add issns for source_id
-DROP TABLE IF EXISTS stg_uz_ds5;
-
-CREATE TABLE stg_uz_ds5 AS
+CREATE TABLE stg_uz_sources TABLESPACE temp_tbs AS
 SELECT
-  a.source_id,
-  a.source_year,
-  CAST('issn' AS VARCHAR(30)) AS source_document_id_type,
-  (
-    SELECT wdi.document_id
-    FROM wos_document_identifiers wdi
-    JOIN wos_issn_stats wis ON wis.issn = wdi.document_id
-    WHERE wdi.document_id_type = 'issn' AND wdi.source_id = a.source_id
-    ORDER BY wis.publication_count DESC, wdi.document_id
-    LIMIT 1
-  ) AS source_issn,
-  a.cited_source_uid,
-  a.reference_year,
-  CAST('issn' AS VARCHAR(30)) AS reference_document_id_type,
-  (
-    SELECT wdi.document_id
-    FROM wos_document_identifiers wdi
-    JOIN wos_issn_stats wis ON wis.issn = wdi.document_id
-    WHERE wdi.document_id_type = 'issn' AND wdi.source_id = a.cited_source_uid
-    ORDER BY wis.publication_count DESC, wdi.document_id
-    LIMIT 1
-  ) AS reference_issn
-FROM stg_uz_ds3 a;
+  source_id,
+  publication_year::INT AS source_year,
+  document_id_type AS source_document_id_type,
+  document_id AS source_issn
+FROM (
+  SELECT
+    wp.source_id,
+    wp.publication_year,
+    wdi.document_id_type,
+    wdi.document_id,
+    row_number()
+      OVER (PARTITION BY wp.source_id ORDER BY wdi.document_id_type DESC, wis.publication_count DESC, wdi.document_id)--
+      AS rank
+  FROM wos_publications wp
+  JOIN wos_document_identifiers wdi ON wdi.source_id = wp.source_id AND wdi.document_id_type IN ('issn', 'eissn')
+  JOIN wos_doc_id_stats wis ON wis.document_id_type = wdi.document_id_type AND wis.document_id = wdi.document_id
+  WHERE wp.publication_year::INT = :year AND wp.document_type = 'Article'
+) sq
+WHERE rank = 1;
 
-CREATE INDEX stg_uz_ds5_idx ON stg_uz_ds5(source_id, cited_source_uid, reference_year, reference_issn, source_issn,
-                                  source_year);
+ALTER TABLE stg_uz_sources ADD CONSTRAINT stg_uz_sources_pk PRIMARY KEY (source_id) USING INDEX TABLESPACE index_tbs;
 
--- select distinct 
-DROP TABLE IF EXISTS stg_uz_ds;
-
-CREATE TABLE stg_uz_ds AS
-SELECT DISTINCT *
-FROM stg_uz_ds5;
-
-CREATE INDEX stg_uz_ds_idx ON stg_uz_ds(source_id, cited_source_uid, source_issn, reference_issn);
-
--- ensure that ref pubs year is not greater that source_id pubyear
 DROP TABLE IF EXISTS :dataset_name;
 
--- cleans out references published after pub_year (this is by convention)<<<<<<< HEAD
 CREATE TABLE :dataset_name TABLESPACE p2_studies AS
-SELECT *
-FROM stg_uz_ds
-WHERE reference_year::INT <= :year;
+  -- The records should be DISTINCT on source_id + cited_source_uid
+SELECT
+  source_id,
+  source_year,
+  source_document_id_type,
+  source_issn,
+  cited_source_uid,
+  reference_year,
+  document_id_type AS reference_document_id_type,
+  document_id AS reference_issn
+FROM (
+  SELECT
+    sus.*,
+    wr.cited_source_uid,
+    wp.publication_year AS reference_year,
+    wdi.document_id_type,
+    wdi.document_id,
+    row_number()
+      OVER (PARTITION BY wp.source_id ORDER BY wdi.document_id_type DESC, wis.publication_count DESC, wdi.document_id)--
+      AS rank
+    -- reference_issn
+  FROM stg_uz_sources sus
+  JOIN wos_references wr ON wr.source_id = sus.source_id
+    -- Checks below are redundant since we're joining to wos_publications
+    -- AND substring(wr.cited_source_uid, 1, 4) = 'WOS:'
+    -- AND length(wr.cited_source_uid) = 19
+    -- ensure that ref pubs year is not greater that source_id pubyear
+  JOIN wos_publications wp ON wp.source_id = wr.cited_source_uid AND wp.publication_year::INT <= sus.source_year
+  JOIN wos_document_identifiers wdi ON wdi.source_id = wp.source_id AND wdi.document_id_type IN ('issn', 'eissn')
+  JOIN wos_doc_id_stats wis ON wis.document_id_type = wdi.document_id_type AND wis.document_id = wdi.document_id
+) sq
+WHERE rank = 1;
 
 ALTER TABLE :dataset_name SET SCHEMA public;
 
 -- clean up
--- DROP TABLE stg_uz_ds1;
--- DROP TABLE stg_uz_ds2;
--- DROP TABLE stg_uz_ds3;
--- DROP TABLE stg_uz_ds4;
--- DROP TABLE stg_uz_ds5;
--- DROP TABLE stg_uz_ds;
+-- DROP TABLE stg_uz_sources;
 
 SELECT NOW();
-
-
-
-
-
--- Pubs/DDL/wos_mvs.sql
+++ Pubs/DDL/wos_mvs.sql
@@ -4,10 +4,13 @@
 -- DataGrip: start execution from here
 SET TIMEZONE = 'US/Eastern';
 
-CREATE MATERIALIZED VIEW wos_issn_stats TABLESPACE wos_tbs AS
-SELECT CAST(document_id AS CHAR(9)) AS issn, count(1) AS publication_count
-FROM wos_document_identifiers wdi
-WHERE document_id_type = 'issn'
-GROUP BY document_id;
+DROP MATERIALIZED VIEW IF EXISTS wos_doc_id_stats;
 
-CREATE UNIQUE INDEX wis_issn_uk ON wos_issn_stats(issn) TABLESPACE index_tbs;
+CREATE MATERIALIZED VIEW wos_doc_id_stats TABLESPACE wos_tbs AS
+SELECT document_id_type, document_id, count(1) AS publication_count
+FROM wos_document_identifiers wdi
+WHERE document_id_type IN ('issn', 'eissn')
+GROUP BY document_id_type, document_id;
+-- 6m:48s
+
+CREATE UNIQUE INDEX wdis_document_id_uk ON wos_doc_id_stats(document_id_type, document_id) TABLESPACE index_tbs;
