97a65f38983940210751329bd93ede6e573d05ce
VJ Davey <akdavey@email.wm.edu>
2019-02-07 15:22:42 -0500

    spark analysis updates

--- P2_studies/Uzzi/Spark/spark_analysis.sh
+++ P2_studies/Uzzi/Spark/spark_analysis.sh
@@ -22,6 +22,8 @@ ENVIRONMENT
   * POSTGRES_PASSWORD                           a PostgreSQL password for the target server
   * POSTGRES_HOSTNAME                           the IP address of the server hosting the data
   * POSTGRES_SCHEMA                             the schema to execute code against
+  * TARGET_DATASET                              the target dataset to evaluate
+  * NUM_PERMUTATIONS                            num of permutations to run
 HEREDOC
   exit 1
 fi
@@ -35,27 +37,29 @@ hdfs dfs -rm -r -f /user/spark/data/*
 
 # Next, import the target table from the PostgreSQL database
 echo "*** SPARK IMPORT STARTED : $(date) ***"
-echo "*** LOADING dataset1980 ***"
+echo "*** LOADING ${TARGET_DATASET} ***"
 sqoop import --verbose --connect jdbc:postgresql://${POSTGRES_HOSTNAME}/${POSTGRES_DATABASE} \
---username ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} -m 1 --table dataset1980 \
+--username ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} -m 1 --table ${TARGET_DATASET} \
 --columns source_id,source_year,source_document_id_type,source_issn,cited_source_uid,reference_year,reference_document_id_type,reference_issn \
 --map-column-java source_id=String,source_year=String,source_document_id_type=String,\
 source_issn=String,cited_source_uid=String,reference_year=Integer,\
 reference_document_id_type=String,reference_issn=String \
---warehouse-dir=/hive/warehouse --hive-import --hive-table dataset1980 -- --schema ${POSTGRES_SCHEMA}
+--warehouse-dir=/hive/warehouse --hive-import --hive-table ${TARGET_DATASET} -- --schema ${POSTGRES_SCHEMA}
 echo "*** SPARK IMPORT COMPLETED : $(date) ***"
 
-# Then, import the target table's shuffled version from the PostgreSQL database
-echo "*** SPARK IMPORT STARTED : $(date) ***"
-echo "*** LOADING dataset1980_shuffled ***"
-sqoop import --verbose --connect jdbc:postgresql://${POSTGRES_HOSTNAME}/${POSTGRES_DATABASE} \
---username ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} -m 1 --table dataset1980_shuffled \
---columns source_id,source_year,source_document_id_type,source_issn,shuffled_cited_source_uid,shuffled_reference_year,shuffled_reference_document_id_type,shuffled_reference_issn \
---map-column-java source_id=String,source_year=String,source_document_id_type=String,\
-source_issn=String,shuffled_cited_source_uid=String,shuffled_reference_year=Integer,\
-shuffled_reference_document_id_type=String,shuffled_reference_issn=String \
---warehouse-dir=/hive/warehouse --hive-import --hive-table dataset1980_shuffled -- --schema ${POSTGRES_SCHEMA}
-echo "*** SPARK IMPORT COMPLETED : $(date) ***"
+for ((i=0; i<$NUM_PERMUTATIONS; i++)); do
+  # Then, import the target table's shuffled version from the PostgreSQL database
+  echo "*** SPARK IMPORT STARTED : $(date) ***"
+  echo "*** LOADING ${TARGET_DATASET}_shuffled ***"
+  sqoop import --verbose --connect jdbc:postgresql://${POSTGRES_HOSTNAME}/${POSTGRES_DATABASE} \
+  --username ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} -m 1 --table ${TARGET_DATASET}_shuffled \
+  --columns source_id,source_year,source_document_id_type,source_issn,shuffled_cited_source_uid,shuffled_reference_year,shuffled_reference_document_id_type,shuffled_reference_issn \
+  --map-column-java source_id=String,source_year=String,source_document_id_type=String,\
+  source_issn=String,shuffled_cited_source_uid=String,shuffled_reference_year=Integer,\
+  shuffled_reference_document_id_type=String,shuffled_reference_issn=String \
+  --warehouse-dir=/hive/warehouse --hive-import --hive-table ${TARGET_DATASET}_shuffled -- --schema ${POSTGRES_SCHEMA}
+  echo "*** SPARK IMPORT COMPLETED : $(date) ***"
 
-# After data is imported, submit a job to pyspark that will make use of the imported tables
-#$SPARK_HOME/bin/spark-submit --driver-memory 8g  ./permute_in_spark.py
+  # After data is imported, submit a job to pyspark that will make use of the imported tables
+  #$SPARK_HOME/bin/spark-submit --driver-memory 8g  ./permute_in_spark.py
+done
