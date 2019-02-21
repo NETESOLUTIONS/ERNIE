31e1efdeb18e82edee0434c0b2db266f9364e4ce
Siyu <syliu0519@gmail.com>
2019-02-21 17:18:47 -0500

    Update hardening scripts

---
+++ Security/hardening/enable_sysv_service.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+########################################
+# enable system service
+# Arguments:
+#   $1  service name
+# Returns:
+#   Nonea
+# Examples:
+#   enable_sysv_service chargen-dgram
+########################################
+
+enable_sysv_service() {
+  echo "___CHECK___"
+  # If the service is not present, the check fails
+  output=$(systemctl list-unit-files | grep -w $1.service || echo "")
+  if [[ ("${output}") && ("$(systemctl is-enabled $1.service)" != "enabled") ]]; then
+    echo "Check FAILED, correcting ..."
+    echo "___SET___"
+    systemctl enable $1.service
+  elif [[ ! "${output}" ]]; then
+    echo "No service found, please install"
+    exit 1
+  else
+    echo "Check PASSED"
+  fi
+  printf "\n\n"
+}
