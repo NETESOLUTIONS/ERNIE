038dd7496b3c14a635ce389422ba65f14ab8df7b
Dmitriy "DK" Korobskiy <dk@nete.com>
2019-03-10 17:00:41 -0400

    Implement parsing in xmltable()
    WIP: #ER-181 Evaluate option to parse Scopus XML data into PostgreSQL 11

--- Scopus/Postgres/DDL/scopus_ddl.sql
+++ Scopus/Postgres/DDL/scopus_ddl.sql
@@ -4,7 +4,7 @@
 -- DataGrip: start execution from here
 SET TIMEZONE = 'US/Eastern';
 
-DROP TABLE IF EXISTS scopus_publications;
+DROP TABLE IF EXISTS scopus_publications CASCADE;
 
 CREATE TABLE scopus_publication_groups (
   sgr BIGINT,
@@ -13,7 +13,7 @@ CREATE TABLE scopus_publication_groups (
   CONSTRAINT scopus_publication_groups_pk PRIMARY KEY (sgr) USING INDEX TABLESPACE index_tbs
 ) TABLESPACE scopus_tbs;
 
-DROP TABLE IF EXISTS scopus_publications;
+DROP TABLE IF EXISTS scopus_publications CASCADE;
 
 CREATE TABLE scopus_publications (
   scp BIGINT
@@ -23,15 +23,36 @@ CREATE TABLE scopus_publications (
   language_code CHAR(3),
   citation_title TEXT,
   title_lang_code CHAR(3),
-  abstract TEXT,
-  abstract_lang_code CHAR(3)
+--   abstract TEXT,
+--   abstract_lang_code CHAR(3),
+  correspondence_person_indexed_name TEXT,
+  correspondence_org TEXT,
+  correspondence_city TEXT,
+  correspondence_country TEXT,
+  correspondence_e_address TEXT
 ) TABLESPACE scopus_tbs;
 
-DROP TABLE IF EXISTS scopus_references sr;
+DROP TABLE IF EXISTS scopus_pub_authors CASCADE;
+
+CREATE TABLE scopus_pub_authors (
+  scp BIGINT
+    CONSTRAINT spa_source_scp_fk REFERENCES scopus_publications ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  author_seq SMALLINT,
+  auid BIGINT,
+  author_indexed_name TEXT,
+  author_surname TEXT,
+  author_given_name TEXT,
+  author_initials TEXT,
+  author_e_address TEXT,
+  CONSTRAINT scopus_pub_authors_pk PRIMARY KEY (scp, author_seq) USING INDEX TABLESPACE index_tbs
+) TABLESPACE scopus_tbs;
+
+DROP TABLE IF EXISTS scopus_references CASCADE;
 
 CREATE TABLE scopus_references (
-  source_scp BIGINT,
-  ref_sgr BIGINT
-    CONSTRAINT sr_ref_sgr_fk REFERENCES scopus_publication_groups ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
-  CONSTRAINT scopus_references_pk PRIMARY KEY (source_scp, ref_sgr) USING INDEX TABLESPACE index_tbs
+scp BIGINT
+CONSTRAINT sr_source_scp_fk REFERENCES scopus_publications ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+ref_sgr BIGINT
+CONSTRAINT sr_ref_sgr_fk REFERENCES scopus_publication_groups ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+CONSTRAINT scopus_references_pk PRIMARY KEY (scp, ref_sgr) USING INDEX TABLESPACE index_tbs
 ) TABLESPACE scopus_tbs;
---
+++ Scopus/Postgres/parsing_queries.sql
@@ -0,0 +1,108 @@
+\set ON_ERROR_STOP on
+\set ECHO all
+
+-- DataGrip: start execution from here
+SET TIMEZONE = 'US/Eastern';
+
+-- scopus_publication_groups, scopus_publications attributes
+SELECT
+  sgr,
+  pub_year,
+  make_date(pub_year, pub_month, pub_day) AS pub_date,
+  scp,
+  language_code,
+  citation_title,
+  title_lang_code,
+  abstract_lang_code,
+  correspondence_person_indexed_name,
+  correspondence_city,
+  correspondence_country,
+  correspondence_e_address
+FROM xmltable(--
+-- The `xml:` namespace doesn’t need to be specified
+  XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce), --
+  '//bibrecord' PASSING :scopus_doc COLUMNS --
+  --@formatter:off
+  -- region scopus_publication_groups
+  sgr BIGINT PATH 'item-info/itemidlist/itemid[@idtype="SGR"]',
+  pub_year SMALLINT PATH 'head/source/publicationyear/@first',
+  pub_month SMALLINT PATH 'head/source/publicationdate/month',
+  pub_day SMALLINT PATH 'head/source/publicationdate/day',
+  -- endregion
+
+  -- region scopus_publications
+  scp BIGINT PATH 'item-info/itemidlist/itemid[@idtype="SCP"]',
+  language_code CHAR(3) PATH 'head/citation-info/citation-language/@xml:lang',
+  citation_title TEXT PATH
+    'head/citation-title/titletext[@xml:lang="eng"] | head/citation-title/titletext[@original="y"]',
+  title_lang_code CHAR(3) PATH
+    'head/citation-title/titletext[@xml:lang="eng"]/@xml:lang | head/citation-title/titletext[@original="y"]/@xml:lang',
+  abstract_lang_code CHAR(3) PATH
+    'head/abstracts/abstract[@xml:lang="eng"]/@xml:lang | head/abstracts/abstract[@original="y"]/@xml:lang',
+  correspondence_person_indexed_name TEXT PATH 'head/correspondence/person/ce:indexed-name',
+  correspondence_city TEXT PATH 'head/correspondence/affiliation/city',
+  correspondence_country TEXT PATH 'head/correspondence/affiliation/country',
+  correspondence_e_address TEXT PATH 'head/correspondence/ce:e-address'
+  -- endregion
+  --@formatter:on
+  );
+
+-- scopus_publications: concatenated correspondence organizations
+SELECT scp, string_agg(organization, chr(10)) AS correspondence_orgs
+FROM xmltable(--
+  XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce), --
+  '//bibrecord/head/correspondence/affiliation/organization' PASSING :scopus_doc COLUMNS --
+  --@formatter:off
+  scp BIGINT PATH '../../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
+  organization TEXT PATH 'normalize-space()'
+  --@formatter:on
+  )
+GROUP BY scp;
+
+-- scopus_publications: concatenated abstracts
+/*
+TODO Report. Despite what docs say, the text contents of child elements are *not* concatenated to the result
+E.g. abstract TEXT PATH 'head/abstracts/abstract[@xml:lang="eng"]'
+*/
+SELECT scp, string_agg(abstract, chr(10) || chr(10)) AS abstract
+FROM xmltable(--
+-- The `xml:` namespace doesn’t need to be specified
+  XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce), --
+  --@formatter:off
+  '//bibrecord/head/abstracts/abstract[@xml:lang="eng"]/ce:para'
+'//bibrecord/head/abstracts/abstract[@original="y"]/ce:para'
+  PASSING :scopus_doc --
+  COLUMNS --
+
+  scp BIGINT PATH '../../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
+  abstract TEXT PATH 'normalize-space()'
+--@formatter:on
+  )
+GROUP BY scp;
+
+-- scopus_pub_authors
+SELECT scp, author_seq, auid, author_indexed_name, author_surname, author_given_name, author_initials, author_e_address
+FROM xmltable(--
+  XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce), --
+  '//bibrecord/head/author-group/author' PASSING :scopus_doc COLUMNS --
+  --@formatter:off
+  scp BIGINT PATH '../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
+  author_seq SMALLINT PATH '@seq',
+  auid BIGINT PATH '@auid',
+  author_indexed_name TEXT PATH 'ce:indexed-name',
+  author_surname TEXT PATH 'ce:surname',
+  author_given_name TEXT PATH 'ce:given-name',
+  author_initials TEXT PATH 'ce:initials',
+  author_e_address TEXT PATH 'ce:e-address'
+  --@formatter:on
+  );
+
+-- scopus_references
+SELECT scp, ref_sgr
+FROM xmltable(--
+  '//bibrecord/tail/bibliography/reference' PASSING :scopus_doc COLUMNS --
+  --@formatter:off
+  scp BIGINT PATH '../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
+  ref_sgr BIGINT PATH 'ref-info/refd-itemidlist/itemid[@idtype="SGR"]'
+  --@formatter:on
+  );
