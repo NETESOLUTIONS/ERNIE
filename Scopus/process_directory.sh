75eac62353043d06cf5376fc62a2774d8c21247b
VJ Davey <akdavey@email.wm.edu>
2019-04-05 12:23:54 -0400

    Update process_directory.sh

--- Scopus/process_directory.sh
+++ Scopus/process_directory.sh
@@ -139,7 +139,7 @@ for scopus_data_archive in *.zip; do
         [[ ${STOP_ON_THE_FIRST_ERROR} == "true" ]] && check_errors
     fi
     echo "hello world"
-    ((processed_xml_counter++))
+    ((++processed_xml_counter))
     echo "hello world"
     rm -rf "${subdir}"
   done
