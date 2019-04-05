b797a4bb27e6654940053660dbd30a23494b84ba
VJ Davey <akdavey@email.wm.edu>
2019-04-05 12:58:19 -0400

    Update process_directory.sh

--- Scopus/process_directory.sh
+++ Scopus/process_directory.sh
@@ -138,6 +138,10 @@ for scopus_data_archive in *.zip; do
         parallel ${PARALLEL_HALT_OPTION} --joblog ${PARALLEL_LOG} --line-buffer --tagstring '|job#{#} s#{%}|' parse_xml "{}"; then
         [[ ${STOP_ON_THE_FIRST_ERROR} == "true" ]] && check_errors
     fi
+    while read -r line;
+      do echo $line | grep "1" && ((++failed_xml_counter)) || ((++processed_xml_counter))
+    done < <(awk 'NR>1{print $7}' "${PARALLEL_LOG}")
+    > "${PARALLEL_LOG}"
     rm -rf "${subdir}"
   done
 
