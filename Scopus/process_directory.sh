e3f6e968d7a37be905e35ebda509ccd6dce2bbb7
VJ Davey <akdavey@email.wm.edu>
2019-04-05 11:21:03 -0400

    Update process_directory.sh

--- Scopus/process_directory.sh
+++ Scopus/process_directory.sh
@@ -141,7 +141,6 @@ for scopus_data_archive in *.zip; do
     if ! find "${subdir}" -name '2*.xml' | \
         parallel ${PARALLEL_HALT_OPTION} --line-buffer --tagstring '|job#{#} s#{%}|' parse_xml "{}"; then
       [[ ${STOP_ON_THE_FIRST_ERROR} == "true" ]] && check_errors
-      if [[ $? != 0 ]]; then ((failed_xml_counter++)) ; fi
     fi
     rm -rf "${subdir}"
   done
