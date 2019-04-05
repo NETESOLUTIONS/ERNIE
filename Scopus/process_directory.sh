c90f60cd979e0c87086f0fe673d06687256e0724
VJ Davey <akdavey@email.wm.edu>
2019-04-05 12:03:12 -0400

    Update process_directory.sh

--- Scopus/process_directory.sh
+++ Scopus/process_directory.sh
@@ -138,12 +138,8 @@ for scopus_data_archive in *.zip; do
     # --joblog "${PARALLEL_JOB_LOG}"
     if ! find "${subdir}" -name '2*.xml' | \
         parallel ${PARALLEL_HALT_OPTION} --line-buffer --tagstring '|job#{#} s#{%}|' parse_xml "{}"; then
-        ((processed_xml_counter++))
         [[ ${STOP_ON_THE_FIRST_ERROR} == "true" ]] && check_errors
-    else
-      ((failed_xml_counter++))
     fi
-    check_errors
     rm -rf "${subdir}"
   done
 
@@ -152,16 +148,16 @@ for scopus_data_archive in *.zip; do
   echo "NUMBER OF XML FILES WHICH FAILED PARSING: ${failed_xml_counter}"
 
   stop_time=$(date '+%s')
-  ((delta=stop_time - start_time)) || :
-  ((delta_s=delta % 60)) || :
-  ((delta_m=(delta / 60) % 60)) || :
-  ((della_h=delta / 3600)) || :
-  printf "\n$(TZ=America/New_York date) Done with ${scopus_data_archive} archive in %dh:%02dm:%02ds\n" ${della_h} \
-         ${delta_m} ${delta_s}
-  ((elapsed=elapsed + delta))
-  ((est_total=num_zips * elapsed / i)) || :
-  ((eta=process_start_time + est_total))
-  echo "ETA to complete current year: $(TZ=America/New_York date --date=@${eta})"
+  #((delta=stop_time - start_time)) || :
+  #((delta_s=delta % 60)) || :
+  #((delta_m=(delta / 60) % 60)) || :
+  #((della_h=delta / 3600)) || :
+  #printf "\n$(TZ=America/New_York date) Done with ${scopus_data_archive} archive in %dh:%02dm:%02ds\n" ${della_h} \
+  #       ${delta_m} ${delta_s}
+  #((elapsed=elapsed + delta))
+  #((est_total=num_zips * elapsed / i)) || :
+  #((eta=process_start_time + est_total))
+  #echo "ETA to complete current year: $(TZ=America/New_York date --date=@${eta})"
   cd ..
 done
 
