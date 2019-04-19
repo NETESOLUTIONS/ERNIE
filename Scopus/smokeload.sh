ef0b8403f01ac546e550064e7cbbe78e7abb0511
Dmitriy "DK" Korobskiy <dk@nete.com>
2019-04-19 13:23:57 -0400

    :construction: Scopus smokeload: remove resume mode

--- Scopus/smokeload.sh
+++ Scopus/smokeload.sh
@@ -12,23 +12,21 @@ SYNOPSIS
 
 DESCRIPTION
 
-  The script processes specified directories in alphabetical or reverse order.
-  The process upserts data and automatically resumes on failures.
+  Process specified directories in alphabetical or reverse order.
+  data_directory could be an absolute or relative to the working directory location.
 
-  If you need to stop process gracefully after the current directory is processed, create `./.stop` file.
+  To stop process gracefully after the data_directory is processed, create a `{data_directory}/.stop` signal file.
 
   The following options are available:
 
-    -c    clean data before processing and don't resume processing. WARNING: be aware that you'll lose all loaded data!
+    -c    clean data and previous failures before processing
+    WARNING: be extra cautious! You'll lose all loaded data.
+
     -r    reverse order of processing
 
 EXAMPLES
 
-  Process 2000-20XX data directories
-
-      smokeload.sh 20??
-
-  Process all 20XX data directories in a reverse order
+  Process all 20XX data directories in a reverse order:
 
       smokeload.sh -r /erniedev_data1/Scopus/20??
 HEREDOC
@@ -39,17 +37,18 @@ set -e
 set -o pipefail
 #set -x
 
-STOP_FILE=".stop"
+readonly STOP_FILE=".stop"
 # Get a script directory, same as by $(dirname $0)
-script_dir=${0%/*}
-absolute_script_dir=$(cd "${script_dir}" && pwd)
-FAILED_FILES_DIR=../failed
+readonly SCRIPT_DIR=${0%/*}
+readonly ABSOLUTE_SCRIPT_DIR=$(cd "${SCRIPT_DIR}" && pwd)
+readonly FAILED_FILES_DIR=../failed
+
 while (( $# > 0 )); do
   case "$1" in
     -c)
-      clean_mode="-nr";;
+      readonly CLEAN_MODE="true";;
     -r)
-      sort_order="--reverse";;
+      readonly SORT_ORDER="--reverse";;
     *)
       break
   esac
@@ -58,13 +57,13 @@ done
 arg_array=( "$@" )
 echo "${arg_array[*]}"
 # Courtesy of https://stackoverflow.com/questions/7442417/how-to-sort-an-array-in-bash
-IFS=$'\n' sorted_args=($(sort ${sort_order} <<<"${arg_array[*]}")); unset IFS
+IFS=$'\n' sorted_args=($(sort ${SORT_ORDER} <<<"${arg_array[*]}")); unset IFS
 
 echo -e "\n## Running under ${USER}@${HOSTNAME} in ${PWD} ##\n"
 echo -e "Data directories to process:\n${sorted_args[@]}"
 
-if [[ ${clean_mode} ]]; then
-  echo "In clean mode: truncating all data ..."
+if [[ ${CLEAN_MODE} == "true" ]]; then
+  echo "IN CLEAN MODE. TRUNCATING ALL DATA..."
   # language=PostgresPLSQL
   psql -v ON_ERROR_STOP=on --echo-all <<'HEREDOC'
     TRUNCATE scopus_publication_groups CASCADE;
@@ -72,45 +71,37 @@ if [[ ${clean_mode} ]]; then
     TRUNCATE scopus_conference_events CASCADE;
     TRUNCATE scopus_classification_lookup CASCADE;
 HEREDOC
-  rm -rf /erniedev_data1/Scopus/failed/*
+
+  cd ${sorted_args[0]}
+  rm -rf "${FAILED_FILES_DIR}"
 fi
 
-
 rm -f eta.log
-declare -i process_start_time directories i=0 start_time stop_time delta delta_s delta_m della_h elapsed=0 est_total eta
-directories=${#sorted_args[@]}
+declare -i directories=${#sorted_args[@]} i=0 start_time dir_start_time dir_stop_time delta delta_s delta_m della_h \
+    elapsed=0 est_total eta
 for DATA_DIR in "${sorted_args[@]}"; do
-  start_time=$(date '+%s')
+  dir_start_time=$(date '+%s')
+  (( i == 0 )) && start_time = ${dir_start_time}
   echo -e "\n## Directory #$((++i)) out of ${directories} ##"
-  if [[ -d "${DATA_DIR}/tmp"  && ! ${clean_mode} ]]; then
-    resume_mode=" (resumed)"
-    # Don't count partial directory
-    #((directories--))
-  else
-    unset resume_mode
-    process_start_time=${start_time}
-  fi
-  echo "Processing${resume_mode} ${DATA_DIR} directory ..."
-  # TODO: have option to pass c as option and specify clean mode
-  "${absolute_script_dir}/process_directory.sh" "${DATA_DIR}" "${FAILED_FILES_DIR}" || :
-  stop_time=$(date '+%s')
+  echo "Processing ${DATA_DIR} directory ..."
+  "${ABSOLUTE_SCRIPT_DIR}/process_directory.sh" "${DATA_DIR}" "${FAILED_FILES_DIR}" || :
+  dir_stop_time=$(date '+%s')
 
-  ((delta=stop_time - start_time + 1)) || :
+  ((delta=dir_stop_time - dir_start_time + 1)) || :
   ((delta_s=delta % 60)) || :
   ((delta_m=(delta / 60) % 60)) || :
   ((della_h=delta / 3600)) || :
-  printf "\n$(TZ=America/New_York date) Done with ${DATA_DIR} data directory in %dh:%02dm:%02ds${resume_mode}\n" ${della_h} \
+  printf "\n$(TZ=America/New_York date) Done with ${DATA_DIR} data directory in %dh:%02dm:%02ds\n" ${della_h} \
          ${delta_m} ${delta_s} | tee -a eta.log
   if [[ -f "${STOP_FILE}" ]]; then
     echo "Found the stop signal file. Gracefully stopping..."
     rm -f "${STOP_FILE}"
     break
   fi
-  if [[ ! ${resume_mode} ]]; then
-    echo "elapsed is : ${elapsed} | delta is : ${delta}"
-    ((elapsed=elapsed + delta))
-    ((est_total=directories * elapsed / i)) || :
-    ((eta=process_start_time + est_total))
-    echo "ETA after ${DATA_DIR} data directory: $(TZ=America/New_York date --date=@${eta})" | tee -a eta.log
-  fi
+
+#  echo "elapsed is : ${elapsed} | delta is : ${delta}"
+  ((elapsed=elapsed + delta))
+  ((est_total=elapsed * directories / i)) || :
+  ((eta=start_time + est_total))
+  echo "ETA after ${DATA_DIR} data directory: $(TZ=America/New_York date --date=@${eta})" | tee -a eta.log
 done
