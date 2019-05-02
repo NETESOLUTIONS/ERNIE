f4d50ec3a7cd99c2807d8a5d5de4d05c3fdd6bb9
VJ Davey <akdavey@email.wm.edu>
2019-05-02 12:46:39 -0400

    Update abt_more_requests.sql

--- Scopus/Abt_Analysis/Analysis_scripts/corrections_and_requests/abt_more_requests.sql
+++ Scopus/Abt_Analysis/Analysis_scripts/corrections_and_requests/abt_more_requests.sql
@@ -1,5 +1,10 @@
 /* SQL scripts for generating person and author ID level datasets to compare with and recreate portions of the SummaryStatistics XLSX file*/
 
+/* Create a table year_range if it does not exist. populate with year values that are usable for creation of a blank slate table in following queries*/
+CREATE TABLE year_range(year INT);
+INSERT INTO year_range VALUES (1995),(1996),(1997),(1998),(1999),(2000),(2001),(2002),(2003),(2004),(2005),(2006),(2007),(2008),(2009),(2010),(2011),(2012),(2013),(2014),(2015),(2016),(2017),(2018);
+
+
 /* CCI GROUP AUTHOR ID LEVEL w/o Base Table */
 COPY (
   WITH award_author_documents AS
