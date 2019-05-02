c678d668e090ee584b98f72d4db0e42a19e7ad24
VJ Davey <akdavey@email.wm.edu>
2019-05-02 13:43:49 -0400

    Update abt_more_requests.sql

--- Scopus/Abt_Analysis/Analysis_scripts/corrections_and_requests/abt_more_requests.sql
+++ Scopus/Abt_Analysis/Analysis_scripts/corrections_and_requests/abt_more_requests.sql
@@ -4,7 +4,6 @@
 CREATE TABLE year_range(year INT);
 INSERT INTO year_range VALUES (1995),(1996),(1997),(1998),(1999),(2000),(2001),(2002),(2003),(2004),(2005),(2006),(2007),(2008),(2009),(2010),(2011),(2012),(2013),(2014),(2015),(2016),(2017),(2018);
 
-
 /* CCI GROUP AUTHOR ID LEVEL w/o Base Table */
 COPY (
   WITH award_author_documents AS
@@ -647,4 +646,4 @@ COPY(
                    ORDER BY surname,given_name,award_number,first_year,publication_year
                 )
         SELECT * FROM calculations
-) TO '/tmp/1A_CCI_person_year_level_data_before_during_after.csv' CSV HEADER;
+) TO '/tmp/1A_COMP_person_year_level_data_before_during_after.csv' CSV HEADER;
