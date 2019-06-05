00b6d5e4e86a3bec9585bbe8ae9cf65580bbdf65
dl2774@columbia.edu <dl2774@columbia.edu>
2019-06-05 12:39:04 -0400

    fold change for allwos vrs. restricted and uzzi-shuffle vrs. nete-permutation

--- P2_studies/disciplinary_composition_analysis/Fc_calculation.sql
+++ P2_studies/disciplinary_composition_analysis/Fc_calculation.sql
@@ -6,12 +6,6 @@
  with the consequence that the sample distribution is different from the distribution sampled. In the following, George and I proceeded with the following
  steps:
 
- 1. Take a data set for a domain say arts and humanities
- 3. Shuffle the dataset in 1. with uzzi algorithm
- 4. Find common publications between original datset in 2. and shuffled in 3.
- 5. Now count the number of subjects wrt to the works cited by each publication
- 6. 5. but for shuffled dataset
-
  */
 
 
@@ -20,25 +14,20 @@
 \timing
 
 
-SET DEFAULT_TABLESPACE='p2_studies';
+SET DEFAULT_TABLESPACE=P2_studies;
 
--- calculate fc
-SELECT :
-'dataset1' AS dataset1,
-'dataset2' AS dataset2 ,
-'shuffled_dataset' AS shuffled_dataset
-FROM (
+-- Arts and Humanities
+
 create table t1 as (with cte as
 (
 select a.cited_source_uid,
        b.subject
-        from :dataset1
+        from :dataset1985_subject_subject
             a INNER JOIN wos_publication_subjects b on a.cited_source_uid=b.source_id
-        where a.source_id in ((select source_id from :dataset2)
-        intersect (select source_id from shuffled_dataset) and b.subject_classification_type='extended'
+        where a.source_id in  ((select source_id from :dataset1985_subject)
+        intersect (select source_id from :dataset1985_shuffle_subject)) and b.subject_classification_type='extended'
 )
 select subject,count(cited_source_uid) from cte group by subject order by subject);
-
 
 
 create table t2 as (with cte as
@@ -46,19 +35,28 @@ create table t2 as (with cte as
 select
        a.cited_source_uid,
        b.subject
-        from dataset1985_ah_shuffle
+        from :dataset1985_shuffle_subject
            a INNER JOIN wos_publication_subjects b on a.cited_source_uid=b.source_id
-            where a.source_id in  ((select source_id from dataset1985_ah)
-            intersect (select source_id from dataset1985_ah_shuffle)) and b.subject_classification_type='extended'
+            where a.source_id in  ((select source_id from :dataset1985_subject)
+            intersect (select source_id from :dataset1985_shuffle_subject)) and b.subject_classification_type='extended'
 )
 select subject,count(cited_source_uid) from cte group by subject order by subject);
 
 
 create table t3 as select a.subject as orig ,b.subject as shuffle ,a.count as orig_count,b.count as shuffle_count from t1 a FULL OUTER JOIN t2 b on a.subject=b.subject;
-alter table t3 add column merged_subjects varchar;
-update t3 set merged_subjects=orig;
-update t3 set merged_subjects=shuffle where merged_subjects is null;
+alter table t3 add column subject_of_citation varchar;
+update t3 set subject_of_citation=orig;
+update t3 set subject_of_citation=shuffle where subject_of_citation is null;
 
-create table t4 as select merged_subjects, orig_count, shuffle_count from t3;
-update t4 set orig_count=0 where orig_count is null;
-\copy (select * from t4) to '~/dataset1985_ah_shuffle_subject_analysis.csv' CSV HEADER DELIMITER ','; ))
+create table :t4 as select subject_of_citation, orig_count, shuffle_count from t3;
+update :t4 set orig_count=1 where orig_count is null;
+-- \copy (select * from :t4) to '~/dataset1985_ah_shuffle_subject_analysis.csv' CSV HEADER DELIMITER ',';--
+ALTER TABLE :t4 ADD COLUMN fc NUMERIC;
+UPDATE :t4 SET fc=0 WHERE fc IS NULL ;
+UPDATE :t4 SET shuffle_count= 1 WHERE shuffle_count IS NULL ;
+UPDATE :t4 SET fc=shuffle_count/orig_count WHERE shuffle_count > orig_count ;
+UPDATE :t4 SET fc=-orig_count/shuffle_count WHERE orig_count> shuffle_count;
+UPDATE :t4 SET fc=1 WHERE orig_count=shuffle_count;
+
+
+DROP TABLE IF EXISTS t1,t2,t3;
