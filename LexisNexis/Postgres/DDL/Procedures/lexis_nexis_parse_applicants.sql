e8f72af92b311178c0c941c3b60220aa073e5ac0
VJ Davey <akdavey@email.wm.edu>
2019-06-21 11:07:38 -0400

    Update lexis_nexis_parse_applicants.sql

--- LexisNexis/Postgres/DDL/Procedures/lexis_nexis_parse_applicants.sql
+++ LexisNexis/Postgres/DDL/Procedures/lexis_nexis_parse_applicants.sql
@@ -14,7 +14,8 @@ BEGIN
                                        organization_name, organization_type, organization_country, organization_state,
                                        organization_city, organization_address, registered_number,
                                        issuing_office)
-        SELECT xmltable.country_code,
+        SELECT DISTINCT
+        xmltable.country_code,
         xmltable.doc_number,
         xmltable.kind_code,
         xmltable.sequence,
@@ -28,15 +29,16 @@ BEGIN
         xmltable.organization_address,
         xmltable.registered_number,
         xmltable.issuing_office
-        FROM xmltable('//bibliographic-data/parties/applicants/applicant' PASSING input_xml COLUMNS
+        FROM xmltable('//bibliographic-data/parties/applicants/applicant[not(@data-format)]' PASSING input_xml COLUMNS
         country_code TEXT PATH '../../../publication-reference/document-id/country',
         doc_number TEXT PATH '../../../publication-reference/document-id/doc-number',
         kind_code TEXT PATH '../../../publication-reference/document-id/kind',
-        LANGUAGE TEXT PATH 'addressbook/@lang',
-        SEQUENCE SMALLINT PATH '@sequence',
+        language TEXT PATH 'addressbook/@lang',
+        sequence SMALLINT PATH '@sequence',
         designation TEXT PATH '@designation',
         organization_name TEXT PATH 'addressbook/orgname',
         organization_type TEXT PATH 'addressbook/orgname-standardized/@type',
+        organization_key INT PATH 'addressbook/orgname-normalized/@key',
         organization_country TEXT PATH 'addressbook/address/country',
         organization_state TEXT PATH 'addressbook/address/state',
         organization_city TEXT PATH 'addressbook/address/city',
