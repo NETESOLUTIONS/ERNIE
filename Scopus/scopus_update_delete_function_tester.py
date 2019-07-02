ccc5ccafedcd3723b4bd97e536a502cd76348299
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MBP.fios-router.home>
2019-07-02 10:35:48 -0400

    scopus automated update process

--- Scopus/scopus_update_delete_function_tester.py
+++ Scopus/scopus_update_delete_function_tester.py
@@ -36,8 +36,8 @@ def delete_function(data_directory):
     for file in os.listdir(data_directory):
             file_mtimeresult = os.stat(os.path.join(data_directory, file))
             file_mtimeresult = [file, (present_time - file_mtimeresult.st_mtime)]
-            if file_mtimeresult[1] < (840 * 3600):
-                print("The present files" + " " + str(file_mtimeresult[0]) + " " + "will be removed....")
+            if file_mtimeresult[1] > (840 * 3600):
+                print("The present file" + " " + str(file_mtimeresult[0]) + " " + "will be removed....")
                 #os.remove(os.path.join(directory, file))
                 #print("The present files " " " + str(file_mtimeresult[0]) + " "  "is removed!")
 
