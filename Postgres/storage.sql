05cbc48ab5f24ea6991e0d984a7ac08410802210
Dmitriy "DK" Korobskiy <dk@nete.com>
2019-07-03 18:45:50 -0400

    :construction: Ad-hoc queries

--- Postgres/storage.sql
+++ Postgres/storage.sql
@@ -7,7 +7,8 @@ SELECT pg_size_pretty(sum(pg_total_relation_size(pc.oid))) AS total_size
 FROM pg_class pc --
 JOIN pg_namespace pn ON pn.oid = pc.relnamespace AND pn.nspname = 'public' -- Tables, sequences and MVs occupy space.
   -- Indexes and TOAST tables are added automatically by pg_total_relation_size()
-WHERE pc.relkind IN('r', 'S', 'm');
+WHERE
+  pc.relkind IN ('r', 'S', 'm');
 -- 2974 GB
 
 -- All tablespaces
@@ -21,7 +22,8 @@ SELECT
   END AS location,
   pt.oid AS tablespace_oid
 FROM pg_tablespace pt, pg_settings ps
-WHERE ps.name = 'data_directory'
+WHERE
+  ps.name = 'data_directory'
 ORDER BY pg_tablespace_size(pt.spcname) DESC;
 
 /*
@@ -50,19 +52,24 @@ JOIN pg_database pd ON pd.datname = current_catalog
 JOIN pg_tablespace db_pt ON db_pt.oid = pd.dattablespace
 JOIN pg_namespace pn ON pn.oid = pc.relnamespace AND pn.nspname = 'public'
 JOIN pg_authid pa ON pa.oid = pc.relowner
-WHERE coalesce(obj_pt.spcname, db_pt.spcname) = :tablespace AND relname NOT LIKE 'pg_toast_%'
+WHERE
+  coalesce(obj_pt.spcname, db_pt.spcname) = :tablespace
+AND relname NOT LIKE 'pg_toast_%'
 ORDER BY pg_total_relation_size(pc.oid) DESC;
 
 -- Default tablespace parameter
 SHOW default_tablespace;
 
 -- Current DB's default tablespace
-SELECT datname AS db, pt.spcname AS db_default_tablespace,
+SELECT
+  datname AS db,
+  pt.spcname AS db_default_tablespace,
   --
-       pg_size_pretty(pg_tablespace_size(pt.spcname)) AS used_space
+  pg_size_pretty(pg_tablespace_size(pt.spcname)) AS used_space
 FROM pg_database pd
 JOIN pg_tablespace pt ON pt.oid = pd.dattablespace
-WHERE datname = current_catalog;
+WHERE
+  datname = current_catalog;
 
 SHOW temp_tablespaces;
 
@@ -115,31 +122,63 @@ JOIN pg_authid pa ON pa.oid = pc.relowner
 LEFT JOIN pg_tablespace obj_pt ON obj_pt.oid = pc.reltablespace
 JOIN pg_database pd ON pd.datname = current_catalog
 JOIN pg_tablespace db_pt ON db_pt.oid = pd.dattablespace
-WHERE pc.relname LIKE :name_pattern
+WHERE
+  pc.relname LIKE :name_pattern
 ORDER BY pg_total_relation_size(pc.oid) DESC;
 
--- Tablespace of relation(s) (data-containing objects) by name pattern
-SELECT pc.relname, --
-       pn.nspname AS schema, --
-       CASE pc.relkind -- By default, CASE will cast results as char (pc.relkind)
-         WHEN 'r' THEN CAST('table' AS TEXT)
-         WHEN 'i' THEN 'index'
-         WHEN 'S' THEN 'sequence'
-         WHEN 'v' THEN 'view'
-         WHEN 'm' THEN 'materialized view'
-         WHEN 'c' THEN 'composite type'
-         WHEN 't' THEN 'TOAST table'
-         WHEN 'f' THEN 'foreign table'
-         ELSE pc.relkind --
-       END AS kind, --
-       coalesce(obj_pt.spcname, db_pt.spcname) AS tablespace, pa.rolname AS owner
+-- Size and tablespace of relation(s) (data-containing objects) by schema
+SELECT
+  pc.relname,
+--   pn.nspname AS schema,
+  pa.rolname AS owner,
+  pg_size_pretty(pg_total_relation_size(pc.oid)),
+  CASE pc.relkind -- By default, CASE will cast results as char (pc.relkind)
+    WHEN 'r' THEN CAST('table' AS TEXT)
+    WHEN 'i' THEN 'index'
+    WHEN 'S' THEN 'sequence'
+    WHEN 'v' THEN 'view'
+    WHEN 'm' THEN 'materialized view'
+    WHEN 'c' THEN 'composite type'
+    WHEN 't' THEN 'TOAST table'
+    WHEN 'f' THEN 'foreign table'
+    ELSE pc.relkind --
+  END AS kind,
+  coalesce(obj_pt.spcname, db_pt.spcname) AS tablespace
 FROM pg_class pc --
 JOIN pg_namespace pn ON pn.oid = pc.relnamespace
 JOIN pg_authid pa ON pa.oid = pc.relowner
 LEFT JOIN pg_tablespace obj_pt ON obj_pt.oid = pc.reltablespace
 JOIN pg_database pd ON pd.datname = current_catalog
 JOIN pg_tablespace db_pt ON db_pt.oid = pd.dattablespace
-WHERE pc.relname LIKE :name_pattern
+WHERE
+  pn.nspname = :schema
+ORDER BY pg_total_relation_size(pc.oid) DESC;
+
+-- Tablespace of relation(s) (data-containing objects) by name pattern
+SELECT
+  pc.relname, --
+  pn.nspname AS schema, --
+  CASE pc.relkind -- By default, CASE will cast results as char (pc.relkind)
+    WHEN 'r' THEN CAST('table' AS TEXT)
+    WHEN 'i' THEN 'index'
+    WHEN 'S' THEN 'sequence'
+    WHEN 'v' THEN 'view'
+    WHEN 'm' THEN 'materialized view'
+    WHEN 'c' THEN 'composite type'
+    WHEN 't' THEN 'TOAST table'
+    WHEN 'f' THEN 'foreign table'
+    ELSE pc.relkind --
+  END AS kind, --
+  coalesce(obj_pt.spcname, db_pt.spcname) AS tablespace,
+  pa.rolname AS owner
+FROM pg_class pc --
+JOIN pg_namespace pn ON pn.oid = pc.relnamespace
+JOIN pg_authid pa ON pa.oid = pc.relowner
+LEFT JOIN pg_tablespace obj_pt ON obj_pt.oid = pc.reltablespace
+JOIN pg_database pd ON pd.datname = current_catalog
+JOIN pg_tablespace db_pt ON db_pt.oid = pd.dattablespace
+WHERE
+  pc.relname LIKE :name_pattern
 ORDER BY schema, relname;
 
 -- Relations (data-containing objects) by kind
@@ -148,7 +187,7 @@ SELECT
   pg_size_pretty(pg_total_relation_size(pc.oid)),
   CASE pc.relkind
     WHEN 'r' -- By default, CASE will cast results as char (pc.relkind)
-            THEN CAST('table' AS TEXT)
+      THEN CAST('table' AS TEXT)
     WHEN 'i' THEN 'index'
     WHEN 'S' THEN 'sequence'
     WHEN 'v' THEN 'view'
@@ -163,7 +202,8 @@ FROM pg_class pc --
 LEFT JOIN pg_tablespace obj_pt ON obj_pt.oid = pc.reltablespace
 JOIN pg_database pd ON pd.datname = current_catalog
 JOIN pg_tablespace db_pt ON db_pt.oid = pd.dattablespace
-WHERE pc.relkind = :relkind
+WHERE
+  pc.relkind = :relkind
 ORDER BY pg_total_relation_size(pc.oid) DESC;
 
 -- Total size for a table + indexes
@@ -174,7 +214,8 @@ SELECT pc_index.relname AS index_name, pg_size_pretty(pg_total_relation_size(pc_
 FROM pg_class pc_table
 JOIN pg_index pi ON pc_table.oid = pi.indrelid
 JOIN pg_class pc_index ON pc_index.oid = pi.indexrelid
-WHERE pc_table.relname = 'temp_wos_reference'
+WHERE
+  pc_table.relname = 'temp_wos_reference'
   --'derwent_familyid'
 ORDER BY pc_index.relname;
 
@@ -188,7 +229,9 @@ SELECT
                                                           END) AS ddl,
   pt.oid AS tablespace_oid
 FROM pg_tablespace pt, pg_settings ps
-WHERE ps.name = 'data_directory' AND pt.spcname NOT LIKE 'pg_%'
+WHERE
+  ps.name = 'data_directory'
+AND pt.spcname NOT LIKE 'pg_%'
 ORDER BY pt.spcname;
 
 -- region Create a tablespace
@@ -207,7 +250,8 @@ DROP TABLESPACE :tbs;
 -- region Move objects to a tablespace
 -- 1.7 GB
 -- 17s for a Premium to Premium storage move
-ALTER TABLE :table SET TABLESPACE :tbs;
+ALTER TABLE :table
+  SET TABLESPACE :tbs;
 
 -- 2.6 GB
 -- 23-42s for a Premium to Premium storage move
