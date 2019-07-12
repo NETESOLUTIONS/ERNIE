8ef43e0dc64366497634230242c79481cde65974
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MBP.fios-router.home>
2019-07-12 15:01:16 -0400

    tap scopus update

--- Scopus/scopus_updates_tap_test.sql
+++ Scopus/scopus_updates_tap_test.sql
@@ -208,7 +208,7 @@ $$ LANGUAGE plpgsql;
 -- Run functions
 -- Start transaction and plan the tests.
 BEGIN;
-SELECT plan(4);
+SELECT plan(1);
 -- Run the tests.
 SELECT pass( 'My test passed, w00t!' );
 -- Finish the tests and clean up.
