97d18e8895dad54940f5b073768bcd1645dea861
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MBP.fios-router.home>
2019-07-15 14:32:35 -0400

    scopus update automated workflow

--- Scopus/scopus_update_tap_test.sql
+++ Scopus/scopus_update_tap_test.sql
@@ -179,7 +179,7 @@ CREATE OR REPLACE FUNCTION test_that_there_is_no_100_percent_NULL_column_in_scop
  AS $$
  BEGIN
    RETURN NEXT is_empty( 'select distinct tablename, attname from pg_stats
-    where schemaname = ''public'' and tablename like ''scopus_abstracts'' and null_frac = 1', 'No 100% null column');
+    where schemaname = ''public'' and tablename = ''scopus_abstracts'' and null_frac = 1', 'No 100% null column');
  END;
  $$ LANGUAGE plpgsql;
 
