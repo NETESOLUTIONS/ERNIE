a7e9c9da2838c85c4bb134e7da1c8b6bbfa11190
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MBP.fios-router.home>
2019-07-15 10:20:22 -0400

    scopus update automated workflow

--- Scopus/scopus_update_count_before.sql
+++ Scopus/scopus_update_count_before.sql
@@ -35,8 +35,8 @@ ORDER BY n_live_tup DESC;
 
 
 BEGIN;
-SELECT plan(10);
-select test_that_there_is_no_100_percent_NULL_column_in_scopus_tables;
+SELECT plan(11);
+select test_that_there_is_no_100_percent_NULL_column_in_scopus_tables();
 select pass('my test passed! Let us compare the speed.')
 select * from finish();
 ROLLBACK;
