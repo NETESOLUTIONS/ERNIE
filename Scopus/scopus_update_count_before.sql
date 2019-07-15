b4f2c0313f70f2e1d47aafb7099aac2bed68a061
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MBP.fios-router.home>
2019-07-15 10:22:48 -0400

    scopus update automated workflow

--- Scopus/scopus_update_count_before.sql
+++ Scopus/scopus_update_count_before.sql
@@ -14,14 +14,15 @@ AS $$
 DECLARE tab record;
 BEGIN
   FOR tab IN
-  (SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_name in ('scopus_abstracts','scopus_authors','scopus_grants', 'scopus_grant_acknowledgments','scopus_keywords','scopus_publications','scopus_publication_groups','scopus_references','scopus_sources','scopus_subjects','scopus_titles')
+  (SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_name in ('scopus_abstracts','scopus_authors','scopus_grants', 'scopus_grant_acknowledgments',
+    'scopus_keywords','scopus_publications','scopus_publication_groups','scopus_references','scopus_sources','scopus_subjects','scopus_titles')
  )
   LOOP
     EXECUTE format('ANALYZE verbose %I;',tab.table_name);
   END LOOP;
   RETURN NEXT is_empty( 'select distinct tablename, attname from pg_stats
-   where schemaname = ''public'' and tablename in ('scopus_abstracts','scopus_authors','scopus_grants', 'scopus_grant_acknowledgments',
-     'scopus_keywords','scopus_publications','scopus_publication_groups','scopus_references','scopus_sources','scopus_subjects','scopus_titles') and null_frac = 1', 'No 100% null column');
+   where schemaname = ''public'' and tablename in '('scopus_abstracts','scopus_authors','scopus_grants', 'scopus_grant_acknowledgments',
+     'scopus_keywords','scopus_publications','scopus_publication_groups','scopus_references','scopus_sources','scopus_subjects','scopus_titles')' and null_frac = 1', 'No 100% null column');
 END;
 $$ LANGUAGE plpgsql;
 
