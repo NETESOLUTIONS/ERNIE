825d10db32efbb2c85335af27036e0c6dce5f798
Siyu <syliu0519@gmail.com>
2019-07-17 11:07:02 -0400

    Update spark_analysis.sh

--- P2_studies/Permutation_Testing/Spark/spark_analysis.sh
+++ P2_studies/Permutation_Testing/Spark/spark_analysis.sh
@@ -38,9 +38,9 @@ hdfs dfs -rm -r -f /user/spark/data/*
 # Ensure the necessary libraries are installed/updated
 wget https://jdbc.postgresql.org/download/postgresql-42.2.6.jar
 sudo cp postgresql-42.2.6.jar /usr/hdp/current/sqoop-client/lib/
-sudo /usr/bin/anaconda/envs/py35/bin/conda install --name py35 -y --debug psycopg2
-sudo /usr/bin/anaconda/envs/py35/bin/conda install --name py35 -y --debug numpy
-sudo /usr/bin/anaconda/envs/py35/bin/conda install --name py35 -y --debug pandas
+#sudo /usr/bin/anaconda/envs/py35/bin/conda install --name py35 -y --debug psycopg2
+#sudo /usr/bin/anaconda/envs/py35/bin/conda install --name py35 -y --debug numpy
+#sudo /usr/bin/anaconda/envs/py35/bin/conda install --name py35 -y --debug pandas
 #sudo /usr/bin/anaconda/bin/conda install -y --debug psycopg2
 #sudo /usr/bin/anaconda/bin/conda update -y --debug numpy
 #sudo /usr/bin/anaconda/bin/conda update -y --debug pandas
