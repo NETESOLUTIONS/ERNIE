d7dc3616e3e928edcc8500bc1e62df82bc84cc36
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MBP.fios-router.home>
2019-07-22 12:58:08 -0400

    scopus update and test

--- Scopus/Postgres/DDL/Procedures/update_scopus_log.sql
+++ Scopus/Postgres/DDL/Procedures/update_scopus_log.sql
@@ -7,9 +7,16 @@ Runs and creates a update-log. Before update takes tally, after tally, compares 
 
 DROP TABLE IF EXISTS update_log_scopus;
 CREATE TABLE public.update_log_scopus (
-  id           SERIAL,
+  id  SERIAL,
   last_updated TIMESTAMP,
-  num_nct      INTEGER,
+  num_scopus INTEGER,
+  new_num INTEGER,
+  num_update INTEGER,
+  num_delete INTEGER,
+  source_filename VARCHAR(200),
+  record_count BIGINT,
+  source_file_size BIGINT,
+  process_start_time TIMESTAMP
   CONSTRAINT update_log_scopus_pk PRIMARY KEY (id) USING INDEX TABLESPACE index_tbs
 ) TABLESPACE scopus_tbs;
 
