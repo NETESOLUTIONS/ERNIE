48df5af5fcd56850e90bb8dc375b70d5276b480d
Sitaram37 <sitaramssd@gmail.com>
2019-07-24 17:00:42 -0400

    comp references subject areas

---
+++ P2_studies/comp_case_study/comp_refs_subject_areas.sql
@@ -0,0 +1,35 @@
+-- Subject area composition for all distinct references
+
+\set ON_ERROR_STOP on
+\set ECHO all
+
+\set column_name 'year_':year
+
+\set comp_table 'comp_':year
+
+SET TIMEZONE = 'US/Eastern';
+
+SET SEARCH_PATH = public;
+
+ALTER TABLE comp_refs_subject_areas
+    ADD COLUMN :column_name integer;
+
+UPDATE comp_refs_subject_areas crsa
+SET :column_name=temp.area_count
+FROM
+(WITH pubs_year AS (
+    SELECT DISTINCT cited_source_uid
+    FROM :comp_table
+),
+     pubs_subjects AS (
+         SELECT p.source_id, sc.class_code
+         FROM pubs_year p
+                  JOIN scopus_classes sc ON p.source_id = sc.scp
+         WHERE sc.class_type = 'ASJC'
+     )
+         SELECT minor_subject_area, count(*) AS area_count
+         FROM pubs_subjects ps
+                  JOIN scopus_asjc_codes sac ON ps.class_code = sac.code::text
+         GROUP BY minor_subject_area
+         ORDER BY count(*) DESC) temp  WHERE temp.minor_subject_area=crsa.minor_subject_area;
+
