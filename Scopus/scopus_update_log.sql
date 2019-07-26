e03c417cb2b2e9e5257e1778886083ef2f31c670
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MBP.fios-router.home>
2019-07-26 17:32:25 -0400

    scopus automated update process

--- Scopus/scopus_update_log.sql
+++ Scopus/scopus_update_log.sql
@@ -1,17 +1,36 @@
+/*
+ Title: Scopus-update-log
+ Author: Djamil Lakhdar-Hamina
+ Date: 07/25/2019
+ Purpose: Simply update the scopus log, which is a log that keeps a record on the number
+ of scopus publications and the number of deletions thus far
+
+
+ The tests are:
+ 1. do all tables exist
+ 2. do all tables have a pk
+ 3. do any of the tables have columns that are 100% NULL
+ 4. For various tables was there an increases ?
+ */
+
+\timing
 \set ON_ERROR_STOP on
 \set ECHO all
 
 -- DataGrip: start execution from here
 SET TIMEZONE = 'US/Eastern';
 
--- created a delete table which will be counted like scopus_pub
+-- insert into using a values list
+--
 
-INSERT INTO update_log_scopus (update_time, num_scopus_pub,num_delete)
-SELECT
-  current_timestamp,  --
-  count(a.scp),
-  count(b.scp)
-FROM scopus_publications a, del_scps b;
+INSERT INTO update_log_scopus (update_time, num_scopus_pub, num_delete)
+(VALUES
+  (
+    (SELECT current_timestamp),
+    (SELECT count(*) FROM scopus_publications),
+    (SELECT count(*) FROM del_scps)
+                                    ) 
+);
 
 SELECT *
 FROM update_log_scopus
