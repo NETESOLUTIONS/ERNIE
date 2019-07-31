cbb0fd6137d4f764cb04b4d8a6718a264d9f626a
VJ Davey <akdavey@email.wm.edu>
2019-07-31 10:56:56 -0400

    Update MatchGeneral.py

--- FPE/ETL/MatchGeneral.py
+++ FPE/ETL/MatchGeneral.py
@@ -1,6 +1,7 @@
 import fileinput
 import Vector as vec
 from   decimal import *
+import numpy as np
 import psycopg2
 from psycopg2 import sql
 import psycopg2.extras
@@ -30,7 +31,7 @@ def calculate_cosine_score(vector_1,vector_2):
     return Decimal(vector_1.normalize().inner(vector_2.normalize()))
 
 # Return BM25 score of two vectors after normalizing. Based on https://en.wikipedia.org/wiki/Okapi_BM25
-def calculate_bm25_score(term_vector,idf_vector,doc_len, avg_doc_len, k=2.0,b=0.75):
+def calculate_bm25_score(term_vector,idf_vector,doc_len, avg_doc_len, k=Decimal(2.0),b=Decimal(0.75)):
     return sum([(idf_vector[i] * term_vector[i] * (k +1)) / (idf_vector[i] + (k * (1 - b + (b * doc_len / avg_doc_len))))
                     for i in range(len(term_vector))])
 
@@ -48,6 +49,7 @@ if __name__ == '__main__':
     parser.add_argument('-Q','--query_ident_col',help='the column that corresponds to the identifier of the query vectors',required=True)
     parser.add_argument('-i','--index_table',help='the sql table that corresponds to the index vectors',required=True)
     parser.add_argument('-I','--index_ident_col',help='the column that corresponds to the identifier of the index table',required=True)
+    parser.add_argument('-s','--score_bias',help='geometric bias for score values (raising this value makes higher scoring results stand out more)',type=int,default=1)
     args = parser.parse_args()
 
     # Build IDF dictionary
@@ -82,6 +84,18 @@ if __name__ == '__main__':
         index_vectors=input_cur.fetchall()
         query_vec = vec.Vector(list(Decimal(query_fingerprint[concept_id]['Rank']) for concept_id in query_fingerprint.keys()))
         idf_vec = vec.Vector(list(Decimal(IDF.get(concept_id, 0.0)) for concept_id in query_fingerprint.keys()))
+
+        # Collect indexed document length Information
+        # NOTE: Doc lengths here correspond to length of what is indexed (meaning length of fingerprint if using fingerprint vectors)
+        input_cur.execute(sql.SQL('''SELECT {}, COUNT(DISTINCT concept_id)
+                                     FROM {}
+                                     GROUP BY {}''').format(sql.Identifier(args.index_ident_col),
+                                                            sql.Identifier(args.index_table),
+                                                            sql.Identifier(args.index_ident_col)))
+        document_len_results=input_cur.fetchall()
+        document_lengths={i[0]:i[1] for i in document_len_results}
+        average_document_length=np.mean([i[1] for i in document_len_results])
+
         scores = []
         for index_vector in index_vectors:
             index_vector_identifier=index_vector[0]
@@ -92,18 +106,22 @@ if __name__ == '__main__':
             index_vec = vec.Vector(list(Decimal(index_fingerprint.get(concept_id, {}).get('Rank', 0.0)) for concept_id in query_fingerprint.keys()))
             unweighted_cosine_score=calculate_cosine_score(query_vec,index_vec)
             weighted_cosine_score=calculate_cosine_score(query_vec.mult(idf_vec),index_vec.mult(idf_vec))
-            scores.append([index_vector_identifier,unweighted_cosine_score,weighted_cosine_score])
-            #print("{},{}\tUnweighted_Cos:{:.4}\tWeighted_Cos:{:.4}".format(query_vector_identifier,index_vector_identifier,unweighted_cosine_score,weighted_cosine_score))
-        scores.sort(key=lambda x: x[2], reverse=True)
-        print(query_vector_identifier)
-        print(scores[:10])
 
-        #TODO: continue development after regenerating fingerprint tables
             # Calculate BM25 score
-
+            term_vec =  vec.Vector(list(Decimal(index_fingerprint.get(concept_id, {}).get('AFreq', 0.0)) for concept_id in query_fingerprint.keys()))
+            bm25_score= calculate_bm25_score(term_vec.values,idf_vec.values,Decimal(document_lengths[index_vector_identifier]),Decimal(average_document_length))
 
             # Calculate Final scores
 
             # Append all scores to a list of lists
 
+
+            scores.append([ index_vector_identifier,                    # Search result ID
+                            unweighted_cosine_score ** args.score_bias, # Search result unweighted cosine score
+                            weighted_cosine_score ** args.score_bias,   # Search result weighted cosine score
+                            bm25_score ** args.score_bias               # Search result weighted cosine score
+                          ])
         # Return search results to a TXT file or SQL table, sorted by score results
+        scores.sort(key=lambda x: x[2], reverse=True)
+        for i in range(10):
+            print("{},{},{},{:.4},{:.4},{:.4}".format(query_vector_identifier,i+1,scores[i][0],scores[i][1],scores[i][2],scores[i][3]))
