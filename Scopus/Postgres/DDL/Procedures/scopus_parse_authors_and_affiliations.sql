5f3d67930ff259260c2cd35cac57a71b2fbb65df
Djamil Lakhdar-Hamina <djamillakhdar-hamina@Djamils-MacBook-Pro.local>
2019-08-23 21:08:34 -0400

    scopus sub parsers

--- Scopus/Postgres/DDL/Procedures/scopus_parse_authors_and_affiliations.sql
+++ Scopus/Postgres/DDL/Procedures/scopus_parse_authors_and_affiliations.sql
@@ -4,111 +4,58 @@
 -- DataGrip: start execution from here
 SET TIMEZONE = 'US/Eastern';
 
-CREATE OR REPLACE PROCEDURE scopus_parse_authors_and_affiliations(scopus_doc_xml xml)
+CREATE OR REPLACE PROCEDURE scopus_parse_abstracts_and_titles(scopus_doc_xml XML)
     language plpgsql
-AS
-$$
-BEGIN
-    INSERT INTO scopus_authors(scp, author_seq, auid, author_indexed_name, author_surname, author_given_name,
-                               author_initials, author_e_address, author_rank)    -- scopus_authors
-    SELECT DISTINCT scp,
-                    author_seq,
-                    max(auid) as auid,
-                    max(author_indexed_name) as author_indexed_name,
-                    author_surname,
-                    max(author_given_name) as author_given,
-                    max(author_initials) as author_initials,
-                    max(author_e_address) as author_e_address,
-                    ROW_NUMBER()
-                    over (PARTITION BY scp ORDER BY author_seq, author_surname, author_indexed_name)  as author_rank
-    FROM xmltable(--
-                 XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce), --
-                 '//bibrecord/head/author-group/author' PASSING scopus_doc_xml COLUMNS --
-            --@formatter:off
-                     scp BIGINT PATH '../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
-                     author_seq SMALLINT PATH '@seq',
-                     auid BIGINT PATH '@auid',
-                     author_indexed_name TEXT PATH 'ce:indexed-name',
-                     author_surname TEXT PATH 'ce:surname',
-                     author_given_name TEXT PATH 'ce:given-name',
-                     author_initials TEXT PATH 'ce:initials',
-                     author_e_address TEXT PATH 'ce:e-address' )
-GROUP BY scp, author_seq, author_surname
-ON CONFLICT (scp, author_seq) DO UPDATE SET auid=excluded.auid,
-                                        author_surname=excluded.author_surname,
-                                        author_indexed_name=excluded.author_indexed_name, 
-                                        author_given_name=excluded.author_given_name,
-                                        author_initials=excluded.author_initials,
-                                        author_e_address=excluded.author_e_address,
-                                        authro_rank=excluded.author_rank;
+    as
+    $$ BEGIN
+        -- scopus_abstracts
+        INSERT INTO scopus_abstracts(scp, abstract_language, abstract_source)
+        SELECT DISTINCT scp,
+                        abstract_language,
+                        abstract_source
+        FROM xmltable(
+                     XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce),
+                     '//bibrecord/head/abstracts/abstract/ce:para' PASSING scopus_doc_xml COLUMNS
+                         scp BIGINT PATH '../../../../item-info/itemidlist/itemid[@idtype="SCP"]',
+                         --         abstract_text TEXT PATH 'normalize-space()',
+                         abstract_language TEXT PATH '../@xml:lang',
+                         abstract_source TEXT PATH '../@source'
+                 )
+        ON CONFLICT (scp,abstract_language) DO UPDATE SET abstract_source=excluded.abstract_source;
 
-    -- scopus_affiliations
-    INSERT INTO scopus_affiliations(scp, affiliation_no, afid, dptid, city_group, state, postal_code, country_code,
-                                    country)
 
-    SELECT scp,
-           affiliation_no,
-           afid,
-           dptid,
-           coalesce(city_group, city) AS city_group,
-           state,
-           postal_code,
-           country_code,
-           country
-    FROM xmltable(--
-                 '//bibrecord/head/author-group/affiliation' PASSING scopus_doc_xml COLUMNS
-                scp BIGINT PATH '../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
-                affiliation_no FOR ORDINALITY,
-                afid BIGINT PATH '@afid',
-                dptid BIGINT PATH '@dptid',
-                city_group TEXT PATH 'city-group',
-                state TEXT PATH 'state',
-                postal_code TEXT PATH 'postal-code',
-                city TEXT PATH 'city',
-                country_code TEXT PATH '@country',
-                country TEXT PATH 'country'
-             )
-    ON CONFLICT (scp, affiliation_no) DO UPDATE SET scp=excluded.scp,
-                                                    affiliation_no=excluded.affiliation_no,
-                                                    afid=excluded.afid,
-                                                    dptid=excluded.dptid,
-                                                    city_group=excluded.city_group,
-                                                    state=excluded.state,
-                                                    postal_code=excluded.postal_code,
-                                                    country_code=excluded.country_code,
-                                                    country=excluded.country;
+        -- scopus_abstracts: concatenated abstract_text
+        WITH sca AS (
+            SELECT scp,
+                   abstract_language,
+                   string_agg(abstract_text, chr(10)) as abstract_text
+            FROM xmltable(
+                         XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce),
+                         '//bibrecord/head/abstracts/abstract/ce:para' PASSING scopus_doc_xml COLUMNS
+                             scp BIGINT PATH '../../../../item-info/itemidlist/itemid[@idtype="SCP"]',
+                             abstract_text TEXT PATH 'normalize-space()',
+                             abstract_language TEXT PATH '../@xml:lang'
+                     )
+            GROUP BY scp, abstract_language
+        )
+        UPDATE scopus_abstracts sa
+        SET abstract_text=sca.abstract_text
+        FROM sca
+        WHERE sa.scp = sca.scp
+          and sa.abstract_language = sca.abstract_language;
 
-    UPDATE scopus_affiliations sa
-    SET organization=sq.organization
-    FROM (
-             SELECT scp, affiliation_no, RTRIM(organization, ',') AS organization
-             FROM xmltable(--
-                          '//bibrecord/head/author-group/affiliation' PASSING scopus_doc_xml COLUMNS --
-                         scp BIGINT PATH '../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
-                         affiliation_no FOR ORDINALITY,
-                         organization TEXT PATH 'concat(organization[1]/text(), ",",organization[2]/text(), ",",organization[3]/text())'
-                      )
-         ) as sq
-    WHERE sa.scp = sq.scp
-      and sa.affiliation_no = sq.affiliation_no;
-
-    -- scopus_author_affiliations
-    INSERT INTO scopus_author_affiliations(scp, author_seq, affiliation_no)
-    SELECT DISTINCT t1.scp,
-                    t1.author_seq,
-                    t2.affiliation_no
-    FROM xmltable(--
-                 '//bibrecord/head/author-group/author' PASSING scopus_doc_xml COLUMNS --
-                scp BIGINT PATH '../../preceding-sibling::item-info/itemidlist/itemid[@idtype="SCP"]',
-                author_seq SMALLINT PATH '@seq'
-             ) as t1,
-         xmltable(--
-                 '//bibrecord/head/author-group/affiliation' PASSING scopus_doc_xml COLUMNS --
-             affiliation_no FOR ORDINALITY
-             ) as t2
-    WHERE XMLEXISTS('//bibrecord/head/author-group/affiliation' PASSING scopus_doc_xml)
-    ON CONFLICT (scp, author_seq, affiliation_no) DO UPDATE SET author_seq=excluded.author_seq,
-                                                                affiliation_no=excluded.affiliation_no;
-
-END;
-$$;
+-- scopus_titles
+INSERT INTO scopus_titles(scp, title, language)
+SELECT scp,
+       max(title),
+       max(language)
+FROM xmltable(
+             XMLNAMESPACES ('http://www.elsevier.com/xml/ani/common' AS ce),
+             '//bibrecord/head/citation-title/titletext' PASSING scopus_doc_xml COLUMNS
+                 scp BIGINT PATH '../../../item-info/itemidlist/itemid[@idtype="SCP"]',
+                 title TEXT PATH 'normalize-space()',
+                 language TEXT PATH '@language'
+         )
+GROUP BY scp, language
+ON CONFLICT (scp, language) DO UPDATE SET title=excluded.title;
+END; $$;
