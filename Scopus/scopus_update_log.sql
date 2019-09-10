7b6cac126cfd6358e827a71b7f225356344754b7
djamillakhdar-hamina <djamil@nete.com>
2019-09-10 11:08:00 -0400

    "Scopus update optimization"

--- Scopus/scopus_update_log.sql
+++ Scopus/scopus_update_log.sql
@@ -22,7 +22,7 @@ INSERT INTO update_log_scopus (update_time, num_scopus_pub, num_delete)
        (
          (SELECT current_timestamp),
          (SELECT count(*) FROM scopus_publications),
-         (SELECT count(*) FROM del_scps)
+         (SELECT count(*) FROM public.del_scps)
        )
 
       )
