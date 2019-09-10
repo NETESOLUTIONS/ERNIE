bff6fab849120ade678a5f2ff2f27868fd3d7613
George Chacko <chackoge@gmail.com>
2019-09-10 14:19:26 +0000

    script to generate merged tables for rebuttal 2

--- P2_studies/Permutation_Testing/merge_refs_citations.R
+++ P2_studies/Permutation_Testing/merge_refs_citations.R
@@ -1,7 +1,8 @@
 setwd('/ernie1_museum/data_1985')
 rm(list=ls())
 library(data.table)
-# All WoS 1985
+
+### All WoS 1985
 d1000_pubwise_85 <- fread('d1000_85_pubwise_zsc_med.csv')
 refs_85 <- fread('reference_count_1985.csv')
 d1000_pubwise_refs_85 <- merge(d1000_pubwise_85,refs_85,by.x='source_id',by.y='source_id')
@@ -23,4 +24,56 @@ fwrite(imm_pubwise_refs,file='imm_pubwise_refs.csv')
 refs_85_metab <- refs_85[metab=='t']
 metab_pubwise_85 <- fread('metab85_pubwise_zsc_med.csv')
 metab_pubwise_refs <- merge(metab_pubwise_85, refs_85_metab,by.x='source_id',by.y='source_id')
+fwrite(metab_pubwise_refs,file='metab_pubwise_refs.csv')
+
+### All WoS 1995
+setwd('/ernie1_museum/data_1995')
+rm(list=ls())
+d1000_pubwise_95 <- fread('d1000_95_pubwise_zsc_med.csv')
+refs_95 <- fread('reference_count_1995.csv')
+d1000_pubwise_refs_95 <- merge(d1000_pubwise_95,refs_95,by.x='source_id',by.y='source_id')
+fwrite(d1000_pubwise_refs_95,file='d1000_pubwise_refs_95.csv')
+
+# ap_95
+refs_95_ap <- refs_95[ap=='t']
+ap_pubwise_95 <- fread('ap95_pubwise_zsc_med.csv')
+ap_pubwise_refs_95 <- merge(ap_pubwise_95, refs_95_ap, by.x='source_id',by.y='source_id')
+fwrite(ap_pubwise_refs_95,file='ap_pubwise_refs_95.csv')
+
+#imm_95
+refs_95_imm <- refs_95[imm=='t']
+imm_pubwise_95 <- fread('imm95_pubwise_zsc_med.csv')
+imm_pubwise_refs <- merge(imm_pubwise_95,refs_95_imm, by.x='source_id',by.y='source_id')
+fwrite(imm_pubwise_refs,file='imm_pubwise_refs.csv')
+
+# metab_95
+refs_95_metab <- refs_95[metab=='t']
+metab_pubwise_95 <- fread('metab95_pubwise_zsc_med.csv')
+metab_pubwise_refs <- merge(metab_pubwise_95, refs_95_metab,by.x='source_id',by.y='source_id')
+fwrite(metab_pubwise_refs,file='metab_pubwise_refs.csv')
+
+### All WoS 2005
+setwd('/ernie1_museum/data_2005')
+rm(list=ls())
+d1000_pubwise_2005 <- fread('d1000_2005_pubwise_zsc_med.csv')
+refs_2005 <- fread('reference_count_192005.csv')
+d1000_pubwise_refs_2005 <- merge(d1000_pubwise_2005,refs_2005,by.x='source_id',by.y='source_id')
+fwrite(d1000_pubwise_refs_2005,file='d1000_pubwise_refs_2005.csv')
+
+# ap_2005
+refs_2005_ap <- refs_2005[ap=='t']
+ap_pubwise_2005 <- fread('ap2005_pubwise_zsc_med.csv')
+ap_pubwise_refs_2005 <- merge(ap_pubwise_2005, refs_2005_ap, by.x='source_id',by.y='source_id')
+fwrite(ap_pubwise_refs_2005,file='ap_pubwise_refs_2005.csv')
+
+#imm_2005
+refs_2005_imm <- refs_2005[imm=='t']
+imm_pubwise_2005 <- fread('imm2005_pubwise_zsc_med.csv')
+imm_pubwise_refs <- merge(imm_pubwise_2005,refs_2005_imm, by.x='source_id',by.y='source_id')
+fwrite(imm_pubwise_refs,file='imm_pubwise_refs.csv')
+
+# metab_2005
+refs_2005_metab <- refs_2005[metab=='t']
+metab_pubwise_2005 <- fread('metab2005_pubwise_zsc_med.csv')
+metab_pubwise_refs <- merge(metab_pubwise_2005, refs_2005_metab,by.x='source_id',by.y='source_id')
 fwrite(metab_pubwise_refs,file='metab_pubwise_refs.csv')
