a31963fbbb66ab57241577c89ae85540b7cecae9
djamillakhdar-hamina <djamil@nete.com>
2019-09-13 14:50:00 -0400

    "staging table method"

--- Scopus/stg_scopus_merge_ddl.sql
+++ Scopus/stg_scopus_merge_ddl.sql
@@ -265,104 +265,111 @@ create procedure stg_scopus_merge_source_and_conferences()
     language plpgsql
 as
 $$
+DECLARE
+    db_id      int;
+    single_row RECORD;
 BEGIN
-INSERT INTO scopus_sources(ernie_source_id, source_id, issn_main, isbn_main, source_type, source_title,
-                           coden_code, publisher_name, publisher_e_address, pub_date)
-INSERT INTO scopus_sources(ernie_source_id, source_id, issn_main, isbn_main, source_type, source_title,
-                           coden_code, publisher_name, publisher_e_address, pub_date)
-SELECT DISTINCT ON (source_id, issn_main, isbn_main) max(ernie_source_id)                 as ernie_source_id,
-                                                     source_id,
-                                                     issn_main,
-                                                     isbn_main,
-                                                     string_agg(source_type, ' ')         as source_type,
-                                                     string_agg(source_title, ' ')        as source_title,
-                                                     string_agg(coden_code, ' ')          as coden_code,
-                                                     string_agg(publisher_name, ' ')      as publisher_name,
-                                                     string_agg(publisher_e_address, ' ') as publisher_e_address,
-                                                     max(pub_date)                        as pub_date
-FROM jenkins.stg_scopus_sources
-group by ernie_source_id, source_id, issn_main, isbn_main
-ON CONFLICT (source_id, issn_main, isbn_main) DO UPDATE SET ernie_source_id     = EXCLUDED.ernie_source_id,
-                                                            source_type         = EXCLUDED.source_type,
-                                                            source_title        = EXCLUDED.source_title,
-                                                            coden_code          = EXCLUDED.coden_code,
-                                                            publisher_name      = EXCLUDED.publisher_name,
-                                                            publisher_e_address = EXCLUDED.publisher_e_address,
-                                                            pub_date=EXCLUDED.pub_date;
-INSERT INTO scopus_isbns(ernie_source_id, isbn, isbn_length, isbn_type, isbn_level)
-select distinct ernie_source_id,
-       isbn,
-       isbn_length,
-       isbn_type,
-       isbn_level
-from stg_scopus_isbns
-ON CONFLICT (ernie_source_id, isbn, isbn_type) DO UPDATE SET isbn_length=excluded.isbn_length,
-                                                             isbn_level=excluded.isbn_level;
-
-INSERT INTO scopus_issns(ernie_source_id, issn, issn_type)
-select ernie_source_id,
-       issn,
-       issn_type
-from stg_scopus_issns
-ON CONFLICT (ernie_source_id, issn, issn_type) DO UPDATE SET issn=excluded.issn,
-                                                             issn_type=excluded.issn_type;
-
-INSERT INTO scopus_conference_events(conf_code, conf_name, conf_address, conf_city, conf_postal_code,
-                                     conf_start_date,
-                                     conf_end_date, conf_number, conf_catalog_number)
-select distinct
-       conf_code,
-       conf_name,
-       conf_address,
-       conf_city,
-       conf_postal_code,
-       conf_start_date,
-       conf_end_date,
-       conf_number,
-       conf_catalog_number
-from stg_scopus_conference_events
-ON CONFLICT (conf_code, conf_name) DO UPDATE SET conf_address=excluded.conf_address,
-                                                 conf_city=excluded.conf_city,
-                                                 conf_postal_code=excluded.conf_postal_code,
-                                                 conf_start_date=excluded.conf_start_date,
-                                                 conf_end_date=excluded.conf_end_date,
-                                                 conf_number=excluded.conf_number,
-                                                 conf_catalog_number=excluded.conf_catalog_number;
+    FOR single_row IN (
+        SELECT DISTINCT source_id,
+                        issn_main,
+                        isbn_main,
+                        source_type,
+                        source_title,
+                        coden_code,
+                        publisher_name,
+                        publisher_e_address,
+                        pub_date
+        FROM stg_scopus_sources)
+        LOOP
+            INSERT INTO scopus_sources(source_id, issn_main, isbn_main, source_type, source_title,
+                                       coden_code, publisher_name, publisher_e_address, pub_date)
+            VALUES (single_row.source_id, single_row.issn_main, single_row.isbn_main, single_row.source_type,
+                    single_row.source_title,
+                    single_row.coden_code, single_row.publisher_name, single_row.publisher_e_address,
+                    single_row.pub_date)
+            ON CONFLICT (source_id, issn_main, isbn_main) DO UPDATE SET source_type         = EXCLUDED.source_type,
+                                                                        source_title        = EXCLUDED.source_title,
+                                                                        coden_code          = EXCLUDED.coden_code,
+                                                                        publisher_name      = EXCLUDED.publisher_name,
+                                                                        publisher_e_address = EXCLUDED.publisher_e_address,
+                                                                        pub_date            = EXCLUDED.pub_date
+            RETURNING ernie_source_id INTO db_id;
+        END LOOP;
 
 
-INSERT INTO scopus_conf_proceedings(ernie_source_id, conf_code, conf_name, proc_part_no, proc_page_range,
-                                    proc_page_count)
-select ernie_source_id,
-       conf_code,
-       conf_name,
-       proc_part_no,
-       proc_page_range,
-       proc_page_count
-from stg_scopus_conf_proceedings
-ON CONFLICT (ernie_source_id, conf_code, conf_name) DO UPDATE SET proc_part_no=excluded.proc_part_no,
-                                                                  proc_page_range=excluded.proc_page_range,
-                                                                  proc_page_count=excluded.proc_page_count;
-COMMIT;
+    INSERT INTO scopus_isbns (ernie_source_id, isbn, isbn_length, isbn_type, isbn_level)
+    select distinct db_id as ernie_source_id,
+                    isbn,
+                    isbn_length,
+                    isbn_type,
+                    isbn_level
+    from stg_scopus_isbns
+    ON CONFLICT (ernie_source_id, isbn, isbn_type) DO UPDATE SET isbn_length=excluded.isbn_length,
+                                                                 isbn_level=excluded.isbn_level;
+
+    INSERT INTO scopus_issns(ernie_source_id, issn, issn_type)
+    select distinct db_id as ernie_source_id,
+                    issn,
+                    issn_type
+    from stg_scopus_issns
+    ON CONFLICT (ernie_source_id, issn, issn_type) DO UPDATE SET issn=excluded.issn,
+                                                                 issn_type=excluded.issn_type;
+
+    INSERT INTO scopus_conference_events(conf_code, conf_name, conf_address, conf_city, conf_postal_code,
+                                         conf_start_date,
+                                         conf_end_date, conf_number, conf_catalog_number)
+    select distinct conf_code,
+                    conf_name,
+                    conf_address,
+                    conf_city,
+                    conf_postal_code,
+                    conf_start_date,
+                    conf_end_date,
+                    conf_number,
+                    conf_catalog_number
+    from stg_scopus_conference_events
+    ON CONFLICT (conf_code, conf_name) DO UPDATE SET conf_address=excluded.conf_address,
+                                                     conf_city=excluded.conf_city,
+                                                     conf_postal_code=excluded.conf_postal_code,
+                                                     conf_start_date=excluded.conf_start_date,
+                                                     conf_end_date=excluded.conf_end_date,
+                                                     conf_number=excluded.conf_number,
+                                                     conf_catalog_number=excluded.conf_catalog_number;
+
+
+    INSERT INTO scopus_conf_proceedings(ernie_source_id, conf_code, conf_name, proc_part_no, proc_page_range,
+                                        proc_page_count)
+    select distinct db_id as ernie_source_id,
+           conf_code,
+           conf_name,
+           proc_part_no,
+           proc_page_range,
+           proc_page_count
+    from stg_scopus_conf_proceedings
+    ON CONFLICT (ernie_source_id, conf_code, conf_name) DO UPDATE SET proc_part_no=excluded.proc_part_no,
+                                                                      proc_page_range=excluded.proc_page_range,
+                                                                      proc_page_count=excluded.proc_page_count;
+    COMMIT;
 -- scopus_conf_editors
 
-INSERT INTO scopus_conf_editors(ernie_source_id, conf_code, conf_name, indexed_name, role_type,
-                                initials, surname, given_name, degree, suffix)
-select ernie_source_id,
-       conf_code,
-       conf_name,
-       indexed_name,
-       role_type,
-       initials,
-       surname,
-       given_name,
-       degree,
-       suffix
-from stg_scopus_conf_editors
-ON CONFLICT (ernie_source_id, conf_code, conf_name, indexed_name) DO UPDATE SET role_type=excluded.role_type,
-                                                                                initials=excluded.initials,
-                                                                                surname=excluded.surname,
-                                                                                given_name=excluded.given_name,
-                                                                                degree=excluded.degree,
-                                                                                suffix=excluded.suffix;
+    INSERT INTO scopus_conf_editors(ernie_source_id, conf_code, conf_name, indexed_name, role_type,
+                                    initials, surname, given_name, degree, suffix)
+    select db_id as ernie_source_id,
+           conf_code,
+           conf_name,
+           indexed_name,
+           role_type,
+           initials,
+           surname,
+           given_name,
+           degree,
+           suffix
+    from stg_scopus_conf_editors
+    ON CONFLICT (ernie_source_id, conf_code, conf_name, indexed_name) DO UPDATE SET role_type=excluded.role_type,
+                                                                                    initials=excluded.initials,
+                                                                                    surname=excluded.surname,
+                                                                                    given_name=excluded.given_name,
+                                                                                    degree=excluded.degree,
+                                                                                    suffix=excluded.suffix;
 END
 $$;
