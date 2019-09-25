bf8730469adaaa74f15298e29f2c0dbdcfd2731a
djamillakhdar-hamina <djamil@nete.com>
2019-09-25 18:30:26 -0400

    "staging table method, dealt with an issue around scopus_publication_merge, had to delete updated scps from the original scopus publication table. "

--- Scopus/Postgres/DDL/Procedures/stg_scopus_merge_publication_and_group.sql
+++ Scopus/Postgres/DDL/Procedures/stg_scopus_merge_publication_and_group.sql
@@ -18,9 +18,8 @@ BEGIN
 
     INSERT INTO scopus_publications(scp, sgr, correspondence_person_indexed_name, correspondence_city,
                                     correspondence_country, correspondence_e_address, citation_type, citation_language)
-
     SELECT scp,
-           max(sgr)                                                                   as sgr,
+           max(sgr)                                                               as sgr,
            max(correspondence_person_indexed_name)                                as correspondence_person_indexed_name,
            max(correspondence_city)                                               as correspondence_city,
            max(correspondence_country)                                            as correspondence_country,
@@ -28,7 +27,7 @@ BEGIN
            max(citation_type)                                                     as citation_type,
            max(regexp_replace(citation_language, '([a-z])([A-Z])', '\1,\2', 'g')) as citation_language
     FROM stg_scopus_publications
-    group by scp
+    GROUP BY scp
     ON CONFLICT (scp) DO UPDATE SET sgr=excluded.sgr,
                                     correspondence_person_indexed_name=excluded.correspondence_person_indexed_name,
                                     correspondence_city=excluded.correspondence_city,
@@ -36,5 +35,7 @@ BEGIN
                                     correspondence_e_address=excluded.correspondence_e_address,
                                     citation_type=excluded.citation_type,
                                     citation_language=excluded.citation_language;
+
+    DELETE FROM scopus_publications scp USING stg_scopus_publications stg where scp.scp = stg.scp;
 END
 $$;
