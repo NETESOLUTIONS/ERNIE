deb1124b242c6341909bb6ee9c476538f65a06c2
DKroot <dk@nete.com>
2019-09-30 19:00:57 -0400

    Minor fix
    #ER-434 Scopus Updates

--- Scopus/truncate_stg_table.sql
+++ Scopus/truncate_stg_table.sql
@@ -1,7 +1,7 @@
 DO $$
   DECLARE statements CURSOR FOR SELECT tablename
                                   FROM pg_tables
-                                 WHERE tablename LIKE 'stg_scopus%' AND schemaname = current_schema();
+                                 WHERE tablename LIKE 'stg_scopus%' AND schemaname = current_schema;
   BEGIN
     FOR statement IN statements LOOP
       EXECUTE 'TRUNCATE TABLE ' || quote_ident(statement.tablename) || ' CASCADE';
