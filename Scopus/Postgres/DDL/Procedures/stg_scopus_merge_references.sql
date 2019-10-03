575f37473e30bb4119b69af08c6c268008a48e09
djamillakhdar-hamina <djamil@nete.com>
2019-10-03 10:07:05 -0400

    staging table code for parser and merger

--- Scopus/Postgres/DDL/Procedures/stg_scopus_merge_references.sql
+++ Scopus/Postgres/DDL/Procedures/stg_scopus_merge_references.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE PROCEDURE stg_scopus_merge_references()
 $$
 BEGIN
     INSERT INTO scopus_references(scp, ref_sgr, citation_text)
-    SELECT DISTINCT stg_scopus_references.scp, ref_sgr, max(citation_text) as citation_text
+    SELECT DISTINCT scopus_publications.scp, ref_sgr, max(citation_text) as citation_text
     FROM stg_scopus_references, scopus_publications
     WHERE stg_scopus_references.scp = scopus_publications.scp
     GROUP BY stg_scopus_references.scp, ref_sgr
