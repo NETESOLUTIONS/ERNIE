fd141bac5cf2fdb3097f7a46046508c62cc053d3
DKroot <dk@nete.com>
2019-10-27 23:03:59 -0400

    Fix dangling edges in the graph queries
    #ER-448 Setup Neo4j instance and analyze citation graphs

--- P2_studies/comp_case_study/dblp_neo4j_input.sql
+++ P2_studies/comp_case_study/dblp_neo4j_input.sql
@@ -23,32 +23,37 @@ SET SEARCH_PATH TO public;
 
 DROP VIEW IF EXISTS nodes;
 
-CREATE VIEW nodes
-AS
+CREATE VIEW nodes AS
 SELECT source_id AS "node_id:ID", cluster_20 AS cluster_no, pub_year, citation_count
   FROM dblp_graclus
- WHERE publication = TRUE AND citation_count IS NOT NULL AND pub_year IS NOT NULL;
+ WHERE publication = TRUE AND citation_count IS NOT NULL;
 
 SELECT COUNT(1)
-  FROM dblp_graclus
- WHERE publication = TRUE AND citation_count IS NOT NULL AND pub_year IS NOT NULL;
--- 633,122
+  FROM nodes;
+-- 633,165
 
 -- 2nd query
 
 DROP VIEW IF EXISTS edges;
 
-CREATE VIEW edges
-AS
+CREATE VIEW edges AS
+SELECT source_id AS from_node_id, cited_source_uid AS to_node_id
+  FROM dblp_dataset
+ WHERE source_id IN ( SELECT "node_id:ID" FROM nodes ) --
+   AND cited_source_uid IN ( SELECT "node_id:ID" FROM nodes );
+
+/*
 SELECT source_id AS from_node_id, cited_source_uid AS to_node_id
   FROM dblp_dataset
  WHERE cited_source_uid IN (
    SELECT source_id FROM dblp_graclus WHERE publication = TRUE AND citation_count IS NOT NULL AND pub_year IS NOT NULL
  );
+*/
 
 SELECT COUNT(1)
-  FROM dblp_dataset
- WHERE cited_source_uid IN (
-   SELECT source_id FROM dblp_graclus WHERE publication = TRUE AND citation_count IS NOT NULL AND pub_year IS NOT NULL
- );
--- 1,395,607
+  FROM edges;
+-- 1,395,462
+
+SELECT *
+  FROM edges
+ WHERE from_node_id = 29208380;
