2874f08073c711cbad3adf4201be8b53b5b9e8ab
George Chacko <chackoge@gmail.com>
2019-11-25 19:34:47 +0000

    script to generate cluster labeled edgelists

---
+++ P2_studies/comp_case_study/graclus_18_20_22.sql
@@ -0,0 +1,40 @@
+-- Script to generate edgelists labeled with clusters from 
+-- comp.dblp_graclus2 for 18, 20, and 22 cluster runs
+-- Result were tested for the correct number of clusters
+-- George Chacko 11/25/2019
+
+DROP TABLE IF EXISTS comp.dblp_graclus2_edgelist_clusters_18;
+CREATE TABLE comp.dblp_graclus2_edgelist_clusters_18 tablespace p2_studies_tbs 
+AS WITH cte AS (SELECT a.source_id AS citing,a.cited_source_uid 
+AS cited,b.cluster_18 AS citing_cluster 
+FROM comp.dblp_graclus2_edgelist a 
+INNER JOIN comp.dblp_graclus2 b ON a.source_id=b.source_id) 
+SELECT cte.*,b.cluster_18 AS cited_cluster 
+FROM cte INNER JOIN comp.dblp_graclus2 b ON cte.cited=b.source_id;
+\COPY (SELECT * FROM comp.dblp_graclus2_edgelist_clusters_18) TO 
+'~/dblp_graclus2_edgelist_clusters_18.csv' CSV HEADER DELIMITER ',';
+
+DROP TABLE IF EXISTS comp.dblp_graclus2_edgelist_clusters_20;
+CREATE TABLE comp.dblp_graclus2_edgelist_clusters_20 tablespace p2_studies_tbs 
+AS WITH cte AS (SELECT a.source_id AS citing,a.cited_source_uid 
+AS cited,b.cluster_20 AS citing_cluster 
+FROM comp.dblp_graclus2_edgelist a 
+INNER JOIN comp.dblp_graclus2 b ON a.source_id=b.source_id) 
+SELECT cte.*,b.cluster_20 AS cited_cluster 
+FROM cte INNER JOIN comp.dblp_graclus2 b ON cte.cited=b.source_id;
+\COPY (SELECT * FROM comp.dblp_graclus2_edgelist_clusters_20) TO 
+'~/dblp_graclus2_edgelist_clusters_20.csv' CSV HEADER DELIMITER ',';
+
+DROP TABLE IF EXISTS comp.dblp_graclus2_edgelist_clusters_22;
+CREATE TABLE comp.dblp_graclus2_edgelist_clusters_22 tablespace p2_studies_tbs 
+AS WITH cte AS (SELECT a.source_id AS citing,a.cited_source_uid 
+AS cited,b.cluster_22 AS citing_cluster 
+FROM comp.dblp_graclus2_edgelist a 
+INNER JOIN comp.dblp_graclus2 b ON a.source_id=b.source_id) 
+SELECT cte.*,b.cluster_22 AS cited_cluster 
+FROM cte INNER JOIN comp.dblp_graclus2 b ON cte.cited=b.source_id;
+\COPY (SELECT * FROM comp.dblp_graclus2_edgelist_clusters_22) TO 
+'~/dblp_graclus2_edgelist_clusters_22.csv' CSV HEADER DELIMITER ',';
+
+
+
