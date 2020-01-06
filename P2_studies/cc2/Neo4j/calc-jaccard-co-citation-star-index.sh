b5de04fc1e8c00b7ca008cf1c935fe4640bee5d7
Dmitriy "DK" Korobskiy <dk@nete.com>
2020-01-05 19:39:37 -0500

    Jaccard Index implementation: revert to Star Index
    #ER-471 Calculate E(x,y) and Int/Uni for selected co-cited pairs via Neo4j

--- P2_studies/cc2/Neo4j/calc-jaccard-co-citation-star-index.sh
+++ P2_studies/cc2/Neo4j/calc-jaccard-co-citation-star-index.sh
@@ -3,7 +3,7 @@ if [[ $# -lt 2 || "$1" == "-h" ]]; then
   cat <<'HEREDOC'
 NAME
 
-    calc-jaccard-co-citation-star-index.sh -- calculate Jaccard Co-Citation* Conditional Index in Neo4j.
+    calc-jaccard-co-citation-star-index.sh -- calculate Jaccard Co-Citation* Index in Neo4j for selected co-cited pairs.
 
 SYNOPSIS
 
@@ -12,7 +12,7 @@ SYNOPSIS
 
 DESCRIPTION
 
-    Calculate Jaccard Co-Citation* Conditional Index for selected co-cited pairs retrieved directly from a RDBMS.
+    Calculate Jaccard Co-Citation* Index for selected co-cited pairs retrieved directly from a RDBMS.
     Output CSV to stdout.
 
     The following options are available:
@@ -57,22 +57,41 @@ readonly CO_CITED_PAIRS_QUERY="$2"
 #cd "${WORK_DIR}"
 #echo -e "\n## Running under ${USER}@${HOSTNAME} in ${PWD} ##\n"
 
-# Jaccard Co-Citation* Conditional Index: |N(xy) = Co-citing set|/|NxUNy = N*(x) union with N*(y)| in parallel
+#cypher-shell --format plain <<HEREDOC
+#WITH '$JDBC_CONN_STRING' AS db, '${CO_CITED_PAIRS_QUERY}' AS sql
+#CALL apoc.load.jdbc(db, sql) YIELD row
+#MATCH (x:Publication {node_id: row.cited_1})<--(Nxy)-->(y:Publication {node_id: row.cited_2})
+#WITH count(Nxy) AS intersect_size, row.cited_1 AS x_scp, row.cited_2 AS y_scp
+#MATCH (x:Publication {node_id: x_scp})<--(Nx:Publication)
+#  WHERE Nx.node_id <> y_scp
+#WITH collect(Nx) AS nx_list, intersect_size, x_scp, y_scp
+#MATCH (y:Publication {node_id: y_scp})<--(Ny:Publication)
+#  WHERE Ny.node_id <> x_scp
+#WITH nx_list + collect(Ny) AS union_list, intersect_size, x_scp, y_scp
+#UNWIND union_list AS union_node
+#RETURN x_scp AS cited_1, y_scp AS cited_2,
+#       toFloat(intersect_size) / (count(DISTINCT union_node) + 2) AS jaccard_co_citation_star_index;
+#HEREDOC
+
 cypher-shell --format plain <<HEREDOC
+// Jaccard Co-Citation* Index: |N(xy) = Co-citing set|/|NxUNy = N*(x) union with N*(y)| in parallel
+// 5 pairs: 4.4s-16.0s
 WITH '$JDBC_CONN_STRING' AS db, '${CO_CITED_PAIRS_QUERY}' AS sql
 CALL apoc.load.jdbc(db, sql) YIELD row
 WITH collect({x_scp: row.cited_1, y_scp: row.cited_2}) AS pairs
 CALL apoc.cypher.mapParallel2('
   MATCH (x:Publication {node_id: _.x_scp})<--(Nxy)-->(y:Publication {node_id: _.y_scp})
-  WITH count(Nxy) AS intersect_size, min(Nxy.pub_year) AS first_co_citation_year, _.x_scp AS x_scp, _.y_scp AS y_scp
-  OPTIONAL MATCH (x:Publication {node_id: x_scp})<--(Nx:Publication)
-    WHERE Nx.node_id <> y_scp AND Nx.pub_year <= first_co_citation_year
-  WITH collect(Nx) AS nx_list, intersect_size, first_co_citation_year, x_scp, y_scp
-  OPTIONAL MATCH (y:Publication {node_id: y_scp})<--(Ny:Publication)
-    WHERE Ny.node_id <> x_scp AND Ny.pub_year <= first_co_citation_year
+  WITH count(Nxy) AS intersect_size, _.x_scp AS x_scp, _.y_scp AS y_scp
+  MATCH (x:Publication {node_id: x_scp})<--(Nx:Publication)
+    WHERE Nx.node_id <> y_scp
+  WITH collect(Nx) AS nx_list, intersect_size, x_scp, y_scp
+  MATCH (y:Publication {node_id: y_scp})<--(Ny:Publication)
+    WHERE Ny.node_id <> x_scp
   WITH nx_list + collect(Ny) AS union_list, intersect_size, x_scp, y_scp
   UNWIND union_list AS union_node
-  RETURN x_scp, y_scp, toFloat(intersect_size) / (count(DISTINCT union_node) + 2) AS jaccard_index', {}, pairs, 16)
+  RETURN x_scp AS cited_1, y_scp AS cited_2,
+         toFloat(intersect_size) / (count(DISTINCT union_node) + 2) AS jaccard_co_citation_star_index', {}, pairs, 16)
 YIELD value
-RETURN value.x_scp AS cited_1, value.y_scp AS cited_2, value.jaccard_index AS jaccard_co_citation_conditional_index;
+RETURN value.cited_1 AS cited_1, value.cited_2 AS cited_2,
+       value.jaccard_co_citation_star_index AS jaccard_co_citation_star_index;
 HEREDOC
