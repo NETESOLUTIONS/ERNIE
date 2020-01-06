832f5c6221382c1770760b254b5d234dd911462f
DKroot <dk@nete.com>
2020-01-06 15:30:41 -0500

    Switch to the conditional index
    #ER-471 Calculate E(x,y) and Int/Uni for selected co-cited pairs via Neo4j

--- P2_studies/cc2/Neo4j/calc-jaccard-co-citation-index.sh
+++ P2_studies/cc2/Neo4j/calc-jaccard-co-citation-index.sh
@@ -79,19 +79,17 @@ CALL apoc.load.jdbc(db, sql) YIELD row
 WITH collect({x_scp: row.cited_1, y_scp: row.cited_2}) AS pairs
 CALL apoc.cypher.mapParallel2('
   MATCH (x:Publication {node_id: _.x_scp})<--(Nxy)-->(y:Publication {node_id: _.y_scp})
-  WITH count(Nxy) AS intersect_size, _.x_scp AS x_scp, _.y_scp AS y_scp
-  MATCH (x:Publication {node_id: x_scp})<--(Nx:Publication)
-    WHERE Nx.node_id <> y_scp
-  WITH collect(Nx) AS nx_list, intersect_size, x_scp, y_scp
-  MATCH (y:Publication {node_id: y_scp})<--(Ny:Publication)
-    WHERE Ny.node_id <> x_scp
+  WITH count(Nxy) AS intersect_size, min(Nxy.pub_year) AS first_co_citation_year, _.x_scp AS x_scp, _.y_scp AS y_scp
+  OPTIONAL MATCH (x:Publication {node_id: x_scp})<--(Nx:Publication)
+    WHERE Nx.node_id <> y_scp AND Nx.pub_year <= first_co_citation_year
+  WITH collect(Nx) AS nx_list, intersect_size, first_co_citation_year, x_scp, y_scp
+  OPTIONAL MATCH (y:Publication {node_id: y_scp})<--(Ny:Publication)
+    WHERE Ny.node_id <> x_scp AND Ny.pub_year <= first_co_citation_year
   WITH nx_list + collect(Ny) AS union_list, intersect_size, x_scp, y_scp
   UNWIND union_list AS union_node
-  RETURN x_scp AS cited_1, y_scp AS cited_2,
-         toFloat(intersect_size) / (count(DISTINCT union_node) + 2) AS jaccard_co_citation_star_index', {}, pairs, 8)
+  RETURN x_scp, y_scp, toFloat(intersect_size) / (count(DISTINCT union_node) + 2) AS jaccard_index', {}, pairs, 16)
 YIELD value
-RETURN value.cited_1 AS cited_1, value.cited_2 AS cited_2,
-       value.jaccard_co_citation_star_index AS jaccard_co_citation_star_index;
+RETURN value.x_scp AS cited_1, value.y_scp AS cited_2, value.jaccard_index AS jaccard_co_citation_conditional_index;
 HEREDOC
 } | tee >(wc -l >num_of_lines.txt) >"$OUTPUT"
 
