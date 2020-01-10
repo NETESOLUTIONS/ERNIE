5dd8d24915eacbbdfcb3a750881db31a0c78e58e
DKroot <dk@nete.com>
2020-01-10 14:24:17 -0500

    Parallelize calculations
    #ER-471 Calculate E(x,y) and Int/Uni for selected co-cited pairs via Neo4j

--- Neo4j/neo4j-parallel-batch-calculation.sh
+++ Neo4j/neo4j-parallel-batch-calculation.sh
@@ -87,37 +87,36 @@ fi
 set -e
 set -o pipefail
 
-readonly CYPHER_SHELL_OUTPUT=$(mktemp)
-
-# Note: this file should be written to and owned by the `neo4j` user, hence can't use `mktemp`
-readonly BATCH_OUTPUT="/tmp/batch.$$.csv"
-
-while (( $# > 0 )); do
+while (($# > 0)); do
   case "$1" in
     -v)
-      readonly VERBOSE_MODE="true";;
+      readonly VERBOSE_MODE="true"
+      ;;
     *)
       break
+      ;;
   esac
   shift
 done
 
-readonly OUTPUT="$1"
-readonly JDBC_CONN_STRING="$2"
-readonly INPUT_DATA_SQL_QUERY="$3"
-readonly CYPHER_QUERY_FILE="$4"
+declare -rx OUTPUT="$1"
+declare -rx JDBC_CONN_STRING="$2"
+declare -rx INPUT_DATA_SQL_QUERY="$3"
+declare -rx CYPHER_QUERY_FILE="$4"
 echo "Calculating via $CYPHER_QUERY_FILE"
 
 if [[ $5 ]]; then
-  declare -ri EXPECTED_NUM_RECORDS=$5
+  declare -rxi EXPECTED_NUM_RECORDS=$5
   shift
 fi
 if [[ $5 ]]; then
-  declare -ri BATCH_SIZE=$5
-  declare -i expected_batches=$(( EXPECTED_NUM_RECORDS / BATCH_SIZE ))
-  if (( EXPECTED_NUM_RECORDS % BATCH_SIZE > 0 )); then
-    (( ++expected_batches ))
+  declare -rxi BATCH_SIZE=$5
+  declare -i expected_batches=$((EXPECTED_NUM_RECORDS / BATCH_SIZE))
+  if ((EXPECTED_NUM_RECORDS % BATCH_SIZE > 0)); then
+    ((++expected_batches))
   fi
+else
+  declare -i expected_batches=1
 fi
 
 # Get a script directory, same as by $(dirname $0)
@@ -132,18 +131,23 @@ fi
 #cd "${WORK_DIR}"
 #echo -e "\n## Running under ${USER}@${HOSTNAME} in ${PWD} ##\n"
 
-declare -i processed_records=0 batch_num=1
-export sql_query="'${INPUT_DATA_SQL_QUERY}'"
-declare -i start_time batch_start_time batch_end_time elapsed_ms=0 est_total_time_ms
-while (( processed_records < EXPECTED_NUM_RECORDS )); do
+process_batch() {
+  local -ri batch_num=$1
+  local -ri processed_records=$(((batch_num - 1) * BATCH_SIZE))
+  # Note: this file should be written to and owned by the `neo4j` user, hence can't use `mktemp`
+  local -r BATCH_OUTPUT="/tmp/$$-batch-$batch_num.csv"
+  local -r CYPHER_SHELL_OUTPUT=$(mktemp)
+
+  local -i batch_start_time batch_end_time delta_ms delta_s
+
   # Epoch time + milliseconds
   batch_start_time=$(date +%s%3N)
-  (( batch_num == 1 )) && (( start_time=batch_start_time ))
+  #  (( batch_num == 1 )) && (( start_time=batch_start_time ))
   if [[ $BATCH_SIZE ]]; then
     export sql_query="'${INPUT_DATA_SQL_QUERY} LIMIT $BATCH_SIZE OFFSET $processed_records'"
-    declare -i expected_batch_records=$(( EXPECTED_NUM_RECORDS - processed_records ))
-    if (( expected_batch_records > BATCH_SIZE )); then
-      (( expected_batch_records = BATCH_SIZE ))
+    declare -i expected_batch_records=$((EXPECTED_NUM_RECORDS - processed_records))
+    if ((expected_batch_records > BATCH_SIZE)); then
+      ((expected_batch_records = BATCH_SIZE))
     fi
   else
     if [[ $EXPECTED_NUM_RECORDS ]]; then
@@ -151,9 +155,10 @@ while (( processed_records < EXPECTED_NUM_RECORDS )); do
     fi
   fi
 
-  # shellcheck disable=SC2016 # false alarm
+  local cypher_query
   # cypher-shell :param does not support a multi-line string, hence de-tokenizing SQL query using `envsubst`.
-  cypher_query="CALL apoc.export.csv.query(\"$(envsubst '\$sql_query' <"$CYPHER_QUERY_FILE")\", '$BATCH_OUTPUT',
+  # shellcheck disable=SC2016 # false alarm
+  cypher_query="CALL apoc.export.csv.query(\"$(envsubst '\$sql_query' < "$CYPHER_QUERY_FILE")\", '$BATCH_OUTPUT',
     {params: {JDBC_conn_string: '$JDBC_CONN_STRING'}});"
 
   if ! echo "$cypher_query" | cypher-shell > "$CYPHER_SHELL_OUTPUT"; then
@@ -173,33 +178,33 @@ HEREDOC
 
   declare -i num_of_records
   # Suppress printing a file name
-  num_of_records=$(wc --lines <"$BATCH_OUTPUT")
-  if (( num_of_records > 0 )); then
+  num_of_records=$(wc --lines < "$BATCH_OUTPUT")
+  if ((num_of_records > 0)); then
     # Exclude CSV header
-    (( --num_of_records )) || :
+    ((--num_of_records)) || :
   fi
-  if (( batch_num == 1 )); then
+  if ((batch_num == 1)); then
     # Copy to an output file owned by the current user
     cp "$BATCH_OUTPUT" "$OUTPUT"
   fi
 
   if [[ $BATCH_SIZE ]]; then
     echo -n "Batch #${batch_num}/${expected_batches}: "
-    if (( batch_num > 1 )); then
-      tail -n +2 <"$BATCH_OUTPUT" >> "$OUTPUT"
+    if ((batch_num > 1)); then
+      tail -n +2 < "$BATCH_OUTPUT" >> "$OUTPUT"
       if [[ "$VERBOSE_MODE" == true ]]; then
         ls -l "$OUTPUT"
       fi
     fi
   fi
   batch_end_time=$(date +%s%3N)
-  (( delta_ms = batch_end_time - batch_start_time )) || :
-  (( delta_s = delta_ms / 1000 )) || :
+  ((delta_ms = batch_end_time - batch_start_time)) || :
+  ((delta_s = delta_ms / 1000)) || :
 
   # When performing calculations `/` will truncate the result and should be done last
   printf "%d records exported in %dh:%02dm:%02d.%01ds at %.1f records/min\n" "$num_of_records" \
-      $(( delta_s / 3600 )) $(( (delta_s / 60) % 60 )) $(( delta_s % 60 )) $(( delta_ms % 1000 )) \
-      "$(( 10**9 * num_of_records * 1000 * 60 / delta_ms ))e-9"
+  $((delta_s / 3600)) $(((delta_s / 60) % 60)) $((delta_s % 60)) $((delta_ms % 1000)) \
+      "$((10 ** 9 * num_of_records * 1000 * 60 / delta_ms))e-9"
 
   if [[ $expected_batch_records && $num_of_records -ne $expected_batch_records ]]; then
     echo "Error! The actual exported number of records is not the expected number ($expected_batch_records)." 1>&2
@@ -214,23 +219,30 @@ cypher-shell output:
 $(cat "$CYPHER_SHELL_OUTPUT")
 =====
 HEREDOC
-HEREDOC
+    HEREDOC
     exit 1
   fi
-  (( processed_records += num_of_records ))
-  (( elapsed_ms += delta_ms ))
+  #  (( processed_records += num_of_records ))
+  #  (( elapsed_ms += delta_ms ))
+}
+export -f process_batch
 
-  if (( processed_records < EXPECTED_NUM_RECORDS )); then
-    (( est_total_time_ms = elapsed_ms * EXPECTED_NUM_RECORDS / processed_records )) || :
-    printf "ETA: %s" "$(TZ=America/New_York date --date=@$(( (start_time + est_total_time_ms) / 1000 )))"
-  else
-    printf "DONE"
-  fi
-  # When performing calculations `/` will truncate the result and should be done last
-  printf " at %.1f records/min on average\n" "$(( 10**9 * processed_records * 1000 * 60 / elapsed_ms ))e-9"
+#declare -i processed_records=0 batch_num=1
+export sql_query="'${INPUT_DATA_SQL_QUERY}'"
+#declare -i start_time batch_start_time batch_end_time elapsed_ms=0 est_total_time_ms
 
-  (( ++batch_num ))
-done
+seq $expected_batches | \
+    parallel --halt soon,fail=1 --verbose --line-buffer --tagstring '|batch#{} s#{%}|' 'process_batch {}'
+
+#  if (( processed_records < EXPECTED_NUM_RECORDS )); then
+#    (( est_total_time_ms = elapsed_ms * EXPECTED_NUM_RECORDS / processed_records )) || :
+#    printf "ETA: %s" "$(TZ=America/New_York date --date=@$(( (start_time + est_total_time_ms) / 1000 )))"
+#  else
+#    printf "DONE"
+#  fi
+#  # When performing calculations `/` will truncate the result and should be done last
+#  printf " at %.1f records/min on average\n" "$(( 10**9 * processed_records * 1000 * 60 / elapsed_ms ))e-9"
+
 rm -f "$CYPHER_SHELL_OUTPUT"
 
 exit 0
