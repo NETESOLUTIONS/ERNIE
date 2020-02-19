7a483d3f3af9dfe1ffea218d9db23609b7af059f
Wenxi Zhao <50091137+WenxiZhao126@users.noreply.github.com>
2020-02-19 11:26:06 -0500

    Update ct_TAP_tests.sql

--- Trials/Automation/ct_TAP_tests.sql
+++ Trials/Automation/ct_TAP_tests.sql
@@ -155,13 +155,13 @@ ORDER BY verification_year;
 --endregion
 
 --region do clinical trials increase year by year
-WITH cte as (SELECT substring(verification_date, '[0-9]{4}')                                                  date_column,
+WITH cte as (SELECT substring(verification_date, '[0-9]{4}') AS verification_year,
                     count(verification_date) -
                     lag(count(verification_date)) over (order by substring(verification_date, '[0-9]{4}')) as difference
              FROM ct_clinical_studies
              WHERE substring(verification_date, '[0-9]{4}') is not null
-             GROUP BY date_column
-             ORDER BY date_column)
+             GROUP BY verification_year
+             ORDER BY verification_year)
 SELECT cmp_ok(CAST(cte.difference as BIGINT), '>=',
               CAST(:min_yearly_difference as BIGINT),
               format('%s.tables should increase at least %s record', 'CT', :min_yearly_difference))
