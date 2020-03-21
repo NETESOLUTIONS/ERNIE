f714ddba4ebe6c63be3bcd29555d01e625c296dd
George Chacko <george@georges-usbcmbp.local>
2020-03-21 09:18:03 -0500

    script for connected %

---
+++ P2_studies/cc2/qperc.R
@@ -0,0 +1,43 @@
+setwd('~/Desktop/cc2/')
+rm(list=ls()); library(data.table); library(ggplot2); library(extrafont)
+
+x <- fread('timelag3.csv')
+x[,lag:=first_co_cited_year-first_possible_year]
+x <- x[lag >= 0]
+x[order(-scopus_frequency)]
+setkey(x,scopus_frequency)
+
+q <- quantile(x$scopus_frequency,c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0))
+q <- data.frame(perc=names(q), value=q, row.names=NULL)
+q$perc <- gsub("%","",q$perc);q$perc <- as.integer(q$perc)
+q$total <- 0
+q$connected <- 0
+
+q[1,3] <- x[scopus_frequency < q[1,2]][,.N]
+q[1,4] <- x[scopus_frequency < q[1,2]][dc_state=='t'][,.N]
+
+for(i in 2:dim(q)[1]) {
+	print(i)
+	q[i,3] <- x[scopus_frequency >= q[i-1,2] & scopus_frequency < q[i,2]][,.N]
+	q[i,4] <- x[scopus_frequency >= q[i-1,2] & scopus_frequency < q[i,2]][dc_state=='t'][,.N]
+}
+setDT(q)
+q$perc <- as.character(q$perc)
+q[perc==10,perc:='0-10']
+q[perc==20,perc:='10-20']
+q[perc==30,perc:='20-30']
+q[perc==40,perc:='30-40']
+q[perc==50,perc:='40-50']
+q[perc==60,perc:='50-60']
+q[perc==70,perc:='60-70']
+q[perc==80,perc:='70-80']
+q[perc==90,perc:='80-90']
+q[perc==100,perc:='90-100']
+ 
+
+q[,proportion:=round(100*connected/total,1)]
+pdf('percent_connected.pdf')
+qplot(as.factor(perc),proportion,data=q,xlab="co-citation frequency percentile group",ylab="percent connected",ylim=c(0,50)) + 
+theme(text=element_text(family="Arial", size=12))
+dev.off()
+system('cp percent_connected.pdf ~/cocitation_2/')
