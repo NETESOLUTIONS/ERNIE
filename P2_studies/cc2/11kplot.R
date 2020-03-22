6aa89271162ebabfa8972a50b6ef03017f786b63
George Chacko <george@georges-usbcmbp.local>
2020-03-22 14:23:18 -0500

    update

--- P2_studies/cc2/11kplot.R
+++ P2_studies/cc2/11kplot.R
@@ -1,13 +1,13 @@
 setwd('~/Desktop/cc2')
 rm(list=ls())
-library(data.table);
-x <- fread('ten_year_cocit_union_freq11_freqsum.csv')
-pp <- data.frame(cbind(seq(0,100,by=1),unname(quantile(x$fsum,seq(0,1,by=0.01)))))
+library(data.table);library(ggplot2); library(grid)
+x <- fread('scopus_freq.csv'); x <- x[complete.cases(x)]
+pp <- data.frame(cbind(seq(0,100,by=1),unname(quantile(x$scopus_frequency,seq(0,1,by=0.01)))))
 p1 <- qplot(X1,X2,data=pp,geom=c("point","line"),xlab="Percentile",ylab="Co-citation Frequency")
 p2 <- qplot(X1,log(X2),data=pp,geom=c("point","line"),xlab="Percentile",ylab="ln(Co-citation Frequency)")
-vp <- viewport(width = 0.6, height = 0.4, x = 0.5, y = 0.7)
+vp <- viewport(width = 0.6, height = 0.7, x = 0.5, y = 0.5)
 pdf('11k_plot.pdf')
 print(p1)
 print(p2,vp=vp)
 dev.off()
-
+system('cp 11k_plot.pdf ~/cocitation_2/')
