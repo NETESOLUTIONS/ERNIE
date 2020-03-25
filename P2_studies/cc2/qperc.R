169d0bc196b1a08da99da0bdc066aee012b7c4b1
George Chacko <george@georges-usbcmbp.local>
2020-03-25 15:40:47 -0500

    rechecked script

--- P2_studies/cc2/qperc.R
+++ P2_studies/cc2/qperc.R
@@ -1,18 +1,26 @@
+# a simple script to calculate percentage of connected co-cited pairs
+# as a function of percentile groups of frequency. 
+
 setwd('~/Desktop/cc2/')
 rm(list=ls()); library(data.table); library(ggplot2); library(extrafont)
 
 x <- fread('timelag3.csv')
-x[,lag:=first_co_cited_year-first_possible_year]
-x <- x[lag >= 0]
+# x[,lag:=first_co_cited_year-first_possible_year]
+# x <- x[lag >= 0]
 x[order(-scopus_frequency)]
 setkey(x,scopus_frequency)
 
+# x has 4119324 observations
+
 q <- quantile(x$scopus_frequency,c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0))
 q <- data.frame(perc=names(q), value=q, row.names=NULL)
+# minor cleanup
 q$perc <- gsub("%","",q$perc);q$perc <- as.integer(q$perc)
+# add additional columns
 q$total <- 0
 q$connected <- 0
 
+# insert first row of data for q$total & q$connected
 q[1,3] <- x[scopus_frequency < q[1,2]][,.N]
 q[1,4] <- x[scopus_frequency < q[1,2]][dc_state=='t'][,.N]
 
@@ -34,10 +42,10 @@ q[perc==80,perc:='70-80']
 q[perc==90,perc:='80-90']
 q[perc==100,perc:='90-100']
  
-
 q[,proportion:=round(100*connected/total,1)]
+
 pdf('percent_connected.pdf')
-qplot(as.factor(perc),proportion,data=q,xlab="co-citation frequency percentile group",ylab="percent connected",ylim=c(0,50)) + 
-theme(text=element_text(family="Arial", size=12))
+qplot(as.factor(perc),proportion,data=q,xlab="co-citation frequency percentile group",ylab="percent connected",ylim=c(0,50),geom=c("point","line"),group=1) + 
+theme_bw() + theme(text=element_text(family="Arial", size=20))
 dev.off()
 system('cp percent_connected.pdf ~/cocitation_2/')
