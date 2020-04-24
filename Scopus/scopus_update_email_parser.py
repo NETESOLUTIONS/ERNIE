70490c9edb3f5345b6afb135281e8b83e164aacf
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-04-24 16:58:25 -0400

    Update script

--- Scopus/scopus_update_email_parser.py
+++ Scopus/scopus_update_email_parser.py
@@ -7,6 +7,10 @@ The point of this parser is to take an email and scan it for url.
 Once the url is found it opens it up and saves it to a specified directory.
 This is part of the code for an automated process which will leverage Jenkins to set off a process when
 triggered by the reception of an email with a url-link.
+
+Arguments: 
+	1. PMT Content
+	2. Save path
 """
 
 import time
@@ -19,6 +23,7 @@ from sys import argv
 start_time=time.time()
 
 pmt_content=argv[1]
+data_directory = "'" + argv[2] + "'"
 
 ## Build a function that 1) opens email 2) scans it for urls 3) stores urls and then opens file in them 4) then rename this downloaded file and store in specified directory.
 
@@ -36,11 +41,12 @@ def scopus_zip_file_name_date_edit(scopus_zip_file_name):
     scopus_zip_file_name: (str) Edited name with corrected date format.
     
     """
-    
-    date_field = scopus_zip_file_name[(scopus_zip_file_name.index('_ANI-ITEM')-8):scopus_zip_file_name.index('_ANI-ITEM')]
+    old_format = 'YYYY-M-D'
+    match_pattern_index = scopus_zip_file_name.index('_ANI-ITEM')
+    date_field = scopus_zip_file_name[(match_pattern_index-len(old_format)):match_pattern_index]
 
-    first_part = scopus_zip_file_name[:(scopus_zip_file_name.index('_ANI-ITEM')-8)] 
-    last_part = scopus_zip_file_name[scopus_zip_file_name.index('_ANI-ITEM'):]
+    first_part = scopus_zip_file_name[:(match_pattern_index-len(old_format))] 
+    last_part = scopus_zip_file_name[match_pattern_index:]
     
     date_field = date_field.split('-')
     date_field[1] = date_field[1].zfill(2)
@@ -52,7 +58,7 @@ def scopus_zip_file_name_date_edit(scopus_zip_file_name):
     return scopus_zip_file_name
 
 
-def email_parser(pmt_content, data_directory="/erniedev_data2/Scopus_updates"):
+def email_parser(pmt_content, data_directory):
     """
     Assumptions:
     Given an email, read the email, then scan it for url.
@@ -71,14 +77,16 @@ def email_parser(pmt_content, data_directory="/erniedev_data2/Scopus_updates"):
         #through list of links, come up with name, rename/store in testing_directory
         scopus_zip_file_name= re.findall('nete.*ANI.*zip', url)
         scopus_zip_file_name= scopus_zip_file_name[0].split('/')[2]
+        print("Old zip file name: ", scopus_zip_file_name)
         scopus_zip_file_name = scopus_zip_file_name_date_edit(scopus_zip_file_name)
+        print("New zip file name: ", scopus_zip_file_name)
         urllib.request.urlretrieve(url,os.path.join(data_directory,scopus_zip_file_name))
         print(scopus_zip_file_name)
 
 
 ## Run the function with the relevant input, which is already default argument for email_parser
 print("Scanning email now for url...")
-result=email_parser(pmt_content, data_directory="/erniedev_data2/Scopus_updates")
+result=email_parser(pmt_content, data_directory)
 print('The revelevant items are zip files:', result)
 print('The total duration for the whole process:',time.time()-start_time)
 ## End of the script
