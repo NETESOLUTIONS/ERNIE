cf1f3af1dfda5eec2ab80ffd8da0d7e196e1d798
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-05-05 19:34:32 -0400

    add patent family link

---
+++ LexisNexis/Postgres/DDL/Procedures/lexis_nexis_parse_patents_family_link.sql
@@ -0,0 +1,32 @@
+\set ON_ERROR_STOP on
+\set ECHO all
+
+-- DataGrip: start execution from here
+SET TIMEZONE = 'US/Eastern';
+SET search_path TO public;
+
+--Parse us agents
+CREATE OR REPLACE PROCEDURE lexis_nexis_parse_patents_family_link(input_xml XML) AS
+$$
+  BEGIN
+    INSERT INTO lexis_nexis_parse_patents_family_link(family_id, country_code, doc_number, kind_code)
+    SELECT
+          xmltable.family_id,
+          xmltable.country_code,
+          xmltable.doc_number,
+          xmltable.kind_code
+
+     FROM
+     xmltable('//bibliographic-data/patent-family' PASSING input_xml
+              COLUMNS
+                family_id INT PATH '//@family-id' ,
+                country_code TEXT PATH '../publication-reference/document-id/country' NOT NULL,
+                doc_number TEXT PATH '../publication-reference/document-id/doc-number' NOT NULL,
+                kind_code TEXT PATH '../publication-reference/document-id/kind' NOT NULL
+
+                )
+    ON CONFLICT (country_code, doc_number, kind_code)
+    DO UPDATE SET famiy_id=excluded.family_id;
+  END;
+$$
+LANGUAGE plpgsql;
