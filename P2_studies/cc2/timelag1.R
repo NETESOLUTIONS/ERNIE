6b048ed9baf183ebb90736c592f9ec525d3de796
George Chacko <chackoge@gmail.com>
2020-05-09 11:05:34 -0500

    finals

--- P2_studies/cc2/timelag1.R
+++ P2_studies/cc2/timelag1.R
@@ -22,13 +22,16 @@ x[scopus_frequency >= e & scopus_frequency < d, perc:='95th']
 
 y <- x[scopus_frequency >= e]
 colnames(y)[8] <- "connected"
+y[connected=='t',connected:='True']
+y[connected=='f',connected:='False']
+colnames(y)[8] <- "Connected"
 
-y[connected=='f',connected:='false'];y[connected=='t',connected:='true']
-colnames(y)[8] <- "connected"
+# y[connected=='f',connected:='false'];y[connected=='t',connected:='true']
+# colnames(y)[8] <- "connected"
 y$perc <- factor(y$perc,levels=c('99th','98th','97th','96th','95th'))
-p1 <- qplot(perc,scopus_frequency,geom="boxplot",data=y,facets=connected~.,
+p1 <- qplot(perc,scopus_frequency,geom="boxplot",data=y,facets=Connected~.,
 color=perc,xlab="percentile group") + theme(text=element_text(family="Arial", size=12))
-p2 <- qplot(perc,log(scopus_frequency),geom="boxplot",data=y,facets=connected~.,
+p2 <- qplot(perc,log(scopus_frequency),geom="boxplot",data=y,facets=Connected~.,
 color=perc) + theme(text=element_text(family="Arial", size=18)) + theme(legend.position = "none") 
 p2 <- p2 + theme(axis.title.x = element_blank())
 vp <- viewport(width = 0.5, height = 0.4, x = 0.55, y = 0.30)
@@ -40,9 +43,9 @@ dev.off()
 system('cp kinetics.pdf ~/cocitation_2/')
 
 pdf('lag.pdf')
-qplot(lag,scopus_frequency,data=y,facets=connected~perc,color=connected,xlab="time to first co-citation (yrs)",ylab="co-citation frequency") + 
+qplot(lag,scopus_frequency,data=y,facets=Connected~perc,color=Connected,xlab="Time to First Co-citation (yrs)",ylab="Co-citation Frequency") + 
 scale_y_continuous(trans='log2') + theme_bw() + 
-theme(text=element_text(family="Arial", size=18)) + theme(axis.text.x=element_text(angle = -75, hjust = 0)) +
+theme(text=element_text(family="Arial", size=20)) + theme(axis.text.x=element_text(angle = -75, hjust = 0)) +
 theme(legend.text=element_text(size=18))
 dev.off()
-system('cp lag.pdf ~/cocitation_2/R1/R1lag2.pdf')
+system('cp lag.pdf ~/cocitation_2/R1/R1lag3.pdf')
