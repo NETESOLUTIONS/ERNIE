4676b75050fd18857b4b37d647bd418093c440d7
Shreya Chandrasekharan <shreyacs615@gmail.com>
2020-05-10 20:29:02 -0400

    update patents_family_link parser

--- LexisNexis/Postgres/DDL/Procedures/lexis_nexis_parse_patents_family_link.sql
+++ LexisNexis/Postgres/DDL/Procedures/lexis_nexis_parse_patents_family_link.sql
@@ -3,7 +3,7 @@
 
 -- DataGrip: start execution from here
 SET TIMEZONE = 'US/Eastern';
-SET search_path TO public;
+SET search_path TO shreya;
 
 --Parse us agents
 CREATE OR REPLACE PROCEDURE lexis_nexis_parse_patents_family_link(input_xml XML) AS
@@ -19,14 +19,14 @@ $$
      FROM
      xmltable('//bibliographic-data/patent-family' PASSING input_xml
               COLUMNS
-                family_id INT PATH '//@family-id' ,
+                family_id INT PATH '//@family-id' NOT NULL,
                 country_code TEXT PATH '../publication-reference/document-id/country' NOT NULL,
                 doc_number TEXT PATH '../publication-reference/document-id/doc-number' NOT NULL,
                 kind_code TEXT PATH '../publication-reference/document-id/kind' NOT NULL
 
                 )
     ON CONFLICT (country_code, doc_number, kind_code)
-    DO UPDATE SET famiy_id=excluded.family_id;
+    DO UPDATE SET family_id=excluded."family_id";
   END;
 $$
 LANGUAGE plpgsql;
