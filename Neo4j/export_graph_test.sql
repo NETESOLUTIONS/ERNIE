53d5f77814db9c1c1526c1f1e8163cd026f367bc
Wenxi Zhao <50091137+WenxiZhao126@users.noreply.github.com>
2020-05-14 13:16:21 -0400

    Update export_graph_test.sql

--- Neo4j/export_graph_test.sql
+++ Neo4j/export_graph_test.sql
@@ -1,6 +1,12 @@
 \set ON_ERROR_STOP on
 \set ECHO all
 
+\if :{?schema}
+SET search_path = :schema;
+\endif
+
+-- JetBrains IDEs: start execution from here
+SET TIMEZONE = 'US/Eastern';
 /*
  Nodes: LIMIT 10
  Edges:
