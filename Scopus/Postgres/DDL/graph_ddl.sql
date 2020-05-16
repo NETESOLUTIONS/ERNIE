4e228513409d2bc431d5f901fc1fb64199425a84
Wenxi Zhao <50091137+WenxiZhao126@users.noreply.github.com>
2020-05-16 16:48:34 -0400

    Update graph_ddl.sql

--- Scopus/Postgres/DDL/graph_ddl.sql
+++ Scopus/Postgres/DDL/graph_ddl.sql
@@ -4,8 +4,7 @@ SELECT sp.scp AS "node_id:ID", spg.pub_year, sp.citation_type,
   FROM
     scopus_publications sp
       JOIN scopus_publication_groups spg ON sp.sgr = spg.sgr
-      LEFT JOIN scopus_sources ss ON ss.ernie_source_id = sp.ernie_source_id
-LIMIT 10;
+      LEFT JOIN scopus_sources ss ON ss.ernie_source_id = sp.ernie_source_id;
 
 CREATE OR REPLACE VIEW edges(from_node_id, to_node_id) AS
 SELECT sr.scp AS from_node_id, sp.scp AS to_node_id
