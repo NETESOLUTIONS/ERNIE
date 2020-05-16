b6ae21ebfc937f12318ce4e61c3801c87bbf8744
Wenxi Zhao <50091137+WenxiZhao126@users.noreply.github.com>
2020-05-15 20:58:11 -0400

    Update graph_ddl.sql

--- Scopus/Postgres/DDL/graph_ddl.sql
+++ Scopus/Postgres/DDL/graph_ddl.sql
@@ -1,10 +1,11 @@
-CREATE OR REPLACE VIEW nodes("node_id:ID", "pub_year:short") AS
+CREATE OR REPLACE VIEW wenxi.nodes1("node_id:ID", "pub_year:short", "citation_type", "has_issn:boolean") AS
 SELECT sp.scp AS "node_id:ID", spg.pub_year, sp.citation_type,
   (CASE WHEN ss.issn_main != '' THEN TRUE ELSE FALSE END) AS has_issn
   FROM
     scopus_publications sp
       JOIN scopus_publication_groups spg ON sp.sgr = spg.sgr
-      LEFT JOIN scopus_sources ss ON ss.ernie_source_id = sp.ernie_source_id;
+      LEFT JOIN scopus_sources ss ON ss.ernie_source_id = sp.ernie_source_id
+LIMIT 10;
 
 CREATE OR REPLACE VIEW edges(from_node_id, to_node_id) AS
 SELECT sr.scp AS from_node_id, sp.scp AS to_node_id
