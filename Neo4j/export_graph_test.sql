48849d43a8f64762acc02ec5733e35b3fc821142
Wenxi Zhao <50091137+WenxiZhao126@users.noreply.github.com>
2020-05-17 15:39:06 -0400

    Update export_graph_test.sql

--- Neo4j/export_graph_test.sql
+++ Neo4j/export_graph_test.sql
@@ -52,5 +52,5 @@ SET TIMEZONE = 'US/Eastern';
 -- --- 5
 
 
-\copy (SELECT * FROM nodes_test) TO 'nodes.csv' (FORMAT CSV, HEADER ON)
-\copy (SELECT from_node_id AS ":START_ID", to_node_id AS ":END_ID" FROM edges_test) TO 'edges.csv' (FORMAT CSV, HEADER ON)
+\copy (SELECT * FROM nodes_test1) TO 'nodes.csv' (FORMAT CSV, HEADER ON)
+\copy (SELECT from_node_id AS ":START_ID", to_node_id AS ":END_ID" FROM edges_test1) TO 'edges.csv' (FORMAT CSV, HEADER ON)
