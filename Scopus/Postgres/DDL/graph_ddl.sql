9ea6ed967c7dc43caf829a66b9105564c77ff095
Wenxi Zhao <50091137+WenxiZhao126@users.noreply.github.com>
2020-05-19 20:55:41 -0400

    Update graph_ddl.sql

--- Scopus/Postgres/DDL/graph_ddl.sql
+++ Scopus/Postgres/DDL/graph_ddl.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW nodes_test("node_id:ID", "pub_year:short", citation_type, "has_issn:boolean") AS
+CREATE OR REPLACE VIEW nodes("node_id:ID", "pub_year:short", citation_type, "has_issn:boolean") AS
 SELECT sp.scp AS "node_id:ID", spg.pub_year, sp.citation_type, (ss.issn_main != '') AS has_issn
   FROM scopus_publications sp
   JOIN scopus_publication_groups spg
@@ -8,8 +8,8 @@ SELECT sp.scp AS "node_id:ID", spg.pub_year, sp.citation_type, (ss.issn_main != 
   LEFT JOIN scopus_sources ss
             ON ss.ernie_source_id = sp.ernie_source_id;
 
-CREATE OR REPLACE VIEW edges_test AS
+CREATE OR REPLACE VIEW edges AS
 SELECT scp AS from_node_id, ref_sgr AS to_node_id
   FROM scopus_references
- WHERE scp IN (SELECT "node_id:ID" FROM nodes_test)
-   AND ref_sgr IN (SELECT "node_id:ID" FROM nodes_test);
+ WHERE scp IN (SELECT "node_id:ID" FROM nodes)
+   AND ref_sgr IN (SELECT "node_id:ID" FROM nodes);
