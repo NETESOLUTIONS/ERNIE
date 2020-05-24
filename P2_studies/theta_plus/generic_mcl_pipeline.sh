4a06fa237eeefc3176a860f9c19cbe52193097a4
George Chacko <chackoge@gmail.com>
2020-05-24 13:06:39 -0500

    added sleep 10 m after mcl

--- P2_studies/theta_plus/generic_mcl_pipeline.sh
+++ P2_studies/theta_plus/generic_mcl_pipeline.sh
@@ -4,10 +4,10 @@
 # unfortunate bigint events and export as tsv without colnames and rownames
 Rscript ~/ERNIE/P2_studies/theta_plus/pre_process.R --args $1
 # generate MCL output
-source ~/ERNIE/P2_studies/theta_plus/mcl_script.sh
+source ~/DatagripProjects/ERNIE/P2_studies/theta_plus/mcl_script.sh
 sleep 10m
 # postprocess mcloutput as csv
-Rscript ~/ERNIE/P2_studies/theta_plus/post_process.R
+Rscript ~/DatagripProjects/ERNIE/P2_studies/theta_plus/post_process.R
 
 
 
