117c75632fbe8949092c47a3e9598300331a92f6
George Chacko <chackoge@gmail.com>
2020-05-24 13:09:20 -0500

    fixed path

--- P2_studies/theta_plus/generic_mcl_pipeline.sh
+++ P2_studies/theta_plus/generic_mcl_pipeline.sh
@@ -4,10 +4,10 @@
 # unfortunate bigint events and export as tsv without colnames and rownames
 Rscript ~/ERNIE/P2_studies/theta_plus/pre_process.R --args $1
 # generate MCL output
-source ~/DatagripProjects/ERNIE/P2_studies/theta_plus/mcl_script.sh
+source ~/ERNIE/P2_studies/theta_plus/mcl_script.sh
 sleep 10m
 # postprocess mcloutput as csv
-Rscript ~/DatagripProjects/ERNIE/P2_studies/theta_plus/post_process.R
+Rscript ~/ERNIE/P2_studies/theta_plus/post_process.R
 
 
 
