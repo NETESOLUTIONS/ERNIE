7de7765558c69e69eb557605f3dd002bd51980e8
George Chacko <chackoge@gmail.com>
2020-05-24 12:50:12 -0500

    added sleep 10 m after mcl

--- P2_studies/theta_plus/generic_mcl_pipeline.sh
+++ P2_studies/theta_plus/generic_mcl_pipeline.sh
@@ -5,6 +5,7 @@
 Rscript ~/ERNIE/P2_studies/theta_plus/pre_process.R --args $1
 # generate MCL output
 source ~/ERNIE/P2_studies/theta_plus/mcl_script.sh
+sleep 10m
 # postprocess mcloutput as csv
 Rscript ~/ERNIE/P2_studies/theta_plus/post_process.R
 
