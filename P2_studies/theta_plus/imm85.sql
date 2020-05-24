2ab01fe8c9bb8f9740f4f35c658923a6505ac704
George Chacko <chackoge@gmail.com>
2020-05-24 07:44:22 -0500

    Added DROP iF EXISTS statements for FUll RUN

--- P2_studies/theta_plus/imm85.sql
+++ P2_studies/theta_plus/imm85.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS theta_plus.imm1985_testcase_asjc2403;
 CREATE TABLE theta_plus.imm1985_testcase_asjc2403
 TABLESPACE theta_plus_tbs AS
 SELECT sp.scp FROM scopus_publications sp
@@ -12,13 +13,15 @@ AND sc.class_code='2403';
 CREATE INDEX imm1985_testcase_asjc_idx ON theta_plus.imm1985_testcase_asjc2403(scp)
 TABLESPACE index_tbs;
 
+DROP TABLE IF EXISTS theta_plus.imm1985_testcase_cited;
 CREATE TABLE theta_plus.imm1985_testcase_cited
 TABLESPACE theta_plus_tbs AS
 SELECT tp.scp as citing,sr.ref_sgr as cited from theta_plus.imm1985_testcase_asjc2403 tp
-INNER JOIN scopus_references sr on tp.scp = sr.scp
+INNER JOIN scopus_references sr on tp.scp = sr.scp;
 CREATE INDEX imm1985_testcase_cited_idx ON theta_plus.imm1985_testcase_cited(citing,cited)
-TABLESPACE index_tbs
+TABLESPACE index_tbs;
 
+DROP TABLE IF EXISTS theta_plus.imm1985_testcase_citing;
 CREATE TABLE theta_plus.imm1985_testcase_citing TABLESPACE theta_plus_tbs AS
 SELECT sr.scp as citing,tp.scp as cited FROM theta_plus.imm1985_testcase_asjc2403 tp
 INNER JOIN scopus_references sr on tp.scp=sr.ref_sgr;
@@ -29,13 +32,19 @@ select count(1) from imm1985_testcase_asjc2403;
 select count(1) from imm1985_testcase_cited;
 select count(1) from imm1985_testcase_citing;
 
+DROP TABLE IF EXISTS theta_plus.imm1985_testcase_asjc2403_citing_cited;
 CREATE TABLE theta_plus.imm1985_testcase_asjc2403_citing_cited
 TABLESPACE theta_plus_tbs AS
 SELECT DISTINCT citing,cited from imm1985_testcase_cited UNION
 SELECT DISTINCT citing,cited from imm1985_testcase_citing;
-SELECT count(1) from imm1985_testcase_asjc2403_citing_cited
+SELECT count(1) from imm1985_testcase_asjc2403_citing_cited;
 
-CREATE TABLE theta_plus.imm85_nodes
+-- clean up Scopus data
+DELETE FROM theta_plus.imm1985_testcase_asjc2403_citing_cited
+WHERE citing=cited;
+
+DROP TABLE IF EXISTS theta_plus.imm1985_nodes;
+CREATE TABLE theta_plus.imm1985_nodes
 TABLESPACE theta_plus_tbs AS
 SELECT distinct citing as scp
 FROM theta_plus.imm1985_testcase_asjc2403_citing_cited
@@ -43,14 +52,15 @@ UNION
 SELECT distinct cited
 FROM theta_plus.imm1985_testcase_asjc2403_citing_cited;
 
-CREATE TABLE theta_plus.imm85_title_abstracts
+DROP TABLE IF EXISTS theta_plus.imm1985_title_abstracts;
+CREATE TABLE theta_plus.imm1985_title_abstracts
 TABLESPACE theta_plus_tbs AS
 SELECT tpin.scp,st.title,sa.abstract_text
-FROM theta_plus.imm85_nodes tpin
+FROM theta_plus.imm1985_nodes tpin
 INNER JOIN scopus_titles st ON tpin.scp=st.scp
 INNER JOIN scopus_abstracts sa ON tpin.scp=sa.scp
-AND sa.abstract_language='eng'
+AND sa.abstract_language='eng';
 
-select scp,title from theta_plus.imm85_title_abstracts limit 5;
-select count(1) from theta_plus.imm85_title_abstracts;
+select scp,title from theta_plus.imm1985_title_abstracts limit 5;
+select count(1) from theta_plus.imm1985_title_abstracts;
 
