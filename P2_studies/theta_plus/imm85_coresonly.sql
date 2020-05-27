a3548e6f063859d9ae73635a522e12776e0e6ea5
George Chacko <chackoge@gmail.com>
2020-05-27 14:23:39 -0500

    created coresonly cascade

--- P2_studies/theta_plus/imm85_coresonly.sql
+++ P2_studies/theta_plus/imm85_coresonly.sql
@@ -12,7 +12,7 @@ INNER JOIN scopus_classes sc
 ON sp.scp=sc.scp
 AND sc.class_code='2403';
 CREATE INDEX imm1985_testcase_asjc_coresonly_idx
-ON theta_plus.imm1985_testcase_asjc2403(scp)
+ON theta_plus.imm1985_testcase_asjc2403_coresonly(scp)
 TABLESPACE index_tbs;
 
 DROP TABLE IF EXISTS theta_plus.imm1985_testcase_cited_coresonly;
@@ -22,27 +22,27 @@ SELECT tp.scp as citing,sr.ref_sgr AS cited
 FROM theta_plus.imm1985_testcase_asjc2403 tp
 INNER JOIN scopus_references sr on tp.scp = sr.scp;
 CREATE INDEX imm1985_testcase_cited__coresonly_idx
-ON theta_plus.imm1985_testcase_cited(citing,cited)
+ON theta_plus.imm1985_testcase_cited_coresonly(citing,cited)
 TABLESPACE index_tbs;
 
 DROP TABLE IF EXISTS theta_plus.imm1985_testcase_citing_coresonly;
 CREATE TABLE theta_plus.imm1985_testcase_citing_coresonly TABLESPACE theta_plus_tbs AS
-SELECT sr.scp as citing,tp.scp as cited FROM theta_plus.imm1985_testcase_asjc2403 tp
+SELECT sr.scp as citing,tp.scp as cited FROM theta_plus.imm1985_testcase_asjc2403_coresonly tp
 INNER JOIN scopus_references sr on tp.scp=sr.ref_sgr;
 CREATE INDEX imm1985_testcase_citing__coresonly_idx
-ON theta_plus.imm1985_testcase_citing(citing,cited)
+ON theta_plus.imm1985_testcase_citing_coresonly(citing,cited)
 TABLESPACE index_tbs;
 
-select count(1) from imm1985_testcase_asjc2403;
-select count(1) from imm1985_testcase_cited;
-select count(1) from imm1985_testcase_citing;
+select count(1) from imm1985_testcase_asjc2403_coresonly;
+select count(1) from imm1985_testcase_cited_coresonly;
+select count(1) from imm1985_testcase_citing_coresonly;
 
-DROP TABLE IF EXISTS theta_plus.imm1985_testcase_asjc2403_citing_cited__coresonly;
-CREATE TABLE theta_plus.imm1985_testcase_asjc2403_citing_cited__coresonly
+DROP TABLE IF EXISTS theta_plus.imm1985_testcase_asjc2403_citing_cited_coresonly;
+CREATE TABLE theta_plus.imm1985_testcase_asjc2403_citing_cited_coresonly
 TABLESPACE theta_plus_tbs AS
-SELECT DISTINCT citing,cited from imm1985_testcase_cited UNION
-SELECT DISTINCT citing,cited from imm1985_testcase_citing;
-SELECT count(1) from imm1985_testcase_asjc2403_citing_cited;
+SELECT DISTINCT citing,cited from imm1985_testcase_cited_coresonly UNION
+SELECT DISTINCT citing,cited from imm1985_testcase_citing_coresonly;
+SELECT count(1) from imm1985_testcase_asjc2403_citing_cited_coresonly;
 
 -- clean up Scopus data
 DELETE FROM theta_plus.imm1985_testcase_asjc2403_citing_cited__coresonly
