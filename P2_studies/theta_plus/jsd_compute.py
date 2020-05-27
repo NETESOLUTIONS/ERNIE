55bc16294ddd704ff0e40b0d1d9a245d01ed07d9
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-05-27 03:08:30 -0400

    add mcl_text pipeline

---
+++ P2_studies/theta_plus/jsd_compute.py
@@ -0,0 +1,58 @@
+import jsd_modules as jm
+import pandas as pd
+import multiprocessing as mp
+from sqlalchemy import create_engine
+from glob import glob
+from sys import argv
+import os
+
+with open('/home/shreya/mcl_jsd/ernie_password.txt') as f:
+    ernie_password = f.readline()
+     
+schema = "theta_plus"
+sql_scheme = 'postgresql://shreya:' + ernie_password + '@localhost:5432/ernie'
+engine = create_engine(sql_scheme)
+
+rootdir = argv[1] # ---> /erniedev_data3/theta_plus/imm/
+dir_list = sorted(os.listdir(rootdir))
+
+# name = argv[1]
+# val = argv[2]
+name = 'now'
+val = '20'
+start_cluster_num = argv[2]
+cluster_type = argv[3]
+max_cluster_num = argv[4]
+
+for dir_name in dir_list[1:2]:
+    
+    title_abstracts_table = dir_name + '_title_abstracts'
+    all_text_data = pd.read_sql_table(table_name=title_abstracts_table, schema=schema, con=engine)
+    
+    if cluster_type == 'unshuffled':
+        cluster_path = rootdir + dir_name + '/dump.' + dir_name + '_testcase_asjc2403_citing_cited.mci.I20.csv'
+        cluster_df = pd.read_csv(cluster_path)
+    elif cluster_type == 'shuffled':
+        cluster_path = rootdir + dir_name + '/dump.' + dir_name + '_testcase_asjc2403_citing_cited_shuffled_1million.I20.csv'
+        cluster_df = pd.read_csv(cluster_path)
+
+    if max_cluster_num == 'max':
+        max_val = cluster_df['cluster_no'].max()
+    else:
+        max_val = int(max_cluster_num)
+    
+    data_text = cluster_df.merge(all_text_data, left_on='scp', right_on='scp', how='left')[['scp', 'title', 'abstract_text', 'cluster_no']]
+    
+    save_name = '/home/shreya/mcl_jsd/immunology/JSD_output_' +  dir_name + '_' + cluster_type + ".csv" 
+    
+    # p = mp.Pool(mp.cpu_count())
+    p = mp.Pool(6)
+    
+    for cluster_num in range(int(start_cluster_num), max_val+1):
+        
+        print(f'Working on Cluster Number {cluster_num} of {dir_name}_{cluster_type}')
+        jsd_df = p.starmap(jm.compute_jsd, [(data_text[data_text['cluster_no']==cluster_num], name, val, cluster_num)])
+        jsd_df.to_csv(save_name, mode = 'a', index = None, header=False, encoding='utf-8')
+
+
+    print("ALL COMPLETED.")
---
+++ P2_studies/theta_plus/jsd_modules.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+
+from textblob import TextBlob, Word
+import nltk
+from nltk.util import ngrams
+from scipy.spatial import distance
+from sklearn.feature_extraction.text import CountVectorizer
+import sklearn
+import numpy as np
+import swifter  # Makes applying to datframe as fast as vectorizing
+from nltk.probability import FreqDist
+import string
+import glob
+import os
+import pandas as pd
+import nltk
+import re
+from sys import argv
+from ast import literal_eval
+import multiprocessing as mp
+import time
+from scipy import sparse
+import preprocess_text.py
+
+
+# ------------------------------------------------------------------------------------ #
+
+f = open('/home/shreya/mcl_jsd/stop_words.txt', 'r')
+stop_words = [word.rstrip('\n') for word in f]
+
+
+def preprocess_text(doc, n_grams='one'):
+    """
+    Pre-processing using TextBlob: 
+    tokenizing, converting to lower-case, and lemmatization based on POS tagging, 
+    removing stop-words, and retaining tokens greater than length 2
+
+    We can also choose to include n_grams (n = 1,2,3) in the final output
+
+    Argument(s): 'doc' - a string of words or sentences.
+                 'n_grams' - one: only unigrams (tokens consisting of one word each)
+                           - two: only bigrams
+                           - two_plus: unigrams + bigrams
+                           - three: only trigrams 
+                           - three_plus: unigrams + bigrams + trigrams
+
+    Output: 'reuslt_singles' - a list of pre-processed tokens (individual words) of each sentence in 'doc'
+            'result_ngrams' - a list of pre-processed tokens (including n-grams) of each sentence in 'doc'
+
+    """
+
+    blob = TextBlob(doc).lower()
+#     lang = blob.detect_language()
+#     print(lang)
+#     if lang != 'en':
+#         blob = blob.translate(to = 'en')
+
+    result_singles = []
+
+    tag_dict = {"J": 'a',  # Adjective
+                "N": 'n',  # Noun
+                "V": 'v',  # Verb
+                "R": 'r'}  # Adverb
+
+    # For all other types of parts of speech (including those not classified at all)
+    # the tag_dict object maps to 'None'
+    # the method w.lemmatize() defaults to 'Noun' as POS for those classified as 'None'
+
+    for sent in blob.sentences:
+
+        words_and_tags = [(w, tag_dict.get(pos[0])) for w, pos in sent.tags]
+        lemmatized_list = [w.lemmatize(tag) for w, tag in words_and_tags]
+
+        for i in range(len(lemmatized_list)):
+
+            if lemmatized_list[i] not in stop_words and len(lemmatized_list[i].lower()) > 2 and not lemmatized_list[i].isdigit():
+                result_singles.append(lemmatized_list[i].lower())
+
+    result_bigrams = ['_'.join(x) for x in ngrams(result_singles, 2)]
+
+#     result_bigrams = [
+#         token for token in result_bigrams if token != 'psychological_association']
+
+    result_trigrams = ['_'.join(x) for x in ngrams(result_singles, 3)]
+    result_two_plus = result_singles + result_bigrams
+    result_three_plus = result_singles + result_bigrams + result_trigrams
+
+    if n_grams == 'one':
+        result = result_singles
+    elif n_grams == 'two':
+        result = result_bigrams
+    elif n_grams == 'three':
+        result = result_trigrams
+    elif n_grams == 'two_plus':
+        result = result_two_plus
+    elif n_grams == 'three_plus':
+        result = result_three_plus
+
+    return result
+
+
+# ------------------------------------------------------------------------------------ #
+
+def get_frequency(processed_text_list):
+    """
+    Using a built-in NLTK function that generates tuples
+    We get the frequency distribution of all words/n-grams in a tokenized list
+    We can get the proportion of the the token as a fraction of the total corpus size  ----> N/A
+    We can also sort these frequencies and proportions in descending order in a dictionary object ----> N/A
+
+    Argument(s): 'processed_text_list' - A list of pre-processed tokens
+
+    Output(s): freq_dict - A dictionary of tokens and their respective frequencies in descending order
+    """
+    # prop_dict - A dictionary of tokens and their respective proportions as a fraction of the total corpus
+    # combined_dict - A dictionary whose values are both frequencies and proportions combined within a list
+    # """
+
+    word_frequency = FreqDist(word for word in processed_text_list)
+
+#     sorted_counts = sorted(word_frequency.items(), key = lambda x: x[1], reverse = True)
+#     freq_dict = dict(sorted_counts)
+    freq_dict = dict(word_frequency)
+#     prop_dict = {key : freq_dict[key] * 1.0 / sum(freq_dict.values()) for key, value in freq_dict.items()}
+#     combined_dict = {key : [freq_dict[key], freq_dict[key] * 1.0 / sum(freq_dict.values())] for key, value in freq_dict.items()}
+
+    return freq_dict  # , prop_dict, combined_dict
+
+
+# ------------------------------------------------------------------------------------ #
+
+def merge_vocab_dictionary(vocab_column):
+    """
+    Takes any number of token frequency dictionaries and merges them while summing 
+    the respective frequencies and then calculates the proportion of the the tokens 
+    as a fraction of the total corpus size and saves to text and CSV files
+
+
+    Argument(s): vocab_column - A column of dictionary objects
+
+    Output(s): merged_combined_dict - A list object containing the frequencies of all
+               merged dictionary tokens along with their respective proportions
+    """
+
+    merged_freq_dict = {}
+    for dictionary in vocab_column:
+        for key, value in dictionary.items():  # d.items() in Python 3+
+            merged_freq_dict.setdefault(key, []).append(1)
+
+    for key, value in merged_freq_dict.items():
+        merged_freq_dict[key] = sum(value)
+
+#     sorted_merged_freq_dict = sorted(
+#         merged_freq_dict.items(), key=lambda x: x[1], reverse=True)
+
+#     total_sum = sum(merged_freq_dict.values())
+#     merged_prop_dict = {key : merged_freq_dict[key] * 1.0 / total_sum for key, value in merged_freq_dict.items()}
+#     merged_combined_dict = {key : [merged_freq_dict[key], (merged_freq_dict[key] * 1.0 / total_sum)] for key, value in merged_freq_dict.items()}
+
+    return merged_freq_dict
+
+
+# ------------------------------------------------------------------------------------ #
+
+def remove_less_than(frequency_dict, less_than = 1):
+    
+    """
+    We use the 
+    
+    Argument(s): 'frequency_dict' - a dictionary 
+                 'max_frequency' - 
+                 'less_than' - 
+    
+    Output: 'retained' - a dictionary of
+    """
+   
+    retained_dict = {key : value for key, value in frequency_dict.items() if (value > less_than)}
+    
+    return retained_dict
+
+# ------------------------------------------------------------------------------------ #
+
+def filter_after_preprocess(processed_tokens, retained_dict):
+    
+    """
+    We use the 
+    
+    Argument(s): processed_tokens  -
+                 vocabulary_dict -
+    
+    Output: filtered 
+    """
+    filtered = []
+
+    for token in processed_tokens:
+        if token in retained_dict.keys():
+            filtered.append(token)
+
+    return filtered
+
+# ------------------------------------------------------------------------------------ #
+
+def vectorize(text, corpus_by_cluster, count_vectorizer):
+    
+    article_count_mat = count_vectorizer.transform([' '.join(text)])
+    article_sum = sparse.diags(1/article_count_mat.sum(axis=1).A.ravel())
+    article_prob_vec = (article_sum @ article_count_mat).toarray()
+    
+    return article_prob_vec
+
+# ------------------------------------------------------------------------------------ #
+
+def calculate_jsd(doc_prob_vec, cluster_prob_vec):
+    
+    jsd = distance.jensenshannon(doc_prob_vec.tolist()[0], cluster_prob_vec)
+    
+    return jsd
+
+# ------------------------------------------------------------------------------------ #
+
+def fix_eval_issue(doc):
+    return literal_eval(doc)
+
+# ------------------------------------------------------------------------------------ #
+
+
+def compute_jsd(data_text, name, val, cluster_num):
+   
+    original_cluster_size = len(data_text)
+    data_text = data_text.dropna()
+    final_cluster_size = len(data_text)
+    
+    if final_cluster_size < 10:
+            
+        result_dict = {
+            'weight': name, 
+            'inflation': val,
+            'cluster': cluster_num,
+            'total_size': original_cluster_size, 
+            'pre_jsd_size': final_cluster_size,
+            'missing_values': (original_cluster_size-final_cluster_size,),
+            'post_jsd_size': None,
+            'jsd_nans': None, 
+            'mean_jsd': None, 
+            'min_jsd': None,
+            'percentile_25_jsd': None, 
+            'median_jsd': None,
+            'percentile_75_jsd': None, 
+            'max_jsd': None, 
+            'std_dev_jsd': None,
+            'total_unique_unigrams': None,
+            'final_unique_unigrams':  None,
+            'size_1_unigram_prop': None}
+        
+    else:
+        
+        data_text['all_text'] = data_text["title"] + data_text["abstract_text"]
+        data_text['processed_all_text'] = data_text["all_text"].swifter.apply(preprocess_text)
+        data_text['processed_all_text_frequencies'] = data_text['processed_all_text'].swifter.apply(get_frequency)
+        data_all_text_frequency = merge_vocab_dictionary(data_text['processed_all_text_frequencies'])
+
+        retained_dict = remove_less_than(data_all_text_frequency)
+        data_text['filtered_text'] = data_text['processed_all_text'].swifter.apply(filter_after_preprocess, args = (retained_dict,))
+
+        mcl_all_text = data_text.filtered_text.tolist()
+        corpus_by_article = [' '.join(text) for text in mcl_all_text]
+        corpus_by_cluster = [' '.join(corpus_by_article)]
+
+        count_vectorizer = CountVectorizer(lowercase=False, vocabulary=list(set(corpus_by_cluster[0].split())))
+        cluster_count_mat = count_vectorizer.fit_transform(corpus_by_cluster)
+
+        data_text['probability_vector'] = data_text['processed_all_text'].swifter.apply(vectorize, args=(corpus_by_cluster, count_vectorizer,))
+
+        cluster_sum = sparse.diags(1/cluster_count_mat.sum(axis=1).A.ravel())
+        cluster_prob_vec = (cluster_sum @ cluster_count_mat).toarray().tolist()[0]
+
+        data_text['JS_distance'] = data_text['probability_vector'].swifter.apply(calculate_jsd, args = (cluster_prob_vec,))
+
+        data_text = data_text.dropna()
+
+        data_text['JS_divergence'] = np.square(data_text['JS_distance'])
+        jsd_cluster_size = len(data_text)
+
+        jsd_min = min(data_text['JS_divergence'])
+        jsd_25_percentile = np.percentile(data_text['JS_divergence'], 25)
+        jsd_median = np.percentile(data_text['JS_divergence'], 50)
+        jsd_75_percentile = np.percentile(data_text['JS_divergence'], 75)
+        jsd_max = max(data_text['JS_divergence'])
+        jsd_mean = np.mean(data_text['JS_divergence'])
+        jsd_std = np.std(data_text['JS_divergence'])
+
+
+        result_dict = {
+            'weight': name, 
+            'inflation': val,
+            'cluster': cluster_num,
+            'total_size': original_cluster_size, 
+            'pre_jsd_size': final_cluster_size,
+            'missing_values': (original_cluster_size-final_cluster_size),
+            'post_jsd_size': jsd_cluster_size,
+            'jsd_nans': (final_cluster_size-jsd_cluster_size), 
+            'mean_jsd': jsd_mean, 
+            'min_jsd': jsd_min,
+            'percentile_25_jsd': jsd_25_percentile, 
+            'median_jsd': jsd_median,
+            'percentile_75_jsd': jsd_75_percentile, 
+            'max_jsd': jsd_max, 
+            'std_dev_jsd': jsd_std,
+            'total_unique_unigrams': len(data_all_text_frequency),
+            'final_unique_unigrams': len(retained_dict),
+            'size_1_unigram_prop': (1-(len(retained_dict)/len(data_all_text_frequency)))}
+        
+        
+    return result_df
+
+
+now,20,10,584,577,"(7,)",577,0,0.4958940922376906,0.37222123288412606,0.45717704222701233,0.4920737315475664,0.5309882580204898,0.6494669309233472,0.050673646045331436,9493,3457,0.6358369324765617
+now,20,11,520,485,"(35,)",485,0,0.4797781561297574,0.30764502179862707,0.3670056768672697,0.5115420767917086,0.5760471624231673,0.6657604462027619,0.10680182269611185,9056,3134,0.6539310954063604
