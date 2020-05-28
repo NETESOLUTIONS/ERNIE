9a300277c38180016af47ba8d9132abc83f5e04d
George Chacko <chackoge@gmail.com>
2020-05-28 15:57:49 -0500

    Fixed table reference erros

--- P2_studies/theta_plus/imm85_coresonly.sql
+++ P2_studies/theta_plus/imm85_coresonly.sql
@@ -1,3 +1,5 @@
+-- Create base list of scps representing immunology (ASJC=2403)
+-- articles published in English in 1985 of type core
 DROP TABLE IF EXISTS theta_plus.imm1985_testcase_asjc2403_coresonly;
 CREATE TABLE theta_plus.imm1985_testcase_asjc2403_coresonly
 TABLESPACE theta_plus_tbs AS
@@ -15,19 +17,22 @@ CREATE INDEX imm1985_testcase_asjc_coresonly_idx
 ON theta_plus.imm1985_testcase_asjc2403_coresonly(scp)
 TABLESPACE index_tbs;
 
+-- Get publications cited by the base list
 DROP TABLE IF EXISTS theta_plus.imm1985_testcase_cited_coresonly;
 CREATE TABLE theta_plus.imm1985_testcase_cited_coresonly
 TABLESPACE theta_plus_tbs AS
 SELECT tp.scp as citing,sr.ref_sgr AS cited
-FROM theta_plus.imm1985_testcase_asjc2403 tp
+FROM theta_plus.imm1985_testcase_asjc2403_coresonly tp
 INNER JOIN scopus_references sr on tp.scp = sr.scp;
 CREATE INDEX imm1985_testcase_cited__coresonly_idx
 ON theta_plus.imm1985_testcase_cited_coresonly(citing,cited)
 TABLESPACE index_tbs;
 
+-- Get publications that cite the base list
 DROP TABLE IF EXISTS theta_plus.imm1985_testcase_citing_coresonly;
 CREATE TABLE theta_plus.imm1985_testcase_citing_coresonly TABLESPACE theta_plus_tbs AS
-SELECT sr.scp as citing,tp.scp as cited FROM theta_plus.imm1985_testcase_asjc2403_coresonly tp
+SELECT sr.scp as citing,tp.scp as cited
+FROM theta_plus.imm1985_testcase_asjc2403_coresonly tp
 INNER JOIN scopus_references sr on tp.scp=sr.ref_sgr;
 CREATE INDEX imm1985_testcase_citing__coresonly_idx
 ON theta_plus.imm1985_testcase_citing_coresonly(citing,cited)
