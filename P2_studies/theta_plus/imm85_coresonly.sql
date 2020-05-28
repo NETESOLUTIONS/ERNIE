6c94855a8fe60e5d02a9d26d98e9f16fb60981c3
George Chacko <chackoge@gmail.com>
2020-05-28 16:25:13 -0500

    Fixed table reference errors Added additional statements To remove dummies from citing and cited

--- P2_studies/theta_plus/imm85_coresonly.sql
+++ P2_studies/theta_plus/imm85_coresonly.sql
@@ -1,5 +1,6 @@
 -- Create base list of scps representing immunology (ASJC=2403)
--- articles published in English in 1985 of type core
+-- articles published in English in 1985 of type *core*
+
 DROP TABLE IF EXISTS theta_plus.imm1985_testcase_asjc2403_coresonly;
 CREATE TABLE theta_plus.imm1985_testcase_asjc2403_coresonly
 TABLESPACE theta_plus_tbs AS
@@ -47,41 +48,28 @@ CREATE TABLE theta_plus.imm1985_testcase_asjc2403_citing_cited_coresonly
 TABLESPACE theta_plus_tbs AS
 SELECT DISTINCT citing,cited from imm1985_testcase_cited_coresonly UNION
 SELECT DISTINCT citing,cited from imm1985_testcase_citing_coresonly;
-SELECT count(1) from imm1985_testcase_asjc2403_citing_cited_coresonly;
+CREATE INDEX imm1985_testcase_asjc2403_citing_cited_coresonly_idx
+ON theta_plus.imm1985_testcase_asjc2403_citing_cited_coresonly(citing,cited);
 
 -- clean up Scopus data
 DELETE FROM theta_plus.imm1985_testcase_asjc2403_citing_cited__coresonly
 WHERE citing=cited;
 
-DROP TABLE IF EXISTS theta_plus.imm1985_nodes__coresonly;
-CREATE TABLE theta_plus.imm1985_nodes__coresonly
+-- Remove all dummies
+CREATE TABLE tpitacc_1985_nodummies
 TABLESPACE theta_plus_tbs AS
-SELECT distinct citing as scp
-FROM theta_plus.imm1985_testcase_asjc2403_citing_cited_coresonly
-UNION
-SELECT distinct cited
-FROM theta_plus.imm1985_testcase_asjc2403_citing_cited_coresonly;
+WITH cte AS(
+SELECT tpitaccc.citing
+FROM theta_plus.imm1985_testcase_asjc2403_citing_cited_coresonly tpitaccc
+INNER JOIN scopus_publications sp
+ON tpitaccc.citing=sp.scp
+AND sp.pub_type='core'
+SELECT cte.cited
+FROM CTE
+INNER JOIN scopus_publications sp2
+ON cte.cited=sp2.scp
+AND sp2.pub_type='core';
 
-DROP TABLE IF EXISTS theta_plus.imm1985_title_abstracts_coresonly;
-CREATE TABLE theta_plus.imm1985_title_abstracts_coresonly
-TABLESPACE theta_plus_tbs AS
-SELECT tpin.scp,st.title,sa.abstract_text
-FROM theta_plus.imm1985_nodes tpin
-INNER JOIN scopus_titles st ON tpin.scp=st.scp
-INNER JOIN scopus_abstracts sa ON tpin.scp=sa.scp
-AND sa.abstract_language='eng'
-AND st.language='English';
 
-select scp,title from theta_plus.imm1985_title_abstracts_coresonly limit 5;
-select count(1) from theta_plus.imm1985_title_abstracts_coresonly;
-
-select count(1) from imm1990_testcase_asjc2403_coresonly;
-select count(1) from (select distinct scp from imm1990_testcase_asjc2403_coresonly)c;
-select count(1) from imm1985_testcase_cited_coresonly;
-select count(1) from (select distinct citing from imm1985_testcase_cited_coresonly)c;
-select count(1) from (select distinct cited from imm1985_testcase_cited_coresonly)c;
-select count(1) from (select distinct itc.cited from imm1985_testcase_cited_coresonly itc
-INNER JOIN scopus_publications sp ON
-itc.citing=sp.scp)c
 
 
