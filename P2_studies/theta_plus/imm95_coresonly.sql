fe4755b5aab1b09b6d920a5a94c598f2e29f19cc
George Chacko <chackoge@gmail.com>
2020-05-30 08:30:03 -0400

    Fixed syntactic error in script that resulted in single column output

---
+++ P2_studies/theta_plus/imm95_coresonly.sql
@@ -0,0 +1,81 @@
+-- Create base list of scps representing immunology (ASJC=2403)
+-- articles published in English in 1995 of type *core*
+
+DROP TABLE IF EXISTS theta_plus.imm1995_testcase_asjc2403_coresonly;
+CREATE TABLE theta_plus.imm1995_testcase_asjc2403_coresonly
+TABLESPACE theta_plus_tbs AS
+SELECT sp.scp FROM scopus_publications sp
+INNER JOIN scopus_publication_groups spg
+ON sp.scp=spg.sgr
+AND spg.pub_year=1995
+AND sp.citation_type='ar'
+AND sp.citation_language='English'
+AND sp.pub_type='core'
+INNER JOIN scopus_classes sc
+ON sp.scp=sc.scp
+AND sc.class_code='2403';
+CREATE INDEX imm1995_testcase_asjc_coresonly_idx
+ON theta_plus.imm1995_testcase_asjc2403_coresonly(scp)
+TABLESPACE index_tbs;
+
+-- Get publications cited by the base list
+DROP TABLE IF EXISTS theta_plus.imm1995_testcase_cited_coresonly;
+CREATE TABLE theta_plus.imm1995_testcase_cited_coresonly
+TABLESPACE theta_plus_tbs AS
+SELECT tp.scp as citing,sr.ref_sgr AS cited
+FROM theta_plus.imm1995_testcase_asjc2403_coresonly tp
+INNER JOIN scopus_references sr on tp.scp = sr.scp;
+CREATE INDEX imm1995_testcase_cited__coresonly_idx
+ON theta_plus.imm1995_testcase_cited_coresonly(citing,cited)
+TABLESPACE index_tbs;
+
+-- Get publications that cite the base list
+DROP TABLE IF EXISTS theta_plus.imm1995_testcase_citing_coresonly;
+CREATE TABLE theta_plus.imm1995_testcase_citing_coresonly TABLESPACE theta_plus_tbs AS
+SELECT sr.scp as citing,tp.scp as cited
+FROM theta_plus.imm1995_testcase_asjc2403_coresonly tp
+INNER JOIN scopus_references sr on tp.scp=sr.ref_sgr;
+CREATE INDEX imm1995_testcase_citing__coresonly_idx
+ON theta_plus.imm1995_testcase_citing_coresonly(citing,cited)
+TABLESPACE index_tbs;
+
+-- combine citing and cited data
+DROP TABLE IF EXISTS theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly;
+CREATE TABLE theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly
+TABLESPACE theta_plus_tbs AS
+SELECT DISTINCT citing,cited from imm1995_testcase_cited_coresonly UNION
+SELECT DISTINCT citing,cited from imm1995_testcase_citing_coresonly;
+CREATE INDEX imm1995_testcase_asjc2403_citing_cited_coresonly_idx
+ON theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly(citing,cited);
+
+-- clean up combined table of self-citations
+DELETE FROM theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly
+WHERE citing=cited;
+
+-- Remove all dummies from having fished out citing and cited
+DROP TABLE IF EXISTS theta_plus.tpitacc_1995_nodummies;
+CREATE TABLE theta_plus.tpitacc_1995_nodummies
+TABLESPACE theta_plus_tbs AS
+WITH cte AS(
+SELECT tpitaccc.citing,tpitaccc.cited
+FROM theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly tpitaccc
+INNER JOIN scopus_publications sp
+ON tpitaccc.citing=sp.scp
+AND sp.pub_type='core')
+SELECT cte.citing,cte.cited
+FROM cte
+INNER JOIN scopus_publications sp2
+ON cte.cited=sp2.scp
+AND sp2.pub_type='core';
+
+select count(1) from theta_plus.tpitacc_1995_nodummies;
+select count(1) from theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly;
+
+DROP TABLE theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly;
+ALTER TABLE theta_plus.tpitacc_1995_nodummies
+RENAME TO imm1995_testcase_asjc2403_citing_cited_coresonly;
+select count(1) from theta_plus.imm1995_testcase_asjc2403_citing_cited_coresonly;
+
+
+
+
