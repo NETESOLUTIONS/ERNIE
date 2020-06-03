38bf8a34c526f3b3d15c43bc71c163aa7d6a3fa9
Dmitriy "DK" Korobskiy <dk@nete.com>
2020-06-03 09:28:17 -0400

    Fix defect
    #ER-441 Schedule periodic hardening

--- Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/1.4.3-single-user-auth.sh
+++ Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/1.4.3-single-user-auth.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "1.4.3 Ensure authentication required for single user mode"
 
-absolute_sulogin_filename=$(command -v sulogin)
-expected="ExecStart=-/bin/sh -c \"$absolute_sulogin_filename; /usr/bin/systemctl --fail --no-block default\""
+# /sbin is symlinked to /usr/sbin
+expected="ExecStart=-/bin/sh -c \"/usr/sbin/sulogin; /usr/bin/systemctl --fail --no-block default\""
 
 echo "___CHECK 1/2___"
 ensure /usr/lib/systemd/system/rescue.service "$absolute_sulogin_filename" "$expected"
