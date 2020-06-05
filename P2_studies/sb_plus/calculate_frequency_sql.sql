e360cda030f1fcfcec8cb3ed05ba97806a8cd2c0
WenxiZhao126 <w4zhao@ucsd.edu>
2020-06-05 12:51:30 -0400

    update sql query

--- P2_studies/sb_plus/calculate_frequency_sql.sql
+++ P2_studies/sb_plus/calculate_frequency_sql.sql
@@ -21,18 +21,16 @@ COPY test_one_million_pairs FROM '/erniedev_data2/jenkins_home/workspace/ERNIE-N
 SELECT cited_1, cited_2, COUNT(*) FROM cte
  GROUP BY cited_1, cited_2;*/
 
-
-CREATE OR REPLACE FUNCTION calculate_frequency() RETURNS TABLE (cited_1 BIGINT, cited_2 BIGINT, frequency INT)
-  LANGUAGE plpgsql AS
-$func$
-DECLARE
-  cited_1 BIGINT;
-  cited_2 BIGINT;
-BEGIN
-  FOR cited_1, cited_2 IN
-    SELECT cited_1, cited_2 FROM test_one_million_pairs LIMIT 1000
-  LOOP
-    EXECUTE 'WITH cte AS (SELECT sr.scp, tomp.cited_1, tomp.cited_2 FROM test_one_million_pairs tomp
+DO
+$do$
+  DECLARE
+    cited_1 BIGINT;
+    cited_2 BIGINT;
+  BEGIN
+    FOR cited_1, cited_2 IN
+      SELECT * FROM test_one_million_pairs LIMIT 1000
+    LOOP
+      EXECUTE 'WITH cte AS (SELECT sr.scp, tomp.cited_1, tomp.cited_2 FROM test_one_million_pairs tomp
               INNER JOIN public.scopus_references sr
               ON tomp.cited_1 = sr.ref_sgr
               WHERE (scp, cited_2) IN (SELECT sr.scp, sr.ref_sgr
@@ -40,7 +38,6 @@ BEGIN
               WHERE tomp.cited_2 = sr.ref_sgr))
         SELECT cited_1, cited_2, COUNT(*) FROM cte
         GROUP BY cited_1, cited_2';
-  END LOOP;
-  RETURN;
-END
-$func$;
+    END LOOP;
+  END
+$do$;
