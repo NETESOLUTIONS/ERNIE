476f9d1625c5ee7f92ba68ba8be6537de967c144
WenxiZhao126 <w4zhao@ucsd.edu>
2020-06-05 16:31:00 -0400

    update sql query

--- P2_studies/sb_plus/calculate_frequency_sql.sql
+++ P2_studies/sb_plus/calculate_frequency_sql.sql
@@ -18,21 +18,32 @@ COPY test_one_million_pairs FROM '/erniedev_data2/jenkins_home/workspace/ERNIE-N
        ON sr2.scp = sr1.scp AND sr2.ref_sgr = tomp.cited_2;
     */
 
+CREATE UNLOGGED TABLE IF NOT EXISTS test_one_million_pairs_frequency
+(
+  cited_1 BIGINT,
+  cited_2 BIGINT,
+  frequency INT,
+  CONSTRAINT test_one_million_pairs_frequency_pk PRIMARY KEY (cited_1, cited_2) USING INDEX TABLESPACE index_tbs
+);
+
 DO
 $do$
   DECLARE
-    cited_1 BIGINT;
-    cited_2 BIGINT;
+    i INT;
   BEGIN
-    FOR cited_1, cited_2 IN
-      SELECT * FROM test_one_million_pairs LIMIT 1000
+    FOR i IN 1..:N
     LOOP
-      EXECUTE 'SELECT tomp.cited_1, tomp.cited_2, COUNT(1)
-              FROM test_one_million_pairs tomp
-              JOIN scopus_references sr1
-              ON sr1.ref_sgr = tomp.cited_1
-              JOIN scopus_references sr2
-              ON sr2.scp = sr1.scp AND sr2.ref_sgr = tomp.cited_2;';
+      INSERT INTO test_one_million_pairs_frequency (cited_1, cited_2, frequency)
+      SELECT cited_1, cited_2, COUNT(1) as frequency
+        FROM test_one_million_pairs tomp
+        JOIN public.scopus_references sr1
+             ON sr1.ref_sgr = tomp.cited_1
+        JOIN public.scopus_references sr2
+             ON sr2.scp = sr1.scp AND sr2.ref_sgr = tomp.cited_2
+       GROUP BY tomp.cited_1, tomp.cited_2
+       ORDER BY cited_1, cited_2
+       LIMIT :batch_size OFFSET (i - 1)*:batch_size;
     END LOOP;
   END
 $do$;
+
