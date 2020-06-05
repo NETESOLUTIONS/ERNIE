b10e6ee25026be5c6b4c4aa4c9f949429a6d9bad
WenxiZhao126 <w4zhao@ucsd.edu>
2020-06-05 17:25:56 -0400

    update sql query

--- P2_studies/sb_plus/calculate_frequency_sql.sql
+++ P2_studies/sb_plus/calculate_frequency_sql.sql
@@ -28,9 +28,10 @@ CREATE UNLOGGED TABLE IF NOT EXISTS test_one_million_pairs_frequency
 DO
 $do$
   DECLARE
-    i INT;
+    counter INT := 1;
+    processed BIGINT := :batch_size;
   BEGIN
-    FOR i IN 1..:N
+    WHILE processed = :batch_size
     LOOP
       INSERT INTO test_one_million_pairs_frequency (cited_1, cited_2, frequency)
       SELECT cited_1, cited_2, COUNT(1) as frequency
@@ -41,8 +42,10 @@ $do$
              ON sr2.scp = sr1.scp AND sr2.ref_sgr = tomp.cited_2
        GROUP BY tomp.cited_1, tomp.cited_2
        ORDER BY cited_1, cited_2
-       LIMIT :batch_size OFFSET (i - 1)*:batch_size;
+       LIMIT :batch_size OFFSET (counter - 1)*:batch_size;
+      RAISE NOTICE 'BATCH %', counter;
+      counter := counter + 1;
+      GET DIAGNOSTICS processed = ROW_COUNT;
     END LOOP;
   END
 $do$;
-
