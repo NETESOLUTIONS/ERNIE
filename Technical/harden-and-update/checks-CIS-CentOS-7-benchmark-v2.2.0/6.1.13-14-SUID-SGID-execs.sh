dcb52d314fd4a010be6b1bb69fdcb1597857398a
Dmitriy "DK" Korobskiy <dk@nete.com>
2020-06-06 16:39:13 -0400

    Fix defect
    #ER-441 Schedule periodic hardening

--- Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/6.1.13-14-SUID-SGID-execs.sh
+++ Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/6.1.13-14-SUID-SGID-execs.sh
@@ -6,7 +6,7 @@ readonly SUID_WHITELIST="${ABSOLUTE_SCRIPT_DIR}/server-whitelists/SUID-whitelist
 readonly SGID_WHITELIST="${ABSOLUTE_SCRIPT_DIR}/server-whitelists/SGID-whitelist.txt"
 
 echo "____CHECK____"
-echo "Excluding ${exclude_dirs[*]}"
+echo "Scanning all drives, excluding ${exclude_dirs[*]}. Please, wait..."
 printf -v exclude_dir_option -- '-not -path *%s/* ' "${exclude_dirs[@]}"
 
 unset check_failed
@@ -16,13 +16,13 @@ readonly GREP_PIPELINE_INDEX=3
 # shellcheck disable=SC2086 # Don't quote `exclude_dir_option` to expand it into multiple params
 coproc { df --local --output=target \
   | tail -n +2 \
-  | xargs -I '{}' find '{}' ${exclude_dir_option} -xdev -type f -perm "/u=s,g=s" -print;
+  | xargs -I '{}' find '{}' ${exclude_dir_option} -xdev -type f -perm "/u=s,g=s" -print 2> /dev/null;
 }
 # As of Bash 4, `COPROC_PID` has to be saved before it gets reset on process termination
 _co_pid=$COPROC_PID
 while IFS= read -r file; do
-  if [[ -u "$file" ]] && ! grep -F --line-regexp "--file=${SUID_WHITELIST}" || \
-     [[ -g "$file" ]] && ! grep -F --line-regexp "--file=${SGID_WHITELIST}"; then
+  if [[ -u "$file" ]] && ! grep -F --line-regexp --quiet "$file" "${SUID_WHITELIST}" || \
+     [[ -g "$file" ]] && ! grep -F --line-regexp --quiet "$file" "${SGID_WHITELIST}"; then
     if [[ ! $check_failed ]]; then
       check_failed=true
       cat << HEREDOC
