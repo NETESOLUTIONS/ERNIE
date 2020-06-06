998cae35ae70eb07a792ff4aefa3998013ff92b7
Dmitriy "DK" Korobskiy <dk@nete.com>
2020-06-06 15:18:37 -0400

    Minor
    #ER-441 Schedule periodic hardening

--- Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/6.1.13-SUID-execs.sh
+++ Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/6.1.13-SUID-execs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "6.1.13 Audit SUID executables"
+# TODO Disk scanning takes significant time. This could be halved by combining SUID and SGID checks.
 ensure_whitelisting_of_special_file_perm 4000 SUID
