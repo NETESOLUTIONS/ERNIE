e6069a95b41e6c5cbdc66cc8060fa9c9b6ae97b5
Dmitriy "DK" Korobskiy <dk@nete.com>
2020-06-09 18:43:48 -0400

    Hardening 2.1 updates

--- Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/site-specific/5.2-SSH-server.sh
+++ Technical/harden-and-update/checks-CIS-CentOS-7-benchmark-v2.2.0/site-specific/5.2-SSH-server.sh
@@ -13,18 +13,18 @@ printf "\n\n"
 
 echo "5.2.2 Ensure SSH Protocol is set to 2"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*Protocol.*' 'Protocol 2'
+ensure /etc/ssh/sshd_config '^\s*Protocol.*' 'Protocol 2'
 printf "\n\n"
 
 echo "5.2.3 Ensure SSH LogLevel is set to INFO"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*LogLevel' 'LogLevel INFO'
+ensure /etc/ssh/sshd_config '^\s*LogLevel' 'LogLevel INFO'
 printf "\n\n"
 
 # WARNING: X11 is needed to run DataGrip and other GUI apps on the server
 echo "5.2.4 Ensure SSH X11 forwarding is disabled"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*X11Forwarding' 'X11Forwarding no'
+ensure /etc/ssh/sshd_config '^\s*X11Forwarding' 'X11Forwarding no'
 printf "\n\n"
 
 echo "5.2.5 Ensure SSH MaxAuthTries is set to 4 or less"
@@ -42,32 +42,32 @@ printf "\n\n"
 
 echo "5.2.6 Ensure SSH IgnoreRhosts is enabled"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*IgnoreRhosts ' 'IgnoreRhosts yes'
+ensure /etc/ssh/sshd_config '^\s*IgnoreRhosts ' 'IgnoreRhosts yes'
 printf "\n\n"
 
 echo "5.2.7 Ensure SSH HostbasedAuthentication is disabled"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*HostbasedAuthentication ' 'HostbasedAuthentication no'
+ensure /etc/ssh/sshd_config '^\s*HostbasedAuthentication ' 'HostbasedAuthentication no'
 printf "\n\n"
 
 echo "5.2.8 Ensure SSH root login is disabled"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*PermitRootLogin ' 'PermitRootLogin no'
+ensure /etc/ssh/sshd_config '^\s*PermitRootLogin ' 'PermitRootLogin no'
 printf "\n\n"
 
 echo "5.2.9 Ensure SSH PermitEmptyPasswords is disabled"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*PermitEmptyPasswords ' 'PermitEmptyPasswords no'
+ensure /etc/ssh/sshd_config '^\s*PermitEmptyPasswords ' 'PermitEmptyPasswords no'
 printf "\n\n"
 
 echo "5.2.10 Ensure SSH PermitUserEnvironment is disabled"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*PermitUserEnvironment ' 'PermitUserEnvironment no'
+ensure /etc/ssh/sshd_config '^\s*PermitUserEnvironment ' 'PermitUserEnvironment no'
 printf "\n\n"
 
 echo "5.2.11 Ensure only approved MAC algorithms are used"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*MACs ' "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,\
+ensure /etc/ssh/sshd_config '^\s*MACs ' "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,\
 umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com"
 printf "\n\n"
 
@@ -75,40 +75,40 @@ echo "5.2.12 Ensure SSH Idle Timeout Interval is configured"
 
 echo "____CHECK 1/2____"
 # Site policy-specific interval (recommended = 300)
-ensure /etc/ssh/sshd_config '^#*\s*ClientAliveInterval ' 'ClientAliveInterval 3600'
+ensure /etc/ssh/sshd_config '^\s*ClientAliveInterval ' 'ClientAliveInterval 3600'
 
 echo "____CHECK 2/2____"
-ensure /etc/ssh/sshd_config '^#*\s*ClientAliveCountMax ' 'ClientAliveCountMax 0'
+ensure /etc/ssh/sshd_config '^\s*ClientAliveCountMax ' 'ClientAliveCountMax 0'
 
 printf "\n\n"
 
 echo "5.2.13 Ensure SSH LoginGraceTime is set to one minute or less"
 echo "____CHECK____"
-ensure /etc/ssh/sshd_config '^#*\s*LoginGraceTime ' 'LoginGraceTime 60'
-printf "\n\n"
-
-echo "5.2.14 Ensure SSH access is limited"
-ensure /etc/ssh/sshd_config '^AllowGroups '
+ensure /etc/ssh/sshd_config '^\s*LoginGraceTime ' 'LoginGraceTime 60'
 printf "\n\n"
 
 echo "5.2.15 Ensure SSH warning banner is configured"
 echo "____CHECK____"
-if [[ ! -f /etc/issue.net ]]; then
-    cat > /etc/issue.net << 'HEREDOC'
-********************************************************************
-*                                                                  *
-* This system is for the use of authorized users only.  Usage of   *
-* this system may be monitored and recorded by system personnel.   *
-*                                                                  *
-* Anyone using this system expressly consents to such monitoring   *
-* and is advised that if such monitoring reveals possible          *
-* evidence of criminal activity, system personnel may provide the  *
-* evidence from such monitoring to law enforcement officials.      *
-*                                                                  *
-********************************************************************
+if [[ ! -f /etc/issue.ssh ]]; then
+    cat > /etc/issue.ssh << 'HEREDOC'
+           WARNING ** WARNING ** WARNING ** WARNING ** WARNING
+
+This is a U.S. Government computer system, which may be accessed and used
+only for authorized Government business by authorized personnel.
+Unauthorized access or use of this computer system may subject violators to
+criminal, civil, and/or administrative action.  All information on this
+computer system may be intercepted, recorded, read, copied, and disclosed by
+and to authorized personnel for official purposes, including criminal
+investigations.  Such information includes sensitive data encrypted to comply
+with confidentiality and privacy requirements.  Access or use of this computer
+system by any person, whether authorized or unauthorized, constitutes consent
+to these terms.  There is no right of privacy in this system.
+
+          WARNING ** WARNING ** WARNING ** WARNING ** WARNING
+
 HEREDOC
 fi
-ensure /etc/ssh/sshd_config '^#*\s*Banner ' 'Banner /etc/issue.net'
+ensure /etc/ssh/sshd_config '^\s*Banner ' 'Banner /etc/issue.ssh'
 printf "\n\n"
 
 # Reload configuration for changes to take effect
