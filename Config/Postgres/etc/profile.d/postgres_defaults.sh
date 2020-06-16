6dadd3e2de71f5de21e76320ed0f7fdf254cdd8f
Dmitriy "DK" Korobskiy <dk@nete.com>
2020-06-16 11:04:30 -0400

    Tweak `logrotate` config

--- Config/Postgres/etc/profile.d/postgres_defaults.sh
+++ Config/Postgres/etc/profile.d/postgres_defaults.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 export PGDATA=/var/lib/pgsql/12/data
 
-## Connection Defaults ##
-#export PGHOST=localhost
+## Connection Defaults: use sockets and the logged in user name to connect ##
 export PGDATABASE=ernie
-#export PGUSER=pardi_admin
