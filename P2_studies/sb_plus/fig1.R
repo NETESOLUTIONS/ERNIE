ed5fe5fc0e21c59cb11ae599064a2220dc9ecc2d
George Chacko <chackoge@gmail.com>
2020-06-20 16:54:06 -0500

    fig1.R script for sb_plus

--- P2_studies/sb_plus/fig1.R
+++ P2_studies/sb_plus/fig1.R
@@ -1,5 +1,14 @@
 # Plot ECDF for 940 million co-cited pairs
 # George Chacko 6/20/2020
+# Data were generated on by merging the files
+# sbp_1985.csv, sb_plus_batch_chop_1.csv, sb_plus_batch_chop_2.csv
+# sb_plus_batch_chop_3.csv, sb_plus_batch_chop_4.csv, sb_plus_batch_chop_5.csv
+# sb_plus_batch_chop_6.csv, sb_plus_batch_chop_7.csv, sb_plus_batch_chop_8.csv
+# sb_plus_batch_chop_8.csv to create all940.csv
+# Look in /erniedev_data3/sb_plus
+# The merging stragey was to read the files into a list and
+# use rbindlist to create a single data.table
+
 setwd('~/Desktop/sb_plus')
 rm(list=ls())
 library(data.table; library(ggplot2)
