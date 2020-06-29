9064630a1dc718dfacdd3f4950c950a2a1afbec3
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-06-28 22:34:46 -0400

    Update jsd_random.py

--- P2_studies/theta_plus/jsd_random.py
+++ P2_studies/theta_plus/jsd_random.py
@@ -33,7 +33,7 @@ engine = create_engine(sql_scheme)
 # p = mp.Pool(mp.cpu_count())
 p = mp.Pool(6)
 
-tmp_dir_list = ['imm1985']
+tmp_dir_list = ['imm1985_1995']
 for dir_name in tmp_dir_list:
 #for dir_name in dir_list:    
     print(f'Working on {dir_name}')
