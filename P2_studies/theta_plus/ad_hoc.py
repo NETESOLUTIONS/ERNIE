28d0c8d2c12168b9ea94b8f0fbe5942894f9f84d
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-07-07 10:03:49 -0400

    update pipeline

--- P2_studies/theta_plus/ad_hoc.py
+++ P2_studies/theta_plus/ad_hoc.py
@@ -43,7 +43,9 @@ for i in year_list:
 
         df2[i.name][j.name] = df[i.name][j.name][0]/df[i.name][j.name][1]
         
-        
+# ------------------------------------------------------------------------------------------- #      
+
+
 # Altering existing table names        
         
 import psycopg2
@@ -66,7 +68,9 @@ for dir_name in tmp_dir_list:
     query = 'ALTER TABLE IF EXISTS ' + table_name + ' RENAME TO ' + new_table_name + ';'
     curs.execute(query)
     
-    
+
+# ------------------------------------------------------------------------------------------- #      
+
 # Get a sample from a shuffled cluster-scp list
 
 import pandas as pd
@@ -79,9 +83,21 @@ sql_scheme = 'postgresql://' + user_name + ':' + password + '@localhost:5432/ern
 engine = create_engine(sql_scheme)
 year = '1990'
 shuffled_file_name = rootdir + '/imm' + year + '/dump.imm' + year + '_citing_cited_shuffled_1million.I20.csv'
+# shuffled_file_name = "mcl/immunology/eleven_years/dump.imm1990_citing_cited_shuffled_1million.I20.csv"
 shuffled_file = pd.read_csv(shuffled_file_name)
 grouped_shuffled_file = shuffled_file.groupby('cluster_no', as_index=False).agg('count').rename(columns={'scp':'cluster_counts'})
-sample = grouped_shuffled_file[(grouped_shuffled_file['cluster_counts'] >= 30) & (grouped_shuffled_file['cluster_counts'] <= 350) ].sample(n=1000, random_state=2020)['cluster_no'].to_list()
+grouped_sample = grouped_shuffled_file[(grouped_shuffled_file['cluster_counts'] >= 30) & (grouped_shuffled_file['cluster_counts'] <= 350) ].sample(n=1000, random_state=2020).sort_values(by="cluster_no", ignore_index=True)
+grouped_sample['temp_cluster_no'] = grouped_sample.index + 1
+sample = grouped_sample['cluster_no'].to_list()
 shuffled_sample = shuffled_file[shuffled_file['cluster_no'].isin(sample)]
-save_name = 'imm' + year + '_cluster_scp_list_shuffled_sample'
-shuffled_sample.to_sql(save_name, con=engine, schema=schema, index=False, if_exists='fail')
+shuffled_sample = shuffled_sample.merge(grouped_sample[['cluster_no', 'temp_cluster_no']], how='left')
+shuffled_sample = shuffled_sample.rename(columns={'cluster_no':'1990_cluster_no', 'temp_cluster_no':'cluster_no'})
+# jsd_compute.py takes cluster numbers and not index values, 
+# so, we need to change the cluster number to follow an order
+save_name_reindexed = 'imm' + year + '_cluster_scp_list_shuffled_sample_reindexed'
+save_name_original = 'imm' + year + '_cluster_scp_list_shuffled_sample_original'
+shuffled_sample[['cluster_no', 'scp']].to_sql(save_name_reindexed, con=engine, schema=schema, index=False, if_exists='fail')
+shuffled_sample[['1990_cluster_no', 'scp']].to_sql(save_name_original, con=engine, schema=schema, index=False, if_exists='fail')
+
+
+# ------------------------------------------------------------------------------------------- #      
