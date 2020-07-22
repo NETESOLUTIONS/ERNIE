9ac3dd158d4c03a2987e82e880ab028e7b014c2e
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-07-22 13:47:16 -0400

    update rated_to_graclus

--- P2_studies/theta_plus/get_rated_mcl_graclus_all_data.py
+++ P2_studies/theta_plus/get_rated_mcl_graclus_all_data.py
@@ -2,25 +2,24 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sys import argv
 
-user_name = argv[1]
-password = argv[2]
+user_name = "shreya"
+password = "Akshay<3"
 schema = "theta_plus"
 sql_scheme = 'postgresql://' + user_name + ':' + password + '@localhost:5432/ernie'
 engine = create_engine(sql_scheme)
 
 
-"""
-DROP TABLE IF EXISTS rated_set;
-CREATE TABLE rated_set AS
-SELECT expert_rating, imm1985_1995_cluster_no, s.current_cluster_size AS imm1985_1995_cluster_size, 
+main_table_query = """
+SELECT er.expert_rating, er.imm1985_1995_cluster_no, s.current_cluster_size AS imm1985_1995_cluster_size, 
        s.match_year, s.match_cluster_no mcl_year_cluster_no
-FROM expert_ratings
-JOIN superset_to_year_match_30_350 s
-  ON expert_ratings.imm1985_1995_cluster_no = s.current_cluster_number;
+FROM theta_plus.expert_ratings er
+JOIN theta_plus.superset_to_year_match_30_350 s
+  ON er.imm1985_1995_cluster_no = s.current_cluster_number;
 """
 
-main_table_name = 'rated_set'
-main_table = pd.read_sql_table(main_table_name, schema=schema, con=engine)
+
+main_table = pd.read_sql(main_table_query, con=engine)
+main_table.name = 'rated_mcl_graclus_all_data'
 
 main_table['mcl_year_cluster_size'] = None
 main_table['mcl_year_conductance'] = None
@@ -64,7 +63,8 @@ main_table['graclus_half_mclsize_max_article_score'] = None
 main_table['graclus_half_mclsize_median_article_score'] = None
 
 
-print(f'Working on table: {schema}.{main_table_name}')
+print(f'Working on table: {schema}.{main_table.name}')
+print(f'The size of the table is {len(main_table)}')
 for i in range(len(main_table)):
     
     match_year = 'imm' +  str(main_table.at[ i, 'match_year'])
@@ -99,7 +99,7 @@ for i in range(len(main_table)):
     graclus_half_mclsize_multiple_options = graclus_half_mclsize_cluster_no_table.at[0, 'multiple_options']
 
     graclus_half_mclsize_table_name = match_year + "_all_merged_graclus_half_mclsize"
-    graclus_half_mclsize_query = "SELECT conductance, int_edges, boundary, sum_article_score, max_article_score, median_article_score FROM theta_plus." + graclus_half_mclsize_table_name + " WHERE cluster_no=" + graclus_half_mclsize_cluster_no + ";"
+    graclus_half_mclsize_query = "SELECT coherence, conductance, int_edges, boundary, sum_article_score, max_article_score, median_article_score FROM theta_plus." + graclus_half_mclsize_table_name + " WHERE cluster_no=" + graclus_half_mclsize_cluster_no + ";"
     
     mcl_year_table = pd.read_sql(mcl_year_query, con=engine)
     graclus_100_table = pd.read_sql(graclus_100_query, con=engine)
@@ -139,12 +139,14 @@ for i in range(len(main_table)):
     main_table.at[i,'graclus_half_mclsize_multiple_options'] = graclus_half_mclsize_multiple_options 
     
     main_table.at[i,'graclus_half_mclsize_conductance'] = graclus_half_mclsize_table.at[0, 'conductance']
-    main_table.at[i,'graclus_half_mclsize_coherence'] = None
+    main_table.at[i,'graclus_half_mclsize_coherence'] = graclus_half_mclsize_table.at[0, 'coherence']
     main_table.at[i,'graclus_half_mclsize_int_edges'] = graclus_half_mclsize_table.at[0, 'int_edges']
     main_table.at[i,'graclus_half_mclsize_boundary'] = graclus_half_mclsize_table.at[0, 'boundary']
     main_table.at[i,'graclus_half_mclsize_sum_article_score'] = graclus_half_mclsize_table.at[0, 'sum_article_score']
     main_table.at[i,'graclus_half_mclsize_max_article_score'] = graclus_half_mclsize_table.at[0, 'max_article_score']
     main_table.at[i,'graclus_half_mclsize_median_article_score'] = graclus_half_mclsize_table.at[0, 'median_article_score']
 
-main_table.to_sql(main_table_name, schema=schema, con=engine, if_exists='replace', index=False)
+print("Done updating table.")
+main_table = main_table.astype(float)
+main_table.to_sql('rated_mcl_graclus_all_data', schema=schema, con=engine, if_exists='replace', index=False)
 print("All completed.")
