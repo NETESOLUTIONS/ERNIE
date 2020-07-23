981b203d2382337fecd6ba6811698c97b01e0b5d
Wenxi Zhao <50091137+WenxiZhao126@users.noreply.github.com>
2020-07-23 13:25:33 -0400

    Update graclus_input_data_tp.sql

--- P2_studies/comp_case_study/graclus_input_data_tp.sql
+++ P2_studies/comp_case_study/graclus_input_data_tp.sql
@@ -9,7 +9,7 @@
 \set dataset_graclus_data 'graclus_':dataset
 \set dataset_graclus_coded 'graclus_coded_':dataset
 
-SET SEARCH_PATH TO sbplus;
+SET SEARCH_PATH TO sb_plus;
 
 DROP TABLE IF EXISTS :dataset_graclus_data;
 CREATE TABLE :dataset_graclus_data TABLESPACE theta_plus_tbs AS
