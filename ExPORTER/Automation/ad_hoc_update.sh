7f9fa49625ada24159eb1a18cbf9ecb2b44e1740
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-08-07 06:32:32 -0400

    Update ad_hoc_update.sh

--- ExPORTER/Automation/ad_hoc_update.sh
+++ ExPORTER/Automation/ad_hoc_update.sh
@@ -1,29 +1,89 @@
-# EXPORTER Project manual download and update
+#!/usr/bin/env bash
+if [[ $1 == "-h" ]]; then
+  cat <<'HEREDOC'
+NAME
 
-# Author: Shreya Chandrasekharan
-# Date: 08/05/2020
+   ad_hoc_update.sh -- EXPORTER Project manual download and update
+                       Download yearly ExPORTER project and abstract CSVs from exporter.nih.gov
+                       and update in the PostgreSQL databased as needed
 
-# download the yearly RePORTER file and uncompress it
-c_dir=$1
-cd $c_dir
-PRJLINK=$2
-PRJABSLINK=$3
+SYNOPSIS
 
-# Example Project Link: https://exporter.nih.gov/CSVs/final/RePORTER_PRJ_C_FY2019.zip
-# Example Project Abstract Link: https://exporter.nih.gov/CSVs/final/RePORTER_PRJABS_C_FY2019.zip
+   ad_hoc_update.s [ -w data_directory ] [ -P project_link] [ -A abstract_link ]
 
-wget -q "$PRJLINK" --no-check-certificate
+   ad_hoc_update.s -h: display this help
+
+DESCRIPTION
+
+   Download zip files into the working directory and update the database
+   The following option(s) is available:
+
+    -w  work_dir          parent directory of directory where the ZIP files are stored
+    -P  project_link      yearly project CSV download link
+                          # Example Project Link: https://exporter.nih.gov/CSVs/final/RePORTER_PRJ_C_FY2019.zip
+    -A  abstract_link      yearly project abstract CSV download link
+                          # Example Project Abstract Link: https://exporter.nih.gov/CSVs/final/RePORTER_PRJABS_C_FY2019.zip
+                          
+HEREDOC
+  exit 1
+fi
+
+if [[ ! -f /anaconda3/bin/python ]]; then
+    echo "/anaconda3/bin/python does not exist."
+    exit 1
+fi
+
+set -e
+set -o pipefail
+
+while (( $# > 0 )); do
+  echo "Using CLI arg '$1'"
+  case "$1" in
+    -w)
+      shift
+      WORK_DIR=$1
+      ;;
+    -P)
+      shift
+      PRJ_LINK=$1
+      ;;
+    -A)
+      shift
+      ABS_LINK=$1
+      ;;
+    *)
+      break
+  esac
+  shift
+done
+
+cd ${WORK_DIR}
+echo -e "\n## Running under ${USER}@${HOSTNAME} in ${PWD} ##"
+
+# Download Zip files in a local storage directory
+
+mkdir -p RePORTER_downloads
+cd RePORTER_downloads
+# Download the yearly RePORTER file and uncompress it
+echo "Downloading..."
+
+wget -q "${PRJ_LINK}" --no-check-certificate
 if [ $? -ne 0 ]; then
-  echo "Error: No CSV Download Available for year : $PRJLINK";
-  echo "Please perform a manual check of the Exporter website and manually update counter file if necessary"
+  echo "No CSV Download Available for the link"
+  echo "Please perform a manual check of the Exporter website"
+  break
   exit 1;
 fi
-wget -q "$PRJABSLINK" --no-check-certificate
-for file in $(ls $c_dir/*.zip); do unzip $file ; done
+
+wget -q "${ABS_LINK}" --no-check-certificate
+
+echo "Download complete"
+
+for file in $(ls *.zip); do unzip $file ; done
 
 # pass the download to the python script to extract the proper columns, then copy the data into the temp table
 file=$(ls RePORTER_PRJ_C*.csv)
-python $c_dir/column_extractor.py $file; file_extract=$(echo $file | sed s/.csv/_EXTRACTED.csv/g); file_extract=${c_dir}"/"${file_extract}
+python ../column_extractor.py $file; file_extract=$(echo $file | sed s/.csv/_EXTRACTED.csv/g); file_extract=${WORK_DIR}"/"RePORTER_downloads"/"${file_extract}
 # truncate the temp project table: temp_exporter_projects. Then update the table with data from the extract
 psql ernie -c "TRUNCATE TABLE temp_exporter_projects;"
 psql ernie -c "COPY temp_exporter_projects from '${file_extract}' delimiter ',' CSV HEADER;"
@@ -32,20 +92,23 @@ psql ernie -c "COPY temp_exporter_projects from '${file_extract}' delimiter ',' 
 psql ernie -c "INSERT INTO exporter_projects SELECT * from temp_exporter_projects ON CONFLICT ON CONSTRAINT exporter_projects_pk DO UPDATE SET APPLICATION_ID=excluded.APPLICATION_ID, ACTIVITY=excluded.ACTIVITY, ADMINISTERING_IC=excluded.ADMINISTERING_IC, APPLICATION_TYPE=excluded.APPLICATION_TYPE, ARRA_FUNDED=excluded.ARRA_FUNDED, AWARD_NOTICE_DATE=excluded.AWARD_NOTICE_DATE, BUDGET_START=excluded.BUDGET_START, BUDGET_END=excluded.BUDGET_END, CFDA_CODE=excluded.CFDA_CODE, CORE_PROJECT_NUM=excluded.CORE_PROJECT_NUM, ED_INST_TYPE=excluded.ED_INST_TYPE, FOA_NUMBER=excluded.FOA_NUMBER, FULL_PROJECT_NUM=excluded.FULL_PROJECT_NUM, SUBPROJECT_ID=excluded.SUBPROJECT_ID, FUNDING_ICs=excluded.FUNDING_ICs, FY=excluded.FY, IC_NAME=excluded.IC_NAME, NIH_SPENDING_CATS=excluded.NIH_SPENDING_CATS, ORG_CITY=excluded.ORG_CITY, ORG_COUNTRY=excluded.ORG_COUNTRY, ORG_DEPT=excluded.ORG_DEPT, ORG_DISTRICT=excluded.ORG_DISTRICT, ORG_DUNS=excluded.ORG_DUNS, ORG_FIPS=excluded.ORG_FIPS, ORG_NAME=excluded.ORG_NAME, ORG_STATE=excluded.ORG_STATE, ORG_ZIPCODE=excluded.ORG_ZIPCODE, PHR=excluded.PHR, PI_IDS=excluded.PI_IDS, PI_NAMEs=excluded.PI_NAMEs, PROGRAM_OFFICER_NAME=excluded.PROGRAM_OFFICER_NAME, PROJECT_START=excluded.PROJECT_START, PROJECT_END=excluded.PROJECT_END, PROJECT_TERMS=excluded.PROJECT_TERMS, PROJECT_TITLE=excluded.PROJECT_TITLE, SERIAL_NUMBER=excluded.SERIAL_NUMBER, STUDY_SECTION=excluded.STUDY_SECTION, STUDY_SECTION_NAME=excluded.STUDY_SECTION_NAME, SUFFIX=excluded.SUFFIX, SUPPORT_YEAR=excluded.SUPPORT_YEAR, TOTAL_COST=excluded.TOTAL_COST, TOTAL_COST_SUB_PROJECT=excluded.TOTAL_COST_SUB_PROJECT;"
 
 #update the abstracts table
-file=$(ls RePORTER_PRJABS_C*.csv); file=${c_dir}"/"${file}
+file=$(ls RePORTER_PRJABS_C*.csv); file=${WORK_DIR}"/"RePORTER_downloads"/"${file}
 psql ernie -c "TRUNCATE TABLE temp_exporter_project_abstracts;"
 psql ernie -c "COPY temp_exporter_project_abstracts from '${file}' delimiter ',' CSV HEADER encoding 'latin1';"
-psql ernie -c "INSERT INTO exporter_project_abstracts 
-              SELECT * from temp_exporter_project_abstracts 
-              ON CONFLICT ON CONSTRAINT exporter_project_abstracts_pk 
-              DO UPDATE SET APPLICATION_ID=excluded.APPLICATION_ID, ABSTRACT_TEXT=excluded.ABSTRACT_TEXT;"
-
-
+psql ernie -c "INSERT INTO exporter_project_abstracts SELECT * from temp_exporter_project_abstracts ON CONFLICT ON CONSTRAINT exporter_project_abstracts_pk DO UPDATE SET APPLICATION_ID=excluded.APPLICATION_ID, ABSTRACT_TEXT=excluded.ABSTRACT_TEXT;"
 
 #move the CSV files into storage and clean zip files out
-[ -d $c_dir/csv_files ] || mkdir -p $c_dir/csv_files
-mv $file_extract $c_dir/csv_files; mv $file $c_dir/csv_files; rm $c_dir/*.csv ; rm $c_dir/*.zip
+[ -d ../csv_files ] || mkdir -p ../csv_files
+mv $file_extract ../csv_files; mv $file ../csv_files; rm *.csv ; rm *.zip
 
 ## update the exporter log
 
-psql -f exporter_update_log.sql
+psql -f ../exporter_update_log.sql
+
+
+echo "Update complete."
+
+
+
+
+
