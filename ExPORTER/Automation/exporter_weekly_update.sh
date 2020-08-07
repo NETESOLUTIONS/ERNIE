08e3e3a7a9cb55b05eb41062a464280b32477fe4
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-08-07 04:53:38 -0400

    Update exporter_weekly_update.sh

--- ExPORTER/Automation/exporter_weekly_update.sh
+++ ExPORTER/Automation/exporter_weekly_update.sh
@@ -79,7 +79,7 @@ do
     python ../column_extractor.py $file; file_extract=$(echo $file | sed s/.csv/_EXTRACTED.csv/g); file_extract=${WORK_DIR}"/"RePORTER_downloads"/"${file_extract}
     # truncate the temp project table: temp_exporter_projects. Then update the table with data from the extract
     psql ernie -c "TRUNCATE TABLE temp_exporter_projects;"
-    psql ernie -c " COPY temp_exporter_projects from '${file_extract}' DELIMITER ',' FORMAT CSV HEADER ON QUOTE '"'; "
+    psql ernie -c " COPY temp_exporter_projects from '${file_extract}' DELIMITER ',' CSV HEADER QUOTE '"'; "
     # merge the temp table into the main table, update old records with new records based on pk match
     # insert on conflict update
     psql ernie -c "INSERT INTO exporter_projects SELECT * from temp_exporter_projects ON CONFLICT ON CONSTRAINT exporter_projects_pk DO UPDATE SET APPLICATION_ID=excluded.APPLICATION_ID, ACTIVITY=excluded.ACTIVITY, ADMINISTERING_IC=excluded.ADMINISTERING_IC, APPLICATION_TYPE=excluded.APPLICATION_TYPE, ARRA_FUNDED=excluded.ARRA_FUNDED, AWARD_NOTICE_DATE=excluded.AWARD_NOTICE_DATE, BUDGET_START=excluded.BUDGET_START, BUDGET_END=excluded.BUDGET_END, CFDA_CODE=excluded.CFDA_CODE, CORE_PROJECT_NUM=excluded.CORE_PROJECT_NUM, ED_INST_TYPE=excluded.ED_INST_TYPE, FOA_NUMBER=excluded.FOA_NUMBER, FULL_PROJECT_NUM=excluded.FULL_PROJECT_NUM, SUBPROJECT_ID=excluded.SUBPROJECT_ID, FUNDING_ICs=excluded.FUNDING_ICs, FY=excluded.FY, IC_NAME=excluded.IC_NAME, NIH_SPENDING_CATS=excluded.NIH_SPENDING_CATS, ORG_CITY=excluded.ORG_CITY, ORG_COUNTRY=excluded.ORG_COUNTRY, ORG_DEPT=excluded.ORG_DEPT, ORG_DISTRICT=excluded.ORG_DISTRICT, ORG_DUNS=excluded.ORG_DUNS, ORG_FIPS=excluded.ORG_FIPS, ORG_NAME=excluded.ORG_NAME, ORG_STATE=excluded.ORG_STATE, ORG_ZIPCODE=excluded.ORG_ZIPCODE, PHR=excluded.PHR, PI_IDS=excluded.PI_IDS, PI_NAMEs=excluded.PI_NAMEs, PROGRAM_OFFICER_NAME=excluded.PROGRAM_OFFICER_NAME, PROJECT_START=excluded.PROJECT_START, PROJECT_END=excluded.PROJECT_END, PROJECT_TERMS=excluded.PROJECT_TERMS, PROJECT_TITLE=excluded.PROJECT_TITLE, SERIAL_NUMBER=excluded.SERIAL_NUMBER, STUDY_SECTION=excluded.STUDY_SECTION, STUDY_SECTION_NAME=excluded.STUDY_SECTION_NAME, SUFFIX=excluded.SUFFIX, SUPPORT_YEAR=excluded.SUPPORT_YEAR, TOTAL_COST=excluded.TOTAL_COST, TOTAL_COST_SUB_PROJECT=excluded.TOTAL_COST_SUB_PROJECT;"
@@ -87,7 +87,7 @@ do
     #update the abstracts table
     file=$(ls RePORTER_PRJABS_C*.csv); file=${WORK_DIR}"/"RePORTER_downloads"/"${file}
     psql ernie -c "TRUNCATE TABLE temp_exporter_project_abstracts;"
-    psql ernie -c """COPY temp_exporter_project_abstracts from '${file}' DELIMITER ',' FORMAT CSV HEADER ON QUOTE '"'  ENCODING 'latin1';"""
+    psql ernie -c """COPY temp_exporter_project_abstracts from '${file}' DELIMITER ',' CSV HEADER QUOTE '"'  ENCODING 'latin1';"""
     psql ernie -c "INSERT INTO exporter_project_abstracts SELECT * from temp_exporter_project_abstracts ON CONFLICT ON CONSTRAINT exporter_project_abstracts_pk DO UPDATE SET APPLICATION_ID=excluded.APPLICATION_ID, ABSTRACT_TEXT=excluded.ABSTRACT_TEXT;"
 
     #move the CSV files into storage and clean zip files out
