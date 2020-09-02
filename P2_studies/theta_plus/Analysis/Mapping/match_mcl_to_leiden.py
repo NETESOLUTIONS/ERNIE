84aaea196b24c03bfc1becdc23c46f58fc6e1ec2
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-09-01 23:55:54 -0400

    Create match_mcl_to_leiden.py

---
+++ P2_studies/theta_plus/Analysis/Mapping/match_mcl_to_leiden.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import mapping_module as mm
+import multiprocessing as mp
+from sqlalchemy import create_engine
+from sys import argv
+
+user_name = argv[1]
+password = argv[2]
+data_type = argv[3]
+start_year = int(argv[4])
+end_year = int(argv[5])
+leiden_input = argv[6] #quality_func_Res --> CPM_R001
+rootdir = argv[7] # "/erniedev_data3/theta_plus/Leiden/"
+schema = "theta_plus"
+sql_scheme = 'postgresql://' + user_name + ':' + password + '@localhost:5432/ernie'
+# engine = create_engine(sql_scheme)
+
+data_name = data_type + str(start_year) + '_' + str(end_year)
+# mcl_name = data_name + '_cluster_scp_list_unshuffled'
+
+mcl_name = data_name + '_cluster_scp_list_unshuffled.csv'
+leiden_name = data_name + '_cluster_scp_list_leiden_' + leiden_input + '.csv'
+# mcl = pd.read_sql_table(table_name= mcl_name, schema=schema, con=engine)
+mcl = pd.read_csv(mcl_name)
+leiden = pd.read_csv(leiden_name)
+print("Running...")
+p = mp.Pool(6)
+final_df = pd.DataFrame()
+for mcl_cluster_no in range(1, len(mcl)):
+
+    match_dict = p.starmap(mm.match_mcl_to_leiden, [(mcl_cluster_no, mcl, leiden)])
+    match_df = pd.DataFrame.from_dict(match_dict)
+    final_df = final_df.append(match_df, ignore_index=True)
+
+save_name = rootdir + '/' + data_name  + '_match_to_leiden' + leiden_input + '.csv'
+final_df.to_csv(save_name, index = None, header=True, encoding='utf-8')
+
+## In case the connection times out:
+# engine = create_engine(sql_scheme)
+# save_name_sql = mcl_name + '_match_to_leiden'
+# final_df.to_sql(save_name_sql, con=engine, schema=schema, index=False, if_exists='fail')
+
+print("")
+print("All Completed.")    
