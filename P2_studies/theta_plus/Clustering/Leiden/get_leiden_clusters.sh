2b691c2ee3c7daaa3f75e326c39aa1e47cc7e4b2
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-09-02 12:34:11 -0400

    update documentation

--- P2_studies/theta_plus/Clustering/Leiden/get_leiden_clusters.sh
+++ P2_studies/theta_plus/Clustering/Leiden/get_leiden_clusters.sh
@@ -3,15 +3,46 @@ if [[ $1 == "-h" ]]; then
   cat <<'HEREDOC'
 NAME
 
-   
+    get_leiden_clusters.sh - Takes input data name and Leiden parameters to output 
+                            a CSV file of Leiden clusters along with SCPS
 
 SYNOPSIS
 
-  
+    get_leiden_clusters.sh [ -s schema ] [ -t year_table ] [ -w working_dir ] [ -q quality_func ] 
+                         [ -r1 start_res ] [ -r2 stop_res ] [ -r step_res ] [ -l seed ]
+                         
+    get_leiden_clusters.sh -h : display this help
 
 DESCRIPTION
 
-   
+    For the data name (year_table e.g., imm1985) supplied, get unique undirected edges and copy
+    the table along with its corresponding nodes table to working directory. Convert the nodes
+    to 0-indexed integers and save edge output (Leiden input) as a tab-separated file. Run the
+    Leiden algrithm with required parameters using this output and save leiden output. Convert
+    the Leiden output into SCP form and save corresponding cluster-SCP CSV. The standard output 
+    also displays cluster size distribtuion information.
+    
+    The Leiden resolution parameter is set to 1.0 by default and can be changed to any float value
+    by using the -r1 option. This script also allows the user to input a desired range of values
+    for the resolution parameter, if needed, by using -r1 (lower bound, inclusive) -r2 (upper bound, 
+    inclusive), and -r3 (step value). The quality function is set to CPM by default.
+    
+    The following options are available:
+    
+    -s   schema           schema where the edge and node tables are stored
+    -t   year_table       name of the data type (e.g. 'imm') and year
+    -w   working_dir      directory where all output is stored
+    -q   quality_func     quality function to be used by the Leiden algorithm.
+                          Default is CPM.
+    -r1  start_res        Resolution parameter to be used for the Leiden algorithm.
+                          Enter a float - e.g. 1.0
+                          If testing a range of parameters, lower-bound of resolution.
+    -r2  stop_res         Upper-bound of range of resolution parameters
+                          Enter a float - e.g. 1.0
+    -r3  step_res         Step value for range of resolution parameters
+    -l   seed             Seed value for the Leiden algorithm
+                          Set to 2020 by default
+    
 
 HEREDOC
   exit 1
@@ -19,6 +50,13 @@ fi
 
 set -e
 set -o pipefail
+
+declare r1="1.0"
+declare r2=$r1
+declare r3="1.0"
+declare quality_func="CPM"
+script_dir="/erniedev_data3/theta_plus/Leiden"
+declare -i seed=2020
 
 while (( $# > 0 )); do
   case "$1" in
@@ -60,6 +98,7 @@ while (( $# > 0 )); do
   shift
 done
 
+cd ${working_dir}
 
 # Call script that creates table with unique undirected edges
 
@@ -67,7 +106,7 @@ citing_cited_table=${year_table}'_citing_cited'
 
 echo "Getting unique undirected edges of ${citing_cited_table} ..."
 
-psql -f get_unique_pairs_citing_cited.sql -v schema=${schema} -v citing_cited_table=${citing_cited_table}
+psql -f ${script_dir}/get_unique_pairs_citing_cited.sql -v schema=${schema} -v citing_cited_table=${citing_cited_table}
 
 # Copy table to working directory as CSV
 citing_cited_unique_pairs_table=${citing_cited_table}'_unique_pairs'
@@ -86,7 +125,7 @@ HEREDOC
 
 echo "Converting nodes to 0-indexed values..."
 
-python convert_to_leiden_input.py ${year_table} ${citing_cited_unique_pairs_table} ${year_nodes_table}
+python ${script_dir}/convert_to_leiden_input.py ${year_table} ${citing_cited_unique_pairs_table} ${year_nodes_table}
 
 leiden_input=${year_table}'_leiden_input.txt'
 nodes_coded=${year_nodes_table}'_coded.csv'
@@ -105,11 +144,12 @@ do
     
     leiden_output=${year_table}'_cluster_scp_list_leiden_'${quality_func}'_'${res_suffix}'.txt'
 
-    java -jar /usr/local/bin/RunNetworkClustering.jar -q ${quality_func} -r ${i} --seed ${seed} -o ${leiden_output} ${leiden_input}
+    java -jar /usr/local/bin/RunNetworkClustering.jar -q ${quality_func} -a "Leiden" -r ${i} --seed ${seed} -o ${leiden_output} ${leiden_input}
     
-    python convert_to_cluster_output.py ${leiden_output} ${nodes_coded}
+    python ${script_dir}/convert_to_cluster_output.py ${leiden_output} ${nodes_coded}
     
     echo "Done with Resolution $i"
+    echo ""
 
 done
 
