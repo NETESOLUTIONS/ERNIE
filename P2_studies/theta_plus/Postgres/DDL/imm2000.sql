9330d289468d6033aa11dadf8f4c30d7d0556c68
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-09-06 20:51:33 -0400

    add imm DDL

---
+++ P2_studies/theta_plus/Postgres/DDL/imm2000.sql
@@ -0,0 +1,128 @@
+-- Immunology DDL for the year 2000
+
+SET search_path = theta_plus;
+
+-- Get all immunology articles published in the year 2000
+DROP TABLE IF EXISTS theta_plus.imm2000;
+
+CREATE TABLE theta_plus.imm2000
+TABLESPACE theta_plus_tbs AS
+    SELECT sp.scp 
+    FROM public.scopus_publications sp
+    INNER JOIN public.scopus_publication_groups spg
+        ON sp.scp=spg.sgr
+        AND spg.pub_year=2000
+        AND sp.citation_type='ar'
+        AND sp.citation_language='English'
+        AND sp.pub_type='core'
+    INNER JOIN public.scopus_classes sc
+        ON sp.scp=sc.scp
+        AND sc.class_code='2403';
+
+CREATE INDEX IF NOT EXISTS imm2000_idx
+ON theta_plus.imm2000(scp)
+TABLESPACE index_tbs;
+
+-- Get all the cited references of the immunology articles published in 2000
+DROP TABLE IF EXISTS theta_plus.imm2000_cited;
+
+CREATE TABLE theta_plus.imm2000_cited
+TABLESPACE theta_plus_tbs AS
+    SELECT tp.scp as citing, sr.ref_sgr AS cited
+    FROM theta_plus.imm2000 tp
+    INNER JOIN public.scopus_references sr 
+        ON tp.scp = sr.scp;
+
+CREATE INDEX IF NOT EXISTS imm2000_cited_idx
+ON theta_plus.imm2000_cited(citing,cited)
+TABLESPACE index_tbs;
+
+-- Get all the citing references of the immunology articles published in 2000
+DROP TABLE IF EXISTS theta_plus.imm2000_citing;
+
+CREATE TABLE theta_plus.imm2000_citing 
+TABLESPACE theta_plus_tbs AS
+    SELECT sr.scp as citing, tp.scp AS cited 
+    FROM theta_plus.imm2000 tp
+    INNER JOIN public.scopus_references sr 
+        ON tp.scp=sr.ref_sgr;
+
+CREATE INDEX IF NOT EXISTS imm2000_citing_idx 
+ON theta_plus.imm2000_citing(citing,cited)
+TABLESPACE index_tbs;
+
+-- Create table from the union of cited and citing references
+DROP TABLE IF EXISTS theta_plus.imm2000_citing_cited;
+
+CREATE TABLE theta_plus.imm2000_citing_cited
+TABLESPACE theta_plus_tbs AS
+    SELECT DISTINCT citing,cited from imm2000_cited 
+    UNION
+    SELECT DISTINCT citing,cited from imm2000_citing;
+    
+-- clean up Scopus data
+DELETE FROM theta_plus.imm2000_citing_cited
+WHERE citing=cited;
+
+--remove all non-core publications by joining against
+-- scopus publications and requiring type = core
+-- and language = English
+DROP TABLE IF EXISTS XX_imm2000;
+
+ALTER TABLE theta_plus.imm2000_citing_cited
+RENAME TO XX_imm2000;
+
+CREATE TABLE theta_plus.imm2000_citing_cited AS
+
+WITH cte AS(SELECT citing,cited FROM XX_imm2000
+            INNER JOIN public.scopus_publications sp
+                ON XX_imm2000.citing=sp.scp
+                AND sp.citation_language='English'
+                AND sp.pub_type='core')
+
+SELECT citing,cited FROM cte
+    INNER JOIN public.scopus_publications sp2
+    ON cte.cited=sp2.scp
+    AND sp2.citation_language='English'
+    AND sp2.pub_type='core';
+
+DROP TABLE XX_imm2000;
+
+-- Get all nodes in the 2000 dataset
+DROP TABLE IF EXISTS theta_plus.imm2000_nodes;
+
+CREATE TABLE theta_plus.imm2000_nodes
+TABLESPACE theta_plus_tbs AS
+    SELECT distinct citing as scp
+    FROM theta_plus.imm2000_citing_cited
+    UNION
+    SELECT distinct cited
+    FROM theta_plus.imm2000_citing_cited;
+
+CREATE INDEX IF NOT EXISTS imm2000_nodes_idx ON theta_plus.imm2000_nodes(scp);
+
+-- Get all titles and abstracts
+DROP TABLE IF EXISTS theta_plus.imm2000_title_abstracts;
+
+CREATE TABLE theta_plus.imm2000_title_abstracts
+TABLESPACE theta_plus_tbs AS
+    SELECT tpin.scp,st.title,sa.abstract_text
+    FROM theta_plus.imm2000_nodes tpin
+    INNER JOIN public.scopus_titles st 
+        ON tpin.scp=st.scp
+    INNER JOIN public.scopus_abstracts sa 
+        ON tpin.scp=sa.scp
+        AND sa.abstract_language='eng'
+        AND st.language='English';
+
+/*
+-- Merging with the citation counts table
+DROP TABLE IF EXISTS theta_plus.imm2000_citation_counts;
+CREATE TABLE theta_plus.imm2000_citation_counts
+TABLESPACE theta_plus_tbs AS
+SELECT ielu.scp, scc.citation_count, ielu.cluster_no
+FROM theta_plus.imm2000_cluster_scp_list_unshuffled ielu
+LEFT JOIN public.scopus_citation_counts scc
+  ON ielu.scp = scc.scp;
+*/
+
---
+++ P2_studies/theta_plus/Postgres/DDL/imm2000_2004.sql
@@ -0,0 +1,74 @@
+-- Get combined set (superset) of all 5 years of data
+
+SET SEARCH_PATH = theta_plus;
+
+-- create list of scps common to all 5 years (intersection)
+DROP TABLE IF EXISTS theta_plus.imm2000_2004_common_scps;
+CREATE TABLE theta_plus.imm2000_2004_common_scps tablespace theta_plus_tbs AS
+WITH cte AS (SELECT scp FROM imm2000_nodes INTERSECT
+             SELECT scp FROM imm2001_nodes INTERSECT
+             SELECT scp FROM imm2002_nodes INTERSECT
+             SELECT scp FROM imm2003_nodes INTERSECT
+             SELECT scp FROM imm2004_nodes INTERSECT) 
+ SELECT * FROM cte;
+
+CREATE INDEX IF NOT EXISTS imm2000_citing_cited_idx 
+ON theta_plus.imm2000_citing_cited(citing,cited) 
+TABLESPACE index_tbs;
+
+CREATE INDEX IF NOT EXISTS imm2001_citing_cited_idx 
+ON theta_plus.imm2001_citing_cited(citing,cited) 
+TABLESPACE index_tbs;
+
+CREATE INDEX IF NOT EXISTS imm2002_citing_cited_idx 
+ON theta_plus.imm2002_citing_cited(citing,cited) 
+TABLESPACE index_tbs;
+
+CREATE INDEX IF NOT EXISTS imm2003_citing_cited_idx 
+ON theta_plus.imm2003_citing_cited(citing,cited) 
+TABLESPACE index_tbs;
+
+CREATE INDEX IF NOT EXISTS imm2004_citing_cited_idx 
+ON theta_plus.imm2004_citing_cited(citing,cited) 
+TABLESPACE index_tbs;
+
+CREATE INDEX IF NOT EXISTS imm2005_citing_cited_idx 
+ON theta_plus.imm2005_citing_cited(citing,cited) 
+TABLESPACE index_tbs;
+
+
+
+-- create union of edge lists across 5 years
+
+DROP TABLE IF EXISTS theta_plus.imm2000_2004_citing_cited;
+CREATE TABLE theta_plus.imm2000_2004_citing_cited 
+TABLESPACE theta_plus_tbs AS
+    SELECT * FROM imm2000_citing_cited UNION
+    SELECT * FROM imm2001_citing_cited UNION
+    SELECT * FROM imm2002_citing_cited UNION
+    SELECT * FROM imm2003_citing_cited UNION
+    SELECT * FROM imm2004_citing_cited;
+
+DROP TABLE IF EXISTS theta_plus.imm2000_2004_nodes;
+CREATE TABLE theta_plus.imm2000_2004_nodes
+TABLESPACE theta_plus_tbs AS
+    SELECT distinct citing as scp
+    FROM theta_plus.imm2000_2004_citing_cited
+    UNION
+    SELECT distinct cited
+    FROM theta_plus.imm2000_2004_citing_cited;
+    
+CREATE INDEX IF NOT EXISTS imm2000_2004_nodes_idx 
+    ON theta_plus.imm2000_2004_nodes(scp);
+
+DROP TABLE IF EXISTS theta_plus.imm2000_2004_union_title_abstracts;
+CREATE TABLE theta_plus.imm2000_2004_union_title_abstracts
+TABLESPACE theta_plus_tbs AS
+    SELECT tpin.scp, st.title, sa.abstract_text
+    FROM theta_plus.imm2000_2004_nodes tpin
+    INNER JOIN public.scopus_titles st 
+            ON tpin.scp=st.scp
+    INNER JOIN public.scopus_abstracts sa 
+            ON tpin.scp=sa.scp
+            AND sa.abstract_language='eng'
+            AND st.language='English';
