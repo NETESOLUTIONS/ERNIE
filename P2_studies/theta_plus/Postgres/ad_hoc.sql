5193c5d2771826450143fa538d61a9c6e4ec91b0
Shreya C <46837911+c-shreya@users.noreply.github.com>
2020-09-14 15:25:39 -0400

    update tables

--- P2_studies/theta_plus/Postgres/ad_hoc.sql
+++ P2_studies/theta_plus/Postgres/ad_hoc.sql
@@ -148,7 +148,7 @@ LEFT JOIN cte on cte.auid = at.auid) venn
 WHERE venn.auid = imm1985_1995_author_tiers.auid;
 
 
-
+-- imm2000_2004
 -- add cluster number to full graph degrees table
 ALTER TABLE theta_plus.imm2000_2004_full_graph_degrees
 ADD COLUMN mcl_cluster_no BIGINT;
@@ -158,12 +158,113 @@ SET mcl_cluster_no = cslu.cluster_no
 FROM theta_plus.imm2000_2004_cluster_scp_list_unshuffled cslu
 WHERE cslu.scp = theta_plus.imm2000_2004_full_graph_degrees.scp;
 
--- add AUID to full graph degrees table
+-- add AUID to graph degrees tables
 
-CREATE TABLE imm2000_2004_all_authors_full_graph AS
+CREATE TABLE theta_plus.imm2000_2004_all_authors_full_graph AS
     SELECT fgd.*, sa.auid
     FROM theta_plus.imm2000_2004_full_graph_degrees fgd
     JOIN public.scopus_authors sa
-        ON fgd.scp = sa.scp
+        ON fgd.scp = sa.scp;
+
+
+CREATE TABLE theta_plus.imm2000_2004_all_authors_internal AS
+    SELECT icd.*, sa.auid
+    FROM theta_plus.imm2000_2004_internal_cluster_degrees icd
+    JOIN public.scopus_authors sa
+        ON icd.scp = sa.scp;
+
+-- Get cluster number and number of articles per cluster for each author
+CREATE TABLE theta_plus.imm2000_2004_authors_clusters AS
+    SELECT auid, mcl_cluster_no, count(scp) count_articles
+    FROM
+        (SELECT DISTINCT scp, mcl_cluster_no, auid
+        FROM theta_plus.imm2000_2004_all_authors_full_graph ac) distinct_authors
+    GROUP BY auid, mcl_cluster_no
+    ORDER BY auid ASC;
+
+ALTER TABLE imm2000_2004_authors_clusters
+ADD COLUMN count_cited_articles BIGINT;
+
+UPDATE theta_plus.imm2000_2004_authors_clusters
+   SET count_cited_articles = cited_articles.count_cited_articles
+FROM (SELECT cluster_no, auid, count(int_cluster_in_degrees) count_cited_articles
+      FROM
+    (SELECT DISTINCT *
+    FROM theta_plus.imm2000_2004_all_authors_internal
+    WHERE int_cluster_in_degrees > 0) aai
+    GROUP BY cluster_no, auid) cited_articles
+ WHERE theta_plus.imm2000_2004_authors_clusters.auid = cited_articles.auid
+   AND theta_plus.imm2000_2004_authors_clusters.mcl_cluster_no=cited_articles.cluster_no;
+
+
+
+-- eco2000_2010
+-- add cluster number to full graph degrees table
+ALTER TABLE theta_plus_ecology.eco2000_2010_full_graph_degrees
+ADD COLUMN mcl_cluster_no BIGINT;
+
+UPDATE theta_plus_ecology.eco2000_2010_full_graph_degrees
+SET mcl_cluster_no = cslu.cluster_no
+FROM theta_plus_ecology.eco2000_2010_cluster_scp_list_unshuffled cslu
+WHERE cslu.scp = theta_plus_ecology.eco2000_2010_full_graph_degrees.scp;
+
+-- add AUID to full graph degrees table
+
+CREATE TABLE theta_plus_ecology.eco2000_2010_all_authors_full_graph AS
+    SELECT fgd.*, sa.auid
+    FROM theta_plus_ecology.eco2000_2010_full_graph_degrees fgd
+    JOIN public.scopus_authors sa
+        ON fgd.scp = sa.scp;
+
+-- Get cluster number and number of articles per cluster for each author
+CREATE TABLE theta_plus_ecology.eco2000_2010_authors_clusters AS
+    SELECT auid, mcl_cluster_no, count(scp) count_articles
+    FROM
+        (SELECT DISTINCT scp, mcl_cluster_no, auid
+        FROM theta_plus_ecology.eco2000_2010_all_authors_full_graph ac) distinct_authors
+    GROUP BY auid, mcl_cluster_no
+    ORDER BY auid ASC;
+
+
+
+
+--------------------------------------------------------
+
+
+CREATE VIEW theta_plus.imm2000_2004_author_tiers_view
+  (auid, cluster_no, tier) AS
+    WITH cte AS (SELECT cluster_no, auid, min(tier) as tier
+                 FROM theta_plus.imm2000_2004_article_tiers
+                 GROUP BY cluster_no, auid)
+    SELECT auid, cluster_no, CASE
+        WHEN tier = 1 THEN 'tier_1'
+        WHEN tier = 2 THEN 'tier_2'
+        WHEN tier = 3 THEN 'tier_3' END AS tier
+    FROM cte
+    ORDER BY tier ASC;
+
+-- Add count of clusters to author tiers
+DROP TABLE IF EXISTS theta_plus.imm2000_2004_author_tiers;
+CREATE TABLE theta_plus.imm2000_2004_author_tiers AS
+  SELECT cc.auid, cc.total_num_clusters, icc.num_clusters_int_edges, aut.tier_1, aut.tier_2, aut.tier_3
+  FROM
+     (SELECT auid, count(mcl_cluster_no) as total_num_clusters                    -- total number of clusters
+      FROM theta_plus.imm2000_2004_authors_clusters
+      GROUP BY auid
+      ORDER BY total_num_clusters DESC) cc
+
+  LEFT JOIN (SELECT aai.auid, count(aai.cluster_no) as num_clusters_int_edges -- clusters with internal edges
+             FROM (SELECT DISTINCT auid, cluster_no                           -- based on which article tiers were
+                   FROM theta_plus.imm2000_2004_all_authors_internal) aai                -- computed
+                   GROUP BY aai.auid) icc ON cc.auid = icc.auid
+
+  LEFT JOIN (SELECT auid,
+             count(CASE WHEN tier = 'tier_1' THEN 1 END) AS tier_1,
+             count(CASE WHEN tier = 'tier_2' THEN 1 END) AS tier_2,
+             count(CASE WHEN tier = 'tier_3' THEN 1 END) AS tier_3
+             FROM theta_plus.imm2000_2004_author_tiers_view
+             GROUP BY auid) aut ON cc.auid = aut.auid;
+
+
 
 
